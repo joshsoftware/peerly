f122a7e8b2a5ee720e521888c6ba1fef48433308
Sunil <sunil.sonawane@joshsoftware.com>
2020-04-17 13:16:22 +0530

    Testing pre-commit hook

---
+++ node-backend/server.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var graphqlHTTP = require("express-graphql");
+var { buildSchema } = require("graphql");
+
+var app = express();
+app.use("/", graphqlHTTP({}));
+app.listen(process.env.SERVER_PORT);
+console.log("Server running on port: " + process.env.SERVER_PORT);
