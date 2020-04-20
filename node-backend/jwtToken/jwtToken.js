ba278bcf448cad1ab9a9e4c162af6da15ecf26b6
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-20 18:48:11 +0530

    updated jwt token file with environment variables

--- node-backend/jwtToken/jwtToken.js
+++ node-backend/jwtToken/jwtToken.js
@@ -1,24 +1,22 @@
-const jwt = require('jsonwebtoken');
-
-var createToken = function(auth) {
-    return jwt.sign({
-            id: auth.id
-        }, 'my-secret',
-        {
-            expiresIn: process.env.JWT_expire_time
-        });
+const jwt = require("jsonwebtoken");
+var createToken = function (auth) {
+  return jwt.sign(
+    {
+      id: auth.id,
+    },
+    "my-secret",
+    {
+      expiresIn: process.env.JWT_expire_time, //eslint-disable-line  no-undef
+    }
+  );
 };
 
 module.exports = {
-
-  generateToken: function(req, res, next) {
-	  console.log("onkar here");
-      req.token = createToken(req.auth);
-      return next();
+  generateToken: function (req, res, next) {
+    req.token = createToken(req.auth);
+    return next();
   },
-  sendToken: function(req, res) {
-	  console.log(req.token);
-      res.setHeader('x-auth-token', req.token);
-      return res.status(200).send(JSON.stringify(req.user));
-  }
+  sendToken: function (req, res) {
+    return res.status(200).send(req.token);
+  },
 };
