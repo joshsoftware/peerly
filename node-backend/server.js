68b34acbb72e7fc1172e78a7140008202fc7c056
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-04-20 13:15:39 +0530

    fetch list of recognition

--- node-backend/server.js
+++ node-backend/server.js
@@ -1,7 +1,17 @@
 var express = require("express");
 var graphqlHTTP = require("express-graphql");
 var { buildSchema } = require("graphql"); // eslint-disable-line no-unused-vars
+var queryRoot = require("./graphQl/query/queryResolver");
+const schema = new buildSchema.GraphQLSchema({
+  query: queryRoot,
+});
 
 var app = express();
-app.use("v1/graphql", graphqlHTTP({}));
+app.use(
+  "v1/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
 app.listen(process.env.SERVER_PORT); // eslint-disable-line no-undef
