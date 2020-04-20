3fc38c88523867cb7dd291333c1a54bee47bb56e
Avinash Mane <avimanepatil@gmail.com>
2020-04-20 19:10:42 +0530

    removed comment

--- node-backend/migrations/20200416100800-reported-recognitions-table.js
+++ node-backend/migrations/20200416100800-reported-recognitions-table.js
@@ -1,74 +1,72 @@
-'use strict';
+"use strict";
 
 var dbm;
-var type;
-var seed;
+var type; // eslint-disable-line no-unused-vars
+var seed; // eslint-disable-line no-unused-vars
 
-/**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
-exports.setup = function(options, seedLink) {
-  dbm = options.dbmigrate;
-  type = dbm.dataType;
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate; // eslint-disable-line no-unused-vars
+  type = dbm.dataType; // eslint-disable-line no-unused-vars
   seed = seedLink;
 };
 
-
-exports.up = function(db, callback) {
-  db.createTable('reported_recognitions', {
-    id: {
-      type: 'int',
-      primaryKey: true,
-      autoIncrement: true,
-      notNull: true
+exports.up = function (db, callback) {
+  db.createTable(
+    "reported_recognitions",
+    {
+      id: {
+        type: "int",
+        primaryKey: true,
+        autoIncrement: true,
+        notNull: true,
+      },
+      recognition_id: {
+        type: "int",
+        notNull: true,
+        foreignKey: {
+          name: "recognition_recognitionID_fk",
+          table: "recognitions",
+          mapping: "id",
+          rules: {
+            onDelete: "NO ACTION",
+          },
+        },
+      },
+      type_of_reporting: {
+        type: "string",
+        length: 45,
+        notNull: true,
+      },
+      reason_for_reporting: {
+        type: "text",
+        notNull: true,
+      },
+      reported_by: {
+        type: "int",
+        notNull: true,
+        foreignKey: {
+          name: "user_recognitionID_fk",
+          table: "users",
+          mapping: "id",
+          rules: {
+            onDelete: "NO ACTION",
+          },
+        },
+      },
+      reported_on: {
+        type: "timestamp",
+      },
     },
-    recognition_id: {
-      type: 'int',
-      notNull: true,
-      foreignKey: {
-        name: 'recognition_recognitionID_fk',
-        table: 'recognitions',
-        mapping: 'id',
-        rules: {
-          onDelete: 'NO ACTION'
-        }
-      }
-    },
-    type_of_reporting: {
-      type: 'string',
-      length:45,
-      notNull: true
-    },
-    reason_for_reporting: {
-      type: 'text',
-      notNull: true
-    },
-    reported_by: {
-      type: 'int',
-      notNull: true,
-      foreignKey: {
-        name: 'user_recognitionID_fk',
-        table: 'users',
-        mapping: 'id',
-        rules: {
-          onDelete: 'NO ACTION'
-        }
-      }
-    },
-    reported_on: {
-      type: 'timestamp'
-    },
-  }, function(err) {
-    if (err) return callback(err);
-    return callback();
-  });
+    function (err) {
+      if (err) return callback(err);
+      return callback();
+    }
+  );
 };
-exports.down = function(db, callback) {
-  db.dropTable('reported_recognitions', callback);
+exports.down = function (db, callback) {
+  db.dropTable("reported_recognitions", callback);
 };
-
 
 exports._meta = {
-  "version": 1
+  version: 1,
 };
