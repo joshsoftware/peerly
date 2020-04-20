76a5d178ac2d701cda6e839b39ae135e2fe1543f
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-20 18:41:52 +0530

    added updated routes

--- node-backend/routes/routes.js
+++ node-backend/routes/routes.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { generateToken, sendToken } = require('../jwtToken/jwtToken');
-const passport = require('passport');
-const request = require('request');
-const bodyParser = require('body-parser');
-require('../google_auth/google_auth')();
-router.use(bodyParser.urlencoded({ extended: true }))
-
-router.route('/google')
-.post(passport.authenticate('google-token', {session: false}), function(req, res,next) {
-	console.log("in passport auth function");
-        if (!req.user) {
-			console.log("hello");
-            return res.send(401, 'User Not Authenticated by google');
-        }
-        req.auth = {
-            id: req.user.id
-        };
-        next();
-    }, generateToken, sendToken);
+const { generateToken, sendToken } = require("../jwtToken/jwtToken");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+require("../google_auth/google_auth")();
+router.use(bodyParser.urlencoded({ extended: true }));
+router.route("/google").post(
+  passport.authenticate("google-token", { session: false }),
+  function (req, res, next) {
+    if (!req.user) {
+      return res.send(401, "User Not Authenticated by google");
+    }
+    req.auth = {
+      id: req.user.id,
+    };
+    next();
+  },
+  generateToken,
+  sendToken
+);
 
 module.exports = router;
