27847b54aeee424cf482270e0de682056b27496e
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-20 18:30:53 +0530

    added search by email to google authentication page

--- node-backend/google_auth/google_auth.js
+++ node-backend/google_auth/google_auth.js
@@ -1,17 +1,27 @@
-'use strict';
-var passport = require('passport');
-var GoogleTokenStrategy = require('passport-google-token').Strategy;
-var config = require('../config/config');
+"use strict";
+var passport = require("passport");
+const pool = require("../database/database");
+var GoogleTokenStrategy = require("passport-google-token").Strategy;
+var config = require("../config/config");
 
 module.exports = function () {
-    passport.use(new GoogleTokenStrategy({
-            clientID: config.googleAuth.clientID,
-            clientSecret: config.googleAuth.clientSecret
-        },
-        function (accessToken, refreshToken, profile, done) {
-			console.log("in google token");
-			console.log(profile.emails[0].value);
-			return done(null, profile);
-
-       }));
+  passport.use(
+    new GoogleTokenStrategy(
+      {
+        clientID: config.googleAuth.clientID,
+        clientSecret: config.googleAuth.clientSecret,
+      },
+      async function (accessToken, refreshToken, profile, done) {
+        let email = profile.emails[0].value;
+        let result = await pool.query({
+          text: "SELECT * FROM users WHERE email = '" + email + "'",
+        });
+        if (result.rows[0]) {
+          return done(null, profile);
+        } else {
+          done("not authorized", null);
+        }
+      }
+    )
+  );
 };
