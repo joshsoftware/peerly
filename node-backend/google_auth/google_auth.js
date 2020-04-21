373ad532b464609a9bbfa0cc95dc9d60aa76f67a
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-21 16:53:02 +0530

    removed prettier disabled lines

--- node-backend/google_auth/google_auth.js
+++ node-backend/google_auth/google_auth.js
@@ -17,14 +17,17 @@ module.exports = function () {
         let user_name = profile.name.givenName;
         let display_name = profile.displayName;
         let result = await pool.query({
-          text: " select roles.role,users.email,organizations.name from users,roles,organizations where users.email = '" + email + "'"/*eslint-disable-line prettier/prettier */,
+          text:
+            "select roles.role,users.email,organizations.name from users,roles,organizations where users.email = '" +
+            email +
+            "'",
         });
         if (result.rowCount) {
           let token_email = result.rows[0].email;
           let role = result.rows[0].role;
           let organization_data = result.rows[0].name;
           var exp_time = {
-            "expiresIn"/*eslint-disable-line prettier/prettier */: process.env.JWT_expire_time, //eslint-disable-line  no-undef
+            expiresIn: process.env.JWT_expire_time, //eslint-disable-line  no-undef
           };
 
           const token = jwt.sign(
@@ -35,27 +38,42 @@ module.exports = function () {
             exp_time
           );
           return done(null, {
-            "token": token, //eslint-disable-line prettier/prettier 
-            "role": role, //eslint-disable-line prettier/prettier 
-            "organization" : organization_data, //eslint-disable-line prettier/prettier 
+            token: token,
+            role: role,
+            organization: organization_data,
           });
         } else {
-          let domain_name = (profile.emails[0].value)/*eslint-disable-line prettier/prettier */.split("@").pop();
+          let domain_name = profile.emails[0].value.split("@").pop();
           let domain_result = await pool.query({
-            text: "SELECT * FROM organizations WHERE domain_name = '" + domain_name + "'"/*eslint-disable-line prettier/prettier */,
+            text:
+              " SELECT * FROM organizations WHERE domain_name = '" +
+              domain_name +
+              "'",
           });
           if (domain_result.rowCount) {
             let domain_id = domain_result.rows[0].id;
-            await pool.query("insert into users (org_id,name,email,display_name,soft_delete,role_id,hi5_quota_balance) values('"+domain_id+"','"+user_name+"','"+email+"','"+display_name+"',false,1,5);" /*eslint-disable-line prettier/prettier */,
+            await pool.query(
+              "insert into users (org_id,name,email,display_name,soft_delete,role_id,hi5_quota_balance) values('" +
+                domain_id +
+                "','" +
+                user_name +
+                "','" +
+                email +
+                "','" +
+                display_name +
+                "',false,1,5);"
             );
-            let user_retrive = await pool.query("select roles.role,users.email,organizations.name from users,roles,organizations where users.email = '"+ email +"'"/*eslint-disable-line prettier/prettier */,
+            let user_retrive = await pool.query(
+              "select roles.role,users.email,organizations.name from users,roles,organizations where users.email = '" +
+                email +
+                "'"
             );
             if (user_retrive.rowCount) {
               let token_email = user_retrive.rows[0].email;
               let role = user_retrive.rows[0].role;
               let organization_data = user_retrive.rows[0].name;
               var exp_time /*eslint-disable-line no-redeclare*/ = {
-                "expiresIn" /*eslint-disable-line prettier/prettier */: process.env.JWT_EXPIRE_TIME, //eslint-disable-line  no-undef
+                expiresIn: process.env.JWT_EXPIRE_TIME, //eslint-disable-line  no-undef
               };
               const token = jwt.sign(
                 {
@@ -65,9 +83,9 @@ module.exports = function () {
                 exp_time
               );
               return done(null, {
-                "token": token,//eslint-disable-line prettier/prettier 
-                "role": role,//eslint-disable-line prettier/prettier 
-                "organization" : organization_data,//eslint-disable-line prettier/prettier 
+                token: token,
+                role: role,
+                organization: organization_data,
               });
             } else {
               done(null);
