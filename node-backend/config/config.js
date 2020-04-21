6b03770bdc214507180ec37695f51d199fd49d2c
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-21 16:38:41 +0530

    removed prettier disabled lines

--- node-backend/config/config.js
+++ node-backend/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  'googleAuth' /* eslint-disable-line  prettier/prettier*/ : {
-    "clientID" /* eslint-disable-line  prettier/prettier*/ : process.env.CLIENT_ID, //eslint-disable-line  no-undef
-    "clientSecret" /* eslint-disable-line  prettier/prettier*/ : process.env.CLIENT_SECRET, //eslint-disable-line  no-undef
+  googleAuth: {
+    clientID: process.env.CLIENT_ID, //eslint-disable-line  no-undef
+    clientSecret: process.env.CLIENT_SECRET, //eslint-disable-line  no-undef
   },
 };
