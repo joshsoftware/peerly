9635cd13282c1d46b489694e1e078fb24d8db876
pradipdkshirsagar <pradip.kshirsagar@joshsoftware.com>
2020-04-21 17:54:15 +0530

    Added the list, showm, create and delete endpoints for core values

--- go-backend/db/db.go
+++ go-backend/db/db.go
@@ -9,4 +9,10 @@ type Storer interface {
 	//Create(context.Context, User) error
 	//GetUser(context.Context) (User, error)
 	//Delete(context.Context, string) error
+
+	// Core values
+	ListCoreValues(context.Context, int64) ([]CoreValue, error)
+	GetCoreValue(context.Context, int64, int64) (CoreValue, error)
+	CreateCoreValue(ctx context.Context, organisationID int64, coreValue CoreValue) (coreValueID string, err error)
+	DeleteCoreValue(ctx context.Context, organizationID, coreValueID int64) (err error)
 }
