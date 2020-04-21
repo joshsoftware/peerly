9fdefabf447c9227471e52966ad89b7faecfe991
Avinash Mane <avimanepatil@gmail.com>
2020-04-21 16:00:54 +0530

    added reactstrap card

--- react-frontend/src/components/listRecognitions.js
+++ react-frontend/src/components/listRecognitions.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Row, Col, Label, Button, Container } from "reactstrap";
+import { Row, Col, Form, Button, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Card, CardHeader, CardFooter, CardBody, CardText } from "reactstrap";
 import {
   MenuButton,
   MenuList,
@@ -44,39 +45,39 @@ const ListRecognition = () => {
         <Col xs="3"></Col>
         <Col>
           {persons.map((person) => (
-            <Row
-              className="border border-primary mt-5 bg-light grey"
+            <Card
+              className="border border-primary mt-4 bg-light grey"
               key={person.id}
             >
-              <Col>
-                <Row className="mt-2 d-flex justify-content-around mt-2">
-                  <Button className="bg-success">
-                    <h6>+{person.hi5}</h6>
-                  </Button>
-                  <Button>
-                    <h6>{person.name}</h6>
-                  </Button>
-                  <Label className="text-muted">{person.time}</Label>
-                  <Menu>
-                    <MenuButton className="btn bg-light grey">
-                      <FontAwesomeIcon icon={faEllipsisV} />
-                    </MenuButton>
-                    <MenuList>
-                      <MenuItem></MenuItem>
-                      <MenuLink to="view"></MenuLink>
-                    </MenuList>
-                  </Menu>
-                </Row>
-                <Row className="h-50 p-3 border border-success ml-5 mr-5 mt-2 font-italic text-left">
+              <CardHeader className="d-flex justify-content-around">
+                <Button className="bg-success btn-sm">
+                  <h5>+{person.hi5}</h5>
+                </Button>
+                <Button className="btn-sm bg-primary">
+                  <h5>{person.name}</h5>
+                </Button>
+                <CardText className="text-muted">{person.time}</CardText>
+                <Menu>
+                  <MenuButton className="btn bg-light grey">
+                    <FontAwesomeIcon icon={faEllipsisV} />
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem></MenuItem>
+                    <MenuLink to="view"></MenuLink>
+                  </MenuList>
+                </Menu>
+              </CardHeader>
+              <CardBody className="h-50 p-3 ml-5 mr-5 font-italic text-left">
+                <CardText>
                   <h5>{person.text}</h5>
-                </Row>
-                <Row className="ml-5 mt-2 mb-2">
-                  <Label className="text-primary font-weight-lighter">
-                    <h5>#{person.core}</h5>
-                  </Label>
-                </Row>
-              </Col>
-            </Row>
+                </CardText>
+              </CardBody>
+              <CardFooter className="d-flex content-left">
+                <Form.Label className="text-primary font-weight-lighter">
+                  <h5>#{person.core}</h5>
+                </Form.Label>
+              </CardFooter>
+            </Card>
           ))}
         </Col>
         <Col xs="3"></Col>
