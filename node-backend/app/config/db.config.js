66a43cd470b0c77ca78e254d1cdc282f3c2aa3c4
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-22 15:39:00 +0530

    added parseInt to min max value to give integer input

--- node-backend/app/config/db.config.js
+++ node-backend/app/config/db.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-  HOST: process.env.HOST, //eslint-disable-line  no-undef
-  USER: process.env.DB_USER_NAME, //eslint-disable-line  no-undef
-  PASSWORD: process.env.DB_PASSWORD, //eslint-disable-line  no-undef
-  DB: process.env.DB_DATABASE_NAME, //eslint-disable-line  no-undef
-  dialect: "postgres",
+  /*eslint-disable  no-undef*/
+  HOST: process.env.HOST,
+  USER: process.env.DB_USER_NAME,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_DATABASE_NAME,
+  dialect: process.env.DB_DIALECT,
+  /*eslint-enable no-undef*/
   pool: {
-    max: process.env.SEQUELIZE_CONN_MAX, //eslint-disable-line  no-undef
-    min: process.env.SEQUELIZE_CONN_min, //eslint-disable-line  no-undef
-    acquire: process.env.SEQUELIZE_CONN_ACQUIRE, //eslint-disable-line  no-undef
-    idle: process.env.SEQUELIZE_CONN_IDLE, //eslint-disable-line  no-undef
+    /*eslint-disable  no-undef*/
+    max: parseInt(process.env.SEQUELIZE_CONN_MAX),
+    min: parseInt(process.env.SEQUELIZE_CONN_MIN),
+    acquire: process.env.SEQUELIZE_CONN_ACQUIRE,
+    idle: process.env.SEQUELIZE_CONN_IDLE,
+    /*eslint-enable  no-undef*/
   },
 };
