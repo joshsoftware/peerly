d8b20d6ebb33d709f448d020a8374b41f6a2a7cf
pradipdkshirsagar <pradip.kshirsagar@joshsoftware.com>
2020-04-22 10:49:54 +0530

    Added update endpoint for core value management

--- go-backend/service/router.go
+++ go-backend/service/router.go
@@ -27,6 +27,7 @@ func InitRouter(deps Dependencies) (router *mux.Router) {
 	router.HandleFunc("/organisations/{organisation_id:[0-9]+}/core_values", listCoreValuesHandler(deps)).Methods(http.MethodGet).Headers(versionHeader, v1)
 	router.HandleFunc("/organisations/{organisation_id:[0-9]+}/core_values", createCoreValueHandler(deps)).Methods(http.MethodPost).Headers(versionHeader, v1)
 	router.HandleFunc("/organisations/{organisation_id:[0-9]+}/core_values/{id:[0-9]+}", deleteCoreValueHandler(deps)).Methods(http.MethodDelete).Headers(versionHeader, v1)
+	router.HandleFunc("/organisations/{organisation_id:[0-9]+}/core_values/{id:[0-9]+}", updateCoreValueHandler(deps)).Methods(http.MethodPut).Headers(versionHeader, v1)
 	router.HandleFunc("/users", listUsersHandler(deps)).Methods(http.MethodGet).Headers(versionHeader, v1)
 
 	return
