3cb27180e3cb04b3bd4a2e8a2d60d4285e64c32e
pradipdkshirsagar <pradip.kshirsagar@joshsoftware.com>
2020-04-22 18:10:16 +0530

    1. Added validations for create nad list sub core values. 2. Refactored code.

--- go-backend/db/core_value.go
+++ go-backend/db/core_value.go
@@ -2,28 +2,32 @@ package db
 
 import (
 	"context"
+	"time"
 
 	logger "github.com/sirupsen/logrus"
 )
 
-type CoreValue struct {
-	ID                int64  `db:"id" json:"id"`
-	OrgID             int64  `db:"org_id" json:"org_id"`
-	CoreValueText     string `db:"core_value_text" json:"core_value_text"`
-	Description       string `db:"description" json:"description"`
-	ParentCoreValueID *int64 `db:"parent_core_value_id" json:"parent_core_value_id"`
-}
-
 const (
-	listCoreValuesQuery  = `SELECT * FROM core_values WHERE org_id = $1`
-	getCoreValueQuery    = `SELECT * FROM core_values WHERE org_id = $1 and id = $2`
-	createCoreValueQuery = `INSERT INTO core_values (org_id, core_value_text,
-		description, parent_core_value_id) VALUES ($1, $2, $3, $4) RETURNING id`
+	listCoreValuesQuery    = `SELECT * FROM core_values WHERE org_id = $1 and parent_core_value_id IS NULL`
+	listSubCoreValuesQuery = `SELECT * FROM core_values WHERE org_id = $1 and parent_core_value_id = $2`
+	getCoreValueQuery      = `SELECT * FROM core_values WHERE org_id = $1 and id = $2`
+	createCoreValueQuery   = `INSERT INTO core_values (org_id, core_value_text,
+		description, parent_core_value_id, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`
 	deleteSubCoreValueQuery = `DELETE FROM core_values WHERE org_id = $1 and parent_core_value_id = $2`
 	deleteCoreValueQuery    = `DELETE FROM core_values WHERE org_id = $1 and id = $2`
-	updateCoreValueQuery    = `UPDATE core_values SET (core_value_text, description) =
-		($1, $2) where id = $3 and org_id = $4`
+	updateCoreValueQuery    = `UPDATE core_values SET (core_value_text, description, updated_at) =
+		($1, $2, $3) where id = $4 and org_id = $5`
 )
+
+type CoreValue struct {
+	ID                int64     `db:"id" json:"id"`
+	OrgID             int64     `db:"org_id" json:"org_id"`
+	CoreValueText     string    `db:"core_value_text" json:"core_value_text"`
+	Description       string    `db:"description" json:"description"`
+	ParentCoreValueID *int64    `db:"parent_core_value_id" json:"parent_core_value_id"`
+	CreatedAt         time.Time `db:"created_at" json:"-"`
+	UpdatedAt         time.Time `db:"updated_at" json:"-"`
+}
 
 func (s *pgStore) ListCoreValues(ctx context.Context, organisationID int64) (coreValues []CoreValue, err error) {
 	coreValues = make([]CoreValue, 0)
@@ -32,6 +36,23 @@ func (s *pgStore) ListCoreValues(ctx context.Context, organisationID int64) (cor
 		&coreValues,
 		listCoreValuesQuery,
 		organisationID,
+	)
+	if err != nil {
+		logger.WithField("err", err.Error()).Error("Error while getting core values")
+		return
+	}
+
+	return
+}
+
+func (s *pgStore) ListSubCoreValues(ctx context.Context, organisationID, coreValueID int64) (coreValues []CoreValue, err error) {
+	coreValues = make([]CoreValue, 0)
+	err = s.db.SelectContext(
+		ctx,
+		&coreValues,
+		listSubCoreValuesQuery,
+		organisationID,
+		coreValueID,
 	)
 	if err != nil {
 		logger.WithField("err", err.Error()).Error("Error while getting core values")
@@ -57,15 +78,17 @@ func (s *pgStore) GetCoreValue(ctx context.Context, organisationID, coreValueID 
 	return
 }
 
-func (s *pgStore) CreateCoreValue(ctx context.Context, organisationID int64, coreValue CoreValue) (coreValueID string, err error) {
-	err = s.db.GetContext(
+func (s *pgStore) CreateCoreValue(ctx context.Context, organisationID int64, coreValue CoreValue) (err error) {
+	now := time.Now()
+	_, err = s.db.ExecContext(
 		ctx,
-		&coreValueID,
 		createCoreValueQuery,
 		organisationID,
 		coreValue.CoreValueText,
 		coreValue.Description,
 		coreValue.ParentCoreValueID,
+		now,
+		now,
 	)
 	if err != nil {
 		logger.WithField("err", err.Error()).Error("Error while creating core value")
@@ -102,11 +125,13 @@ func (s *pgStore) DeleteCoreValue(ctx context.Context, organisationID, coreValue
 }
 
 func (s *pgStore) UpdateCoreValue(ctx context.Context, organisationID, coreValueID int64, coreValue CoreValue) (err error) {
+	now := time.Now()
 	_, err = s.db.ExecContext(
 		ctx,
 		updateCoreValueQuery,
 		coreValue.CoreValueText,
 		coreValue.Description,
+		now,
 		coreValueID,
 		organisationID,
 	)
--- go-backend/db/mock.go
+++ go-backend/db/mock.go
@@ -14,13 +14,3 @@ func (m *DBMockStore) ListUsers(ctx context.Context) (users []User, err error) {
 	args := m.Called(ctx)
 	return args.Get(0).([]User), args.Error(1)
 }
-
-func (m *DBMockStore) ListCoreValues(ctx context.Context, organisationID string) (coreValues []CoreValue, err error) {
-	args := m.Called(ctx, organisationID)
-	return args.Get(0).([]CoreValue), args.Error(1)
-}
-
-func (m *DBMockStore) GetCoreValues(ctx context.Context, organisationID, coreValueID string) (coreValue CoreValue, err error) {
-	args := m.Called(ctx, organisationID, coreValueID)
-	return args.Get(0).(CoreValue), args.Error(1)
-}
