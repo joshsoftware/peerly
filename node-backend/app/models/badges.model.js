c44cfde77faa5baeaa24835cd481e41310090783
Avinash Mane <avimanepatil@gmail.com>
2020-04-23 21:01:01 +0530

    Added badges Sequelize schema model

---
+++ node-backend/app/models/badges.model.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, Sequelize) => {
+    const Badges = sequelize.define(
+      "badges",
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          notNull: true,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        name: {
+            type: Sequelize.STRING,
+            length: 45,
+            notNull: true,
+        },
+        org_id: {
+          type: Sequelize.INTEGER,
+          notNull: true,
+        },
+        hi5_count_required: {
+          type: Sequelize.INTEGER,
+          notNull: true,
+        },
+        hi5_frequency: {
+          type: Sequelize.STRING,
+          length: 45,
+          notNull: true,
+        },
+      },
+      {
+        timestamp: false,
+        createdAt: false,
+        updatedAt: false,
+      }
+    );
+    return Badges;
+  };
+  
