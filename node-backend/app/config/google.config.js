dcbec643eacc00cbc9eaf0d33a82772be0727812
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-23 17:34:58 +0530

    added login route in route folder

--- node-backend/app/config/google.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleAuth: {
-    clientID: process.env.GOOGLE_CLIENT_ID, //eslint-disable-line  no-undef
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET, //eslint-disable-line  no-undef
-  },
-};
--- node-backend/server.js
+++ node-backend/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 require("dotenv").config("./.env");
 const bodyParser = require("body-parser");
-const routes = require("./app/routes/routes");
-const dbConn = require("./app/models/sequelize");
+const routes = require("./peerly/routes/routes");
+const dbConn = require("./peerly/models/sequelize");
 const app = express();
 const http = require("http");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 dbConn.sequelize;
-app.use("/v1/login", routes);
+app.use("/v1", routes);
 const httpServer = http.createServer(app);
 httpServer.listen(process.env.HTTP_PORT || 8080); // eslint-disable-line no-undef
