cbdb24c386a90214d3d11b02c96536bc5547a19b
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-23 14:01:53 +0530

    added database connectionin server file

--- node-backend/server.js
+++ node-backend/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 require("dotenv").config("./.env");
 const bodyParser = require("body-parser");
 const routes = require("./app/routes/routes");
+const dbConn = require("./app/models/index");
 const app = express();
 const http = require("http");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+dbConn.sequelize;
 app.use("/v1/login", routes);
 const httpServer = http.createServer(app);
 httpServer.listen(process.env.HTTP_PORT || 8080); // eslint-disable-line no-undef
