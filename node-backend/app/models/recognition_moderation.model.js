90eed952e85a6a84dd8674db603d440549a76569
Avinash Mane <avimanepatil@gmail.com>
2020-04-23 21:37:05 +0530

    Added recognition_moderation Sequelize schema model

---
+++ node-backend/app/models/recognition_moderation.model.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, Sequelize) => {
+  const Recognition_Moderation = sequelize.define(
+    "recognition_moderation",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        notNull: true,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      recognition_id: {
+        type: Sequelize.INTEGER,
+        notNull: true,
+      },
+      is_inappropriate: {
+        type: Sequelize.BOOLEAN,
+        notNull: true,
+      },
+      moderated_comment: {
+        type: Sequelize.STRING,
+        length: 45,
+        notNull: true,
+      },
+      moderated_by: {
+        type: Sequelize.INTEGER,
+        notNull: true,
+      },
+      moderated_on: {
+        type: "TIMESTAMP",
+        notNull: true,
+      },
+    },
+    {
+      timestamp: false,
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
+  return Recognition_Moderation;
+};
