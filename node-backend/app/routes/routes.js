d9ebfc5c635689300dab5df4ece73d63cf207346
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-23 14:00:26 +0530

    added function for authentication with database

--- node-backend/app/routes/routes.js
+++ node-backend/app/routes/routes.js
@@ -2,18 +2,14 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const bodyParser = require("body-parser");
+const loginController = require("../controllers/loginController.");
 require("../google_auth/google_auth")();
 router.use(bodyParser.urlencoded({ extended: true }));
 router
   .route("/")
-  .post(passport.authenticate("google-token", { session: false }), function (
-    req,
-    res
-  ) {
-    if (!req.user) {
-      return res.send(401, "User Not Authenticated by google");
-    }
-    res.status(200).send(req.user);
-  });
+  .post(
+    passport.authenticate("google-token", { session: false }),
+    loginController.login
+  );
 
 module.exports = router;
