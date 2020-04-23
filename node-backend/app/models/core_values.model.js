1eca98d532d23c54a040712caa0df601e952e2c4
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-04-23 20:41:12 +0530

    create core_values table sequelize schema

---
+++ node-backend/app/models/core_values.model.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, Sequelize) => {
+  const CoreValues = sequelize.define("core_values", {
+    id: {
+      type: Sequelize.INTEGER,
+      notNull: true,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    org_id: {
+      type: Sequelize.INTEGER,
+      notNull: true,
+    },
+    core_value_text: {
+      type: Sequelize.STRING,
+      notNull: true,
+    },
+    description: {
+      type: Sequelize.STRING
+    },
+    parent_core_value_id: {
+      type: Sequelize.INTEGER,
+      notNull: true,
+    }
+  });
+  return CoreValues;
+}
