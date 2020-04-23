1ac64af60a9b053532c3844799c7eff3bb4d0400
pradipdkshirsagar <pradip.kshirsagar@joshsoftware.com>
2020-04-23 12:57:58 +0530

    Added test cases for list core values and delete core value

--- go-backend/service/user_http_test.go
+++ go-backend/service/user_http_test.go
@@ -28,6 +28,7 @@ func (suite *UsersHandlerTestSuite) SetupTest() {
 
 func TestExampleTestSuite(t *testing.T) {
 	suite.Run(t, new(UsersHandlerTestSuite))
+	suite.Run(t, new(CoreValueHandlerTestSuite))
 }
 
 func (suite *UsersHandlerTestSuite) TestListUsersSuccess() {
