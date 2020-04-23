665b4ecc4fa86a5ead61c42ff92c3e8a7682e47f
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-04-23 20:51:14 +0530

    create recognition_hi5 table sequelize schema

---
+++ node-backend/app/models/recognition_hi5.model.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, Sequelize) => {
+  const RecognitionHi5 = sequelize.define("recognition_hi5", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      notNull: true,
+    },
+    recognition_id: {
+      type: Sequelize.INTEGER,
+      notNull: true,
+    },
+    hi5_by: {
+      type: Sequelize.INTEGER,
+      notNull: true,
+    },
+    hi5_given_on_date: {
+      type: "TIMESTAMP",
+      notNull: true,
+    },
+  });
+  return RecognitionHi5;
+};
