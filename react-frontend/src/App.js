6b1d09a78779397e0a2633d3824e3fb7d933f046
Avinash Mane <avimanepatil@gmail.com>
2020-04-23 16:23:50 +0530

    changes in container folder

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import ListRecognitionContainers from "components/listRecognitionsContainers";
+import ListRecognitionsContainer from "containers/listRecognitionContainers";
 
 function App() {
   return (
     <div className="App">
-      <ListRecognitionContainers />
+      <ListRecognitionsContainer />
     </div>
   );
 }
