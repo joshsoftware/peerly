0924b0b04f3ea103b68cf8ad927d7b7bf1a2cbec
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-23 13:55:26 +0530

    added eslint disable lines for undef from start to end of the code

--- node-backend/app/config/db.config.js
+++ node-backend/app/config/db.config.js
@@ -1,17 +1,15 @@
+/*eslint-disable  no-undef*/
 module.exports = {
-  /*eslint-disable  no-undef*/
   HOST: process.env.HOST,
   USER: process.env.DB_USER_NAME,
   PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB_DATABASE_NAME,
   dialect: process.env.DB_DIALECT,
-  /*eslint-enable no-undef*/
   pool: {
-    /*eslint-disable  no-undef*/
     max: parseInt(process.env.SEQUELIZE_CONN_MAX),
     min: parseInt(process.env.SEQUELIZE_CONN_MIN),
     acquire: process.env.SEQUELIZE_CONN_ACQUIRE,
     idle: process.env.SEQUELIZE_CONN_IDLE,
-    /*eslint-enable  no-undef*/
   },
 };
+/*eslint-enable  no-undef*/
