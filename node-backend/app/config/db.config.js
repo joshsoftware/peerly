1d2b8086f502c4c7b7b15dac20a4f154f35d4bdc
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-23 18:28:22 +0530

    added app folder

---
+++ node-backend/app/config/db.config.js
@@ -0,0 +1,15 @@
+/*eslint-disable  no-undef*/
+module.exports = {
+  HOST: process.env.HOST,
+  USER: process.env.DB_USER_NAME,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_DATABASE_NAME,
+  dialect: process.env.DB_DIALECT,
+  pool: {
+    max: parseInt(process.env.SEQUELIZE_CONN_MAX),
+    min: parseInt(process.env.SEQUELIZE_CONN_MIN),
+    acquire: process.env.SEQUELIZE_CONN_ACQUIRE,
+    idle: process.env.SEQUELIZE_CONN_IDLE,
+  },
+};
+/*eslint-enable  no-undef*/
