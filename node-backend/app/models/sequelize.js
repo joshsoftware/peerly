9055f26821ae7a336a1c42fee7579ca7761af1a8
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-24 08:01:00 +0530

    added seqelize config for heroku

--- node-backend/app/models/sequelize.js
+++ node-backend/app/models/sequelize.js
@@ -1,16 +1,10 @@
 const Sequelize = require("sequelize");
-
-const dbConfig = require("../config/db.config.js");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
-  },
+const sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  port: 5432,
+  host: "https://mypeerlyapp.herokuapp.com/",
+  logging: true, //false
 });
 const db = {};
 db.Sequelize = Sequelize;
