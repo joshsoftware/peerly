83a26d2fdf16aefa5757e52a7955fceba9fa9ec2
mayuriardad <mayuri.ardad@joshsoftware.com>
2020-04-24 09:44:26 +0530

    Configure redux store - Add combine reducers

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import configureStore from "redux-store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={configureStore()}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
