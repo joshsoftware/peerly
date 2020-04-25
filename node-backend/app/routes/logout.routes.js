063b2733e03ee6dcdda43c2c0e89950274c89f23
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-25 20:24:07 +0530

    added function for jwt blacklisted checking

--- node-backend/app/routes/logout.routes.js
+++ node-backend/app/routes/logout.routes.js
@@ -2,9 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const token = require("../jwtTokenValidation/jwtTokenValidation");
+const blacklistedToken = require("../jwtTokenValidation/blacklistedToken");
 const logoutController = require("../controllers/logoutController");
 const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: true }));
-router.route("/logout").get(token.autheticateToken, logoutController.logout);
+router.get(
+  "/logout",
+  blacklistedToken.userBlacklistedToken,
+  token.autheticateToken,
+  logoutController.logout
+);
 
 module.exports = router;
