080539a1905ee00296edb8f65baf64d860a3e308
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-26 13:10:57 +0530

    updated jwt token validation file

--- node-backend/app/jwtTokenValidation/jwtTokenValidation.js
+++ node-backend/app/jwtTokenValidation/jwtTokenValidation.js
@@ -3,14 +3,18 @@ module.exports.autheticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (token === null)
-    return res.status(401).send({
-      message: "Token not provided",
+    res.status(400).send({
+      error: {
+        message: "undefined token",
+      },
     });
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err) => {
-    if (err)
-      return res.status(401).send({
+    if (err) {
+      res.status(401).send({
         message: "Not a valid token",
       });
-    next();
+    } else {
+      next();
+    }
   });
 };
