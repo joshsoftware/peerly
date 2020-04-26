5c52cd764e400d044256dbe4cb5dff024cd45d28
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-26 13:07:50 +0530

    added blacklisted token changes

--- node-backend/app/jwtTokenValidation/blacklistedToken.js
+++ node-backend/app/jwtTokenValidation/blacklistedToken.js
@@ -2,36 +2,28 @@ const db = require("../models/sequelize");
 module.exports.userBlacklistedToken = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (token === null) {
-    return res.status(404).send({
+  let result;
+  await db.sequelize
+    .query(
+      "select id from user_blacklisted_tokens where token = '" + token + "'"
+    )
+    .then((data) => {
+      result = data[1].rowCount;
+    })
+    .catch(() => {
+      res.status(500).send({
+        error: {
+          message: "internal server error",
+        },
+      });
+    });
+  if (result) {
+    res.status(401).send({
       error: {
-        message: "token not provided",
+        message: "unauthorized user",
       },
     });
   } else {
-    let result;
-    await db.sequelize
-      .query(
-        "select id from user_blacklisted_tokens where token = '" + token + "'"
-      )
-      .then((data) => {
-        result = data[1].rowCount;
-      })
-      .catch(() => {
-        res.status(500).send({
-          error: {
-            message: "internal server error",
-          },
-        });
-      });
-    if (result) {
-      res.status(401).send({
-        error: {
-          message: "unauthorized user",
-        },
-      });
-    } else {
-      next();
-    }
+    next();
   }
 };
