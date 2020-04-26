f0363c19f358ea964406e121b06a5ef341094397
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-26 16:28:32 +0530

    added yup validations for access_token

--- node-backend/app/routes/routes.js
+++ node-backend/app/routes/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const passport = require("passport");
 const bodyParser = require("body-parser");
+const yup = require("yup");
 
 const loginController = require("../controllers/loginController");
 require("../google_auth/google_auth")();
@@ -9,15 +10,23 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.post(
   "/oauth/google",
   (req, res, next) => {
-    if (req.body.access_token) {
-      next();
-    } else {
-      res.status(400).send({
-        error: {
-          message: "invalid access token",
-        },
-      });
-    }
+    const access_token = {
+      token: req.body.access_token,
+    };
+    const schema = yup.object().shape({
+      token: yup.string().required(),
+    });
+    schema.isValid(access_token).then(function (valid) {
+      if (valid) {
+        next();
+      } else {
+        res.status(400).send({
+          error: {
+            message: "undefined access token",
+          },
+        });
+      }
+    });
   },
   passport.authenticate("google-token", { session: false }),
   loginController.login
