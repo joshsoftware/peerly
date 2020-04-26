cbe1e3729a33e1986f0f2699923d969b0693b1c9
Avinash Mane <avimanepatil@gmail.com>
2020-04-27 01:36:16 +0530

    added test cases

--- node-backend/app/routes/core_values.routes.js
+++ node-backend/app/routes/core_values.routes.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const CorevalueController = require("../controllers/CoreValueController");
-const router = express.Router();
-router.use(bodyParser.urlencoded({ extended: true }));
+const coreValueRouter = express.Router();
+coreValueRouter.use(bodyParser.urlencoded({ extended: true }));
 //for create core value
-router.post(
+coreValueRouter.post(
   "/organisations/:organisation_id/core_values",
   CorevalueController.create
 );
 //for get all core values
-router.get(
+coreValueRouter.get(
   "/organisations/:organisation_id/core_values",
   CorevalueController.findAll
 );
 //for get core value by id
-router.get(
+coreValueRouter.get(
   "/organisations/:organisation_id/core_values/:id",
   CorevalueController.findOne
 );
 //for update core value
-router.put(
+coreValueRouter.put(
   "/organisations/:organisation_id/core_values/:id",
   CorevalueController.update
 );
 
-module.exports = router;
+module.exports = coreValueRouter;
