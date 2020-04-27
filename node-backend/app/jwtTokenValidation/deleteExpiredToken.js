b06dfcdc0a5317cdaaa88a2ffe67b5eae7a86f04
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-27 15:56:23 +0530

    added jwt token validation file updations

--- node-backend/app/jwtTokenValidation/deleteExpiredToken.js
+++ node-backend/app/jwtTokenValidation/deleteExpiredToken.js
@@ -4,7 +4,7 @@ var cron = require("node-cron");
 const db = require("../models/sequelize");
 let epoch = moment().valueOf();
 epoch = (epoch - (epoch % 1000)) / 1000;
-cron.schedule("* * * * *", () => {
+cron.schedule(process.env.TOKEN_DELETION_INTERVAL, () => {
   db.sequelize.query(
     "delete from user_blacklisted_tokens where expiry_date < '" + epoch + "'"
   );
