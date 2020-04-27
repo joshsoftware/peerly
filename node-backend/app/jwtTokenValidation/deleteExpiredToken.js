08f7cd97a0fe1df4ed21bca79092731531c07496
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-27 19:40:10 +0530

    added moment utc time

--- node-backend/app/jwtTokenValidation/deleteExpiredToken.js
+++ node-backend/app/jwtTokenValidation/deleteExpiredToken.js
@@ -3,9 +3,9 @@ var cron = require("node-cron");
 
 const db = require("../models/sequelize");
 cron.schedule(process.env.TOKEN_DELETION_INTERVAL, () => {
-  let epoch = moment().valueOf();
-  epoch = (epoch - (epoch % 1000)) / 1000;
   db.sequelize.query(
-    "delete from user_blacklisted_tokens where expiry_date < '" + epoch + "'"
+    "delete from user_blacklisted_tokens where expiry_date < '" +
+      moment.utc().unix() +
+      "'"
   );
 });
