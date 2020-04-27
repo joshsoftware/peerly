8afd349a7e045ae0911f32478b0f566ceaceb708
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-27 16:57:26 +0530

    added updated status codes

--- node-backend/app/jwtTokenValidation/jwtTokenValidation.js
+++ node-backend/app/jwtTokenValidation/jwtTokenValidation.js
@@ -3,15 +3,15 @@ module.exports.autheticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (token === null)
-    res.status(401).send({
+    res.status(412).send({
       error: {
         message: "invalid token",
       },
     });
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err) => {
     if (err) {
-      res.status(401).send({
-        message: "Not a valid token",
+      res.status(412).send({
+        message: "invalid token",
       });
     } else {
       next();
