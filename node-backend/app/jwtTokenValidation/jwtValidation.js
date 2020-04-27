135c13f977ba92d0ca0ddbb234727d43a844b88c
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-04-27 19:21:10 +0530

    change a token get data function and update autheticateToken function

--- node-backend/app/jwtTokenValidation/jwtValidation.js
+++ node-backend/app/jwtTokenValidation/jwtValidation.js
@@ -6,39 +6,44 @@ const UserBlacklistedTokens = db.userBlacklistedTokens;
 module.exports.autheticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (token === null)
-    res.status(401).send({
+  if (token == null /*eslint-disable-line no-eq-null*/) {
+    res.status(412).send({
       error: {
         message: "unauthorised user",
       },
     });
-  jwt.verify(token, process.env.JWT_SECRET_KEY, (err) => {
-    if (err) {
-      res.status(401).send({
-        message: "unauthorised user",
-      });
-    } else {
-      UserBlacklistedTokens.findOne({ where: { token: token } }).then(
-        (data) => {
-          if (data === null) {
-            next();
-          } else {
-            res.status(401).send({
-              message: "unauthorised user",
-            });
+  } else {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err) => {
+      if (err) {
+        res.status(401).send({
+          error: {
+            message: "unauthorised user",
+          },
+        });
+      } else {
+        UserBlacklistedTokens.findOne({ where: { token: token } }).then(
+          (data) => {
+            if (data === null) {
+              next();
+            } else {
+              res.status(401).send({
+                error: {
+                  message: "unauthorised user",
+                },
+              });
+            }
           }
-        }
-      );
-    }
-  });
+        );
+      }
+    });
+  }
 };
 
-module.exports.getData = (req, res /*eslint-disable-line no-unused-vars*/) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+module.exports.getData = (token) => {
   let decode = jwt.decode(token);
   const tokenData = {
     userId: decode.sub,
+    exp: decode.exp,
     roleId: decode["https://peerly.com"].roleId,
     orgId: decode["https://peerly.com"].orgId,
   };
