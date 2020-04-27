1bf33d5f9c61e0fa37175aee7b9b261472452f66
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-27 15:59:37 +0530

    added epoch function for execution after time

--- node-backend/app/jwtTokenValidation/deleteExpiredToken.js
+++ node-backend/app/jwtTokenValidation/deleteExpiredToken.js
@@ -2,9 +2,9 @@ const moment = require("moment");
 var cron = require("node-cron");
 
 const db = require("../models/sequelize");
-let epoch = moment().valueOf();
-epoch = (epoch - (epoch % 1000)) / 1000;
 cron.schedule(process.env.TOKEN_DELETION_INTERVAL, () => {
+  let epoch = moment().valueOf();
+  epoch = (epoch - (epoch % 1000)) / 1000;
   db.sequelize.query(
     "delete from user_blacklisted_tokens where expiry_date < '" + epoch + "'"
   );
