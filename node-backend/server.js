a5d60870016481ade1bae5a29834255ceb1d5bd6
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-27 16:49:21 +0530

    added updated server file

--- node-backend/server.js
+++ node-backend/server.js
@@ -5,11 +5,15 @@ const bodyParser = require("body-parser");
 require("dotenv").config("./.env");
 require("./app/jwtTokenValidation/deleteExpiredToken");
 const routes = require("./app/routes/session.routes");
+const routes1 = require("./app/routes/organisation.routes");
 const dbConn = require("./app/models/sequelize");
+const jwtValidate = require("./app/jwtTokenValidation/jwtValidation");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 dbConn.sequelize;
 app.use("/v1", routes);
+app.use(jwtValidate.autheticateToken);
+app.use("/v1", routes1);
 const httpServer = http.createServer(app);
 httpServer.listen(process.env.HTTP_PORT || 8080); // eslint-disable-line no-undef
