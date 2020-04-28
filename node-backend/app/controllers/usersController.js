537135697be61104f3b657749dcde1fb3a8a0f08
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-28 07:45:48 +0530

    updated function to send token

--- node-backend/app/controllers/usersController.js
+++ node-backend/app/controllers/usersController.js
@@ -1,7 +1,9 @@
 const db = require("../models/sequelize");
 const jwtToken = require("../jwtTokenValidation/jwtValidation");
 module.exports.findUsersByOrg = async (req, res) => {
-  let userData = await jwtToken.getData(req, res);
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  let userData = await jwtToken.getData(token);
   db.sequelize
     .query(
       "select users.id,users.name,users.email,users.display_name,users.profile_image_url,users.role_id,users.hi5_quota_balance from users, roles where org_id = '" +
