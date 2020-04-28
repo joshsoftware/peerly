ec544039315b091b7f540c5c4521dda474f57a9d
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-28 12:45:30 +0530

    added updated server.js file

--- node-backend/server.js
+++ node-backend/server.js
@@ -3,16 +3,17 @@ const http = require("http");
 const bodyParser = require("body-parser");
 
 require("dotenv").config("./.env");
-const routes = require("./app/routes/users.routes");
-const orgRoute = require("./app/routes/organisation.routes");
+require("./app/jwtTokenValidation/deleteExpiredToken.js"); // eslint-disable-line node/no-missing-require
+const routes = require("./app/routes/session.routes"); // eslint-disable-line node/no-missing-require
+const routes1 = require("./app/routes/organisation.routes");
 const dbConn = require("./app/models/sequelize");
 const jwtValidate = require("./app/jwtTokenValidation/jwtValidation");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 dbConn.sequelize;
-app.use("/v1", routes);
+app.use("/v1", routes); // eslint-disable-line no-undef
 app.use(jwtValidate.autheticateToken);
-app.use("/v1", orgRoute);
+app.use("/v1", routes1);
 const httpServer = http.createServer(app);
 httpServer.listen(process.env.HTTP_PORT || 8080); // eslint-disable-line no-undef
