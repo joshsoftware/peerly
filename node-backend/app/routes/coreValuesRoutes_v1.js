d73a3303dbc675c0c926140576ce1980ec11d55b
Avinash Mane <avimanepatil@gmail.com>
2020-04-29 18:59:24 +0530

    changes in authorizedRole function

--- node-backend/app/routes/coreValuesRoutes_v1.js
+++ node-backend/app/routes/coreValuesRoutes_v1.js
@@ -7,8 +7,7 @@ coreValueRouter.use(bodyParser.urlencoded({ extended: true }));
 
 async function authorizedRole(req, res, next) {
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  const tokenData = await jwtValidate.getData(token);
+  const tokenData = await jwtValidate.getData(authHeader);
   if (tokenData.roleId == 2) {
     next();
   } else {
