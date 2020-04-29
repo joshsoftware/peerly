1f4b808bb538ced8e491f543c37295752a0cd120
Avinash Mane <avimanepatil@gmail.com>
2020-04-29 16:33:42 +0530

    add authorizedRole function

--- node-backend/app/routes/badgesRoutes_v1.js
+++ node-backend/app/routes/badgesRoutes_v1.js
@@ -1,26 +1,47 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const BadgesController = require("../controllers/v1/badgesController");
+const jwtValidate = require("../jwtTokenValidation/jwtValidation");
 const badgesRouter = express.Router();
 badgesRouter.use(bodyParser.urlencoded({ extended: true }));
-//for create core value
+
+async function authorizedRole(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  const tokenData = await jwtValidate.getData(token);
+  if (tokenData.roleId == 1) {
+    next();
+  } else {
+    res.status(401).send({
+      error: {
+        message: "unauthorised user",
+      },
+    });
+  }
+}
+
+//for create badges
 badgesRouter.post(
   "/organisations/:organisation_id/badges",
+  authorizedRole,
   BadgesController.create
 );
-//for get all core values
+//for get all badges
 badgesRouter.get(
   "/organisations/:organisation_id/badges",
+  authorizedRole,
   BadgesController.findAll
 );
-//for get core value by id
+//for get badges by id
 badgesRouter.get(
   "/organisations/:organisation_id/badges/:id",
+  authorizedRole,
   BadgesController.findOne
 );
-//for update core value
+//for update badges
 badgesRouter.put(
   "/organisations/:organisation_id/badges/:id",
+  authorizedRole,
   BadgesController.update
 );
 
--- node-backend/app/routes/coreValuesRoutes_v1.js
+++ node-backend/app/routes/coreValuesRoutes_v1.js
@@ -1,26 +1,47 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const CorevalueController = require("../controllers/v1/CoreValueController");
+const jwtValidate = require("../jwtTokenValidation/jwtValidation");
 const coreValueRouter = express.Router();
 coreValueRouter.use(bodyParser.urlencoded({ extended: true }));
+
+async function authorizedRole(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  const tokenData = await jwtValidate.getData(token);
+  if (tokenData.roleId == 2) {
+    next();
+  } else {
+    res.status(401).send({
+      error: {
+        message: "unauthorised user",
+      },
+    });
+  }
+}
+
 //for create core value
 coreValueRouter.post(
   "/organisations/:organisation_id/core_values",
+  authorizedRole,
   CorevalueController.create
 );
 //for get all core values
 coreValueRouter.get(
   "/organisations/:organisation_id/core_values",
+  authorizedRole,
   CorevalueController.findAll
 );
 //for get core value by id
 coreValueRouter.get(
   "/organisations/:organisation_id/core_values/:id",
+  authorizedRole,
   CorevalueController.findOne
 );
 //for update core value
 coreValueRouter.put(
   "/organisations/:organisation_id/core_values/:id",
+  authorizedRole,
   CorevalueController.update
 );
 
