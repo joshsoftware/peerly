c40d4bcad6c54d3e6e8c5a69eff50942337f0f4c
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-30 17:51:20 +0530

    added new tests for logout

--- node-backend/app/test/logout.test.js
+++ node-backend/app/test/logout.test.js
@@ -2,6 +2,7 @@
 const supertest = require("supertest"); //eslint-disable-line node/no-unpublished-require
 const should = require("should"); //eslint-disable-line node/no-unpublished-require
 const server = supertest.agent(process.env.TEST_URL);
+const token = process.env.TOKEN;
 /*eslint-disable  no-unused-vars */
 /*eslint-disable  no-undef*/
 describe("test cases for logout", function () {
@@ -20,7 +21,7 @@ describe("test cases for logout", function () {
   it("should give ok status", function (done) {
     server
       .post("/logout")
-      .set("Authorization", "Bearer " + "")
+      .set("Authorization", "Bearer " + token)
       .expect("Content-type", /json/)
       .expect(200)
       .end(function (err, res) {
@@ -31,7 +32,7 @@ describe("test cases for logout", function () {
   it("should give internal server error", function (done) {
     server
       .post("/logout")
-      .set("Authorization", "Bearer " + "")
+      .set("Authorization", "Bearer " + token)
       .expect("Content-type", /json/)
       .expect(500)
       .end(function (err, res) {
@@ -43,7 +44,7 @@ describe("test cases for logout", function () {
   it("should unauthorized user", function (done) {
     server
       .post("/logout")
-      .set("Authorization", "Bearer " + "")
+      .set("Authorization", "Bearer " + "xxxx")
       .expect("Content-type", /json/)
       .expect(401)
       .end(function (err, res) {
