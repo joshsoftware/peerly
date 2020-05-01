33c3cf2b30c404a0c81b35b1c950e7f6fbb5201c
J. Austin Hughey <jah@jah.io>
2020-05-01 15:15:09 -0600

    Mock interfaces for db.GetUserByEmail and db.GetUserByID

--- go-backend/db/mock.go
+++ go-backend/db/mock.go
@@ -2,7 +2,9 @@ package db
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/bxcodec/faker/v3"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -24,5 +26,27 @@ func (m *MockStore) CleanBlacklistedTokens() (err error) {
 
 // CreateUserBlacklistedToken - Mock for testing
 func (m *MockStore) CreateUserBlacklistedToken(ctx context.Context, blacklistedToken UserBlacklistedToken) (err error) {
+	return
+}
+
+// GetUserByEmail - Mock for retrieving a user. Just return a fake user object.
+func (m *MockStore) GetUserByEmail(ctx context.Context, email string) (user User, err error) {
+	user = User{}
+	err = faker.FakeData(&user)
+	if err != nil {
+		fmt.Printf("Error creating mock user for db.GetUserByEmail. %+v", user)
+	}
+
+	user.Email = email // Alter the user's email to be what was passed in
+	return
+}
+
+// CreateNewUser - Mock for database new user creation. Just returns a fake user object
+func (m *MockStore) CreateNewUser(ctx context.Context, u User) (newUser User, err error) {
+	newUser = User{}
+	err = faker.FakeData(&newUser)
+	if err != nil {
+		fmt.Printf("Error creating mock data for db.CreateNewUser. %+v", newUser)
+	}
 	return
 }
