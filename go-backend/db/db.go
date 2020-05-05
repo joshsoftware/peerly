bf2d582fd8b9e3991472ca3a5418ab2b4c5cba47
Rohit Joshi <rohit.joshi@joshsoftware.com>
2020-05-05 18:33:13 +0530

    Add recognition Hi5 API - Add Migrations - Add Handler function - Define routes

--- go-backend/db/db.go
+++ go-backend/db/db.go
@@ -11,6 +11,7 @@ type Storer interface {
 	CreateOrganization(context.Context, Organization) (Organization, error)
 	DeleteOrganization(context.Context, int) error
 	UpdateOrganization(context.Context, Organization, int) (Organization, error)
+	CreateRecognitionHi5(context.Context, RecognitionHi5)(error)
 	//Create(context.Context, User) error
 	//GetUser(context.Context) (User, error)
 	//Delete(context.Context, string) error
