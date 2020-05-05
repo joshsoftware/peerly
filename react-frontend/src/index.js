d2689c4aa5d04b62a382042ae052c1aaebc966b7
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-05 17:03:20 +0530

    added recognition file in indexjs

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import { store } from "redux-store";
-import App from "./App";
+import CreateRecognitionComponent from "./createRecognition/CreateRecognitionComponent";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <CreateRecognitionComponent />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
