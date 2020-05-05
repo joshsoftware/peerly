475e8a5db4dd79c0bc864ba97470c9e73db39e1e
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-05-05 18:10:01 +0530

    add Peerly Label Core-Component

---
+++ react-frontend/src/core-components/PeerlyLabelComponent.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+import PropTypes from "prop-types";
+
+const PeerlyLabelComponent = (props) => {
+  const { text, className } = props;
+  return <Form.Label className={className} defaultValue={text} />;
+};
+
+PeerlyLabelComponent.propTypes = {
+  className: PropTypes.string,
+  text: PropTypes.string,
+};
+
+PeerlyLabelComponent.defaultProps = {
+  text: "Peerly Label",
+};
+
+export default PeerlyLabelComponent;
