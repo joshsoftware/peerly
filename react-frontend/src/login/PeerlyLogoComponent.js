d2cee02f7650963b807fff044d59a9a29800ed7f
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-05-05 18:58:39 +0530

    Add Peerly Logo Common Sub Component

---
+++ react-frontend/src/login/PeerlyLogoComponent.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+
+import PeerlyLabelComponent from "../core-components/PeerlyLabelComponent";
+
+const PeerlyLogoComponent = (props /*eslint-disable-line no-unused-vars*/) => {
+  return (
+    <Row>
+      <Col md="12">
+        <h1>
+          <center>
+            <PeerlyLabelComponent
+              className="text-info font-weight-bold text-center"
+              text="Peerly"
+            />
+          </center>
+        </h1>
+      </Col>
+    </Row>
+  );
+};
+
+PeerlyLogoComponent.defaultProps = {
+  alt: "Peerly Image",
+};
+
+export default PeerlyLogoComponent;
