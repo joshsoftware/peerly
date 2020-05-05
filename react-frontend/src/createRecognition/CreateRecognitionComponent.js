772298080206c25368a6bca408885e37bbc1d053
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-05 19:24:01 +0530

    added label in create recognition component

--- react-frontend/src/createRecognition/CreateRecognitionComponent.js
+++ react-frontend/src/createRecognition/CreateRecognitionComponent.js
@@ -2,15 +2,24 @@ import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
 
 import CreateRecognitionCard from "./CreateRecognitionCard";
+import PeerlyLabelComponent from "../coreComponents/PeerlyLabelComponent";
 const CreateRecognitionComponent = () => {
   return (
     <Container>
       <Row>
+        <Col xs={4}></Col>
+        <Col xs={4}>
+          <PeerlyLabelComponent labelName="Peerly" />
+        </Col>
+        <Col xs={4}></Col>
+      </Row>
+      <Row>
+        <Col xs={2}></Col>
         <Col xs={3}></Col>
-        <Col xs={6}>
+        <Col xs={5}>
           <CreateRecognitionCard />
         </Col>
-        <Col xs={3}></Col>
+        <Col xs={2}></Col>
       </Row>
     </Container>
   );
