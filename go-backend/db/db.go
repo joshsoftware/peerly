e3c68f22c54ef43ecaaac8b9875c5aad4c2bc450
Rohit Joshi <rohit.joshi@joshsoftware.com>
2020-05-06 18:34:08 +0530

    Add CreateRecognitionHi5 method for creating Hi5 - Add foreign keys for given_by and recognition_id - Add Hi5 quota balance check validation

--- go-backend/db/db.go
+++ go-backend/db/db.go
@@ -11,7 +11,8 @@ type Storer interface {
 	CreateOrganization(context.Context, Organization) (Organization, error)
 	DeleteOrganization(context.Context, int) error
 	UpdateOrganization(context.Context, Organization, int) (Organization, error)
-	CreateRecognitionHi5(context.Context, RecognitionHi5)(error)
+	CreateRecognitionHi5(context.Context, RecognitionHi5, int)(error)
+	CheckHi5QuotaBalance(RecognitionHi5)(map[string]ErrorResponse, bool)
 	//Create(context.Context, User) error
 	//GetUser(context.Context) (User, error)
 	//Delete(context.Context, string) error
--- go-backend/db/recognition_hi5.go
+++ go-backend/db/recognition_hi5.go
@@ -2,7 +2,8 @@ package db
 
 import (
 	"context"
-  "fmt"
+	logger "github.com/sirupsen/logrus"
+	"time"
 )
 
 const (
@@ -12,6 +13,8 @@ const (
 		given_by,
 		given_at
 		) VALUES ($1, $2, $3, $4);`
+
+	selectHi5QuotaQuery = `SELECT hi5_quota_balance FROM users WHERE id = $1;`
 )
 
 type RecognitionHi5 struct {
@@ -22,8 +25,40 @@ type RecognitionHi5 struct {
 	GivenAt int64 `db:"given_at" json:"given_at"`
 }
 
-func (s *pgStore)CreateRecognitionHi5(ctx context.Context, reqHi5 RecognitionHi5)(err error){
-  // TODO
-  fmt.Println(reqHi5, "Object")
+func (s *pgStore) CheckHi5QuotaBalance(reqHi5 RecognitionHi5)(errorResponse map[string]ErrorResponse, valid bool){
+	var hi5QuotaBalance int
+	err := s.db.Get(&hi5QuotaBalance, selectHi5QuotaQuery, reqHi5.GivenBy)
+	if err != nil {
+		logger.WithField("err",err.Error()).Error("Error querying Hi5 quota balance")
+		return
+	}
+
+	if hi5QuotaBalance <= 0 {
+		logger.Error("Not enough Hi5 quota balance")
+		errorResponse = map[string]ErrorResponse {
+			"error": ErrorResponse {
+				Code: "insufficient_hi5_quota_balance",
+				Message: "Insufficient Hi5 quota balance.",
+			},
+		}
+		valid = false
+		return
+	}
+	valid = true
+	return
+}
+
+func (s *pgStore) CreateRecognitionHi5(ctx context.Context, reqHi5 RecognitionHi5, recognitionId int)(err error){
+	_, err = s.db.Exec(
+		createRecognitionHi5Query,
+		recognitionId,
+		reqHi5.Comment,
+		reqHi5.GivenBy,
+		time.Now().Unix(),
+	)
+	if err != nil {
+		logger.WithField("err",err.Error()).Error("Error creating recognition Hi5")
+		return
+	}
   return
 }
