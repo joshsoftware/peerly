c27621de11b18d01c021ad2f6b8719b849305737
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-06 17:38:43 +0530

    removed row and columns in component

--- react-frontend/src/createRecognition/CreateRecognitionComponent.js
+++ react-frontend/src/createRecognition/CreateRecognitionComponent.js
@@ -1,18 +1,6 @@
 import React from "react";
-import { Row, Col, Container } from "react-bootstrap";
 
 import CreateRecognitionCard from "./CreateRecognitionCard";
-const CreateRecognitionComponent = () => (
-  <Container>
-    <Row>
-      <Col xs={2}></Col>
-      <Col xs={3}></Col>
-      <Col xs={5}>
-        <CreateRecognitionCard />
-      </Col>
-      <Col xs={2}></Col>
-    </Row>
-  </Container>
-);
+const CreateRecognitionComponent = () => <CreateRecognitionCard />;
 
 export default CreateRecognitionComponent;
