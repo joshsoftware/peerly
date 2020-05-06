7165d146ca86084f62697d57804209317ecf6af3
Rohit Joshi <rohit.joshi@joshsoftware.com>
2020-05-06 18:34:08 +0530

    Add CreateRecognitionHi5 method for creating Hi5 - Add foreign keys for given_by and recognition_id - Add Hi5 quota balance check validation

--- go-backend/service/recognition_hi5_http.go
+++ go-backend/service/recognition_hi5_http.go
@@ -4,23 +4,46 @@ import (
 	"encoding/json"
 	"joshsoftware/peerly/db"
 	"net/http"
+	"strconv"
+	"github.com/gorilla/mux"
 	logger "github.com/sirupsen/logrus"
 )
 
 func createRecognitionHi5Handler(deps Dependencies)(http.HandlerFunc){
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request){
-		var recognitionHi5 db.RecognitionHi5
+		vars := mux.Vars(req)
+		recognitionId, err := strconv.Atoi(vars["recognition_id"])
+		if err != nil {
+			logger.WithField("err", err.Error()).Error("Error recognition_id key is missing")
+			rw.WriteHeader(http.StatusBadRequest)
+			return
+		}
 
-		err := json.NewDecoder(req.Body).Decode(&recognitionHi5)
+		var recognitionHi5 db.RecognitionHi5
+		err = json.NewDecoder(req.Body).Decode(&recognitionHi5)
 		if err != nil {
 			logger.WithField("err", err.Error()).Error("Error Decoding recognitionHi5 data")
 			rw.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
-		err = deps.Store.CreateRecognitionHi5(req.Context(), recognitionHi5)
+		errorResponse, valid := deps.Store.CheckHi5QuotaBalance(recognitionHi5)
+		if !valid {
+			respBytes, err := json.Marshal(errorResponse)
+			if err != nil {
+				logger.WithField("err", err.Error()).Error("Error marshaling organizations data")
+				rw.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			rw.WriteHeader(http.StatusBadRequest)
+			rw.Write(respBytes)
+			return
+		}
+		// TODO
+		// Update Hi5 quota balance on successful creation of Hi5 (subtract 1)
+		err = deps.Store.CreateRecognitionHi5(req.Context(), recognitionHi5, recognitionId)
 		if err != nil {
-			logger.WithField("err", err.Error()).Error("Error in creating recogntion hi5")
+			logger.WithField("err", err.Error()).Error("Error in creating recognition hi5")
 			rw.WriteHeader(http.StatusBadRequest)
 			return
 		}
