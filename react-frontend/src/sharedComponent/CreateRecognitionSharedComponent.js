a7f8ba3c828ceaad98fb38cdf718f7c16e41dce7
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-06 17:39:21 +0530

    created shared component for create recognition

---
+++ react-frontend/src/sharedComponent/CreateRecognitionSharedComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+const CreateRecognitionSharedComponent = () => (
+  <Card>
+    <Card.Header></Card.Header>
+    <Card.Body></Card.Body>
+  </Card>
+);
+
+export default CreateRecognitionSharedComponent;
