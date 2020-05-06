ccb8c0f21698eb0de54783a4e296c416ba3b2b7b
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-06 14:40:28 +0530

    added required fields to label components

--- react-frontend/src/coreComponents/PeerlyLabelComponent.js
+++ react-frontend/src/coreComponents/PeerlyLabelComponent.js
@@ -2,17 +2,12 @@ import React from "react";
 import { Form } from "react-bootstrap";
 import PropTypes from "prop-types";
 
-const PeerlyLabelComponent = (props) => {
-  const { labelName, labelClassName } = props;
-  return <Form.Label className={labelClassName}>{labelName}</Form.Label>;
-};
-
-PeerlyLabelComponent.defaultProps = {
-  labelName: "No Label",
-};
+const PeerlyLabelComponent = ({ labelName, labelClassName }) => (
+  <Form.Label className={labelClassName}>{labelName}</Form.Label>
+);
 
 PeerlyLabelComponent.propTypes = {
-  labelName: PropTypes.string,
+  labelName: PropTypes.string.isRequired,
   labelClassName: PropTypes.string,
 };
 
