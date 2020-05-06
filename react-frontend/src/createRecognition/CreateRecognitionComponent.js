57f6677107c8d19c36cd505c18fb78d40cffd09e
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-06 14:42:21 +0530

    removed row from component

--- react-frontend/src/createRecognition/CreateRecognitionComponent.js
+++ react-frontend/src/createRecognition/CreateRecognitionComponent.js
@@ -2,27 +2,17 @@ import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
 
 import CreateRecognitionCard from "./CreateRecognitionCard";
-import PeerlyLabelComponent from "../coreComponents/PeerlyLabelComponent";
-const CreateRecognitionComponent = () => {
-  return (
-    <Container>
-      <Row>
-        <Col xs={4}></Col>
-        <Col xs={4}>
-          <PeerlyLabelComponent labelName="Peerly" />
-        </Col>
-        <Col xs={4}></Col>
-      </Row>
-      <Row>
-        <Col xs={2}></Col>
-        <Col xs={3}></Col>
-        <Col xs={5}>
-          <CreateRecognitionCard />
-        </Col>
-        <Col xs={2}></Col>
-      </Row>
-    </Container>
-  );
-};
+const CreateRecognitionComponent = () => (
+  <Container>
+    <Row>
+      <Col xs={2}></Col>
+      <Col xs={3}></Col>
+      <Col xs={5}>
+        <CreateRecognitionCard />
+      </Col>
+      <Col xs={2}></Col>
+    </Row>
+  </Container>
+);
 
 export default CreateRecognitionComponent;
