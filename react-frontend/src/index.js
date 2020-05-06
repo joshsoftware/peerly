7bb6c8440c80c5cd11c108efb2087594891f273a
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-06 22:24:54 +0530

    removed unnecessary imports from index file

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import { store } from "redux-store";
-import CreateRecognitionComponent from "./createRecognition/CreateRecognitionComponent";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <CreateRecognitionComponent />
-    </Provider>
+    <Provider store={store}> </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
