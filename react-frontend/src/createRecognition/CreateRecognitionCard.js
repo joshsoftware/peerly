baa57b9c3345e49dacf9be071eeffcae898f7d81
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-07 13:37:12 +0530

    updated card

--- react-frontend/src/createRecognition/CreateRecognitionCard.js
+++ react-frontend/src/createRecognition/CreateRecognitionCard.js
@@ -1,17 +1,58 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import PropTypes from "prop-types";
 
-import CreateRecognitionCardHeader from "./CreateRecognitionCardHeader";
-import CreateRecognitionCardBody from "./CreateRecognitionCardBody";
-const CreateRecognitionCard = () => (
-  <Card>
-    <Card.Header>
-      <CreateRecognitionCardHeader />
-    </Card.Header>
-    <Card.Body>
-      <CreateRecognitionCardBody />
-    </Card.Body>
-  </Card>
-);
+import CreateRecognitionCardHeader from "createRecognition/CreateRecognitionCardHeader";
+import CreateRecognitionCardBody from "createRecognition/CreateRecognitionCardBody";
+
+const CreateRecognitionCard = ({ cardHeader, cardBody }) => {
+  return (
+    <Card>
+      <Card.Header>
+        <CreateRecognitionCardHeader
+          labelName={cardHeader.labelName}
+          src={cardHeader.src}
+          imgClassName={cardHeader.imgClassName}
+          labelClassName={cardHeader.labelClassName}
+        />
+      </Card.Header>
+      <Card.Body>
+        <CreateRecognitionCardBody
+          coreValues={cardBody.coreValues}
+          labelName={cardBody.labelName}
+          labelClassName={cardBody.labelName}
+          buttonclassName={cardBody.buttonclassName}
+          type={cardBody.type}
+          variant={cardBody.variant}
+          size={cardBody.size}
+        />
+      </Card.Body>
+    </Card>
+  );
+};
+
+CreateRecognitionCard.propTypes = {
+  cardHeader: PropTypes.shape({
+    labelName: PropTypes.string.isRequired,
+    src: PropTypes.string.isRequired,
+    imgClassName: PropTypes.string,
+    labelClassName: PropTypes.string,
+  }),
+
+  cardBody: PropTypes.shape({
+    coreValues: PropTypes.arrayOf(
+      PropTypes.shape({
+        labelName: PropTypes.string.isRequired,
+        value: PropTypes.string.isRequired,
+      })
+    ),
+    labelName: PropTypes.string.isRequired,
+    labelClassName: PropTypes.string,
+    buttonclassName: PropTypes.string,
+    type: PropTypes.string,
+    variant: PropTypes.string,
+    size: PropTypes.oneOf(["sm", "lg"]),
+  }),
+};
 
 export default CreateRecognitionCard;
