e0c168e393fe027cd13d57b39ca0164b03d52d69
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-07 23:32:52 +0530

    updated card body of create recognition file

--- react-frontend/src/createRecognition/CreateRecognitionCardBody.js
+++ react-frontend/src/createRecognition/CreateRecognitionCardBody.js
@@ -3,33 +3,37 @@ import PropTypes from "prop-types";
 
 import LabelCoreComponent from "coreComponents/LabelCoreComponent";
 import ButtonCoreComponent from "coreComponents/ButtonCoreComponent";
-import CoreValuesComponent from "./CoreValuesComponent";
+import CoreValuesComponent from "createRecognition/CoreValuesComponent";
 const CreateRecognitionCardBody = (props) => {
   const {
     coreValues,
+    buttonNameText,
     labelName,
     labelClassName,
     buttonclassName,
     type,
     variant,
     size,
+    value,
   } = props;
-
+  const onClick = () => {}; //eslint-disable-line no-empty-function
   return (
     <>
       <LabelCoreComponent labelName={labelName} ClassName={labelClassName} />
       <CoreValuesComponent coreValues={coreValues} />
       <ButtonCoreComponent
-        onClick={""}
-        value="add comments"
+        onClick={onClick}
+        buttonNameText={buttonNameText}
+        value={value}
         buttonclassName={buttonclassName}
         type={type}
         variant={variant}
         size={size}
       />
       <ButtonCoreComponent
-        onClick={""}
-        value="Done"
+        onClick={onClick}
+        buttonNameText={buttonNameText}
+        value={value}
         buttonclassName={buttonclassName}
         type={type}
         variant={variant}
@@ -44,15 +48,15 @@ CreateRecognitionCardBody.propTypes = {
   coreValues: PropTypes.arrayOf(
     PropTypes.shape({
       labelName: PropTypes.string.isRequired,
-      value: PropTypes.string.isRequired,
     })
   ),
   variant: PropTypes.string,
   size: PropTypes.oneOf(["sm", "lg"]),
   buttonclassName: PropTypes.string,
-  onClick: PropTypes.func.isRequired,
   labelName: PropTypes.string.isRequired,
   labelClassName: PropTypes.string,
+  buttonNameText: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
 };
 
 export default CreateRecognitionCardBody;
