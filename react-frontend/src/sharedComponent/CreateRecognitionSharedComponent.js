c8219c2bd103891cdb1d18e210b236a89e289a96
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-07 23:39:32 +0530

    created file for recognition component which will wrap shared card

--- react-frontend/src/sharedComponent/CreateRecognitionSharedComponent.js
+++ react-frontend/src/sharedComponent/CreateRecognitionSharedComponent.js
@@ -2,51 +2,52 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import PropTypes from "prop-types";
 
-import ButtonCoreComponent from "../coreComponents/ButtonCoreComponent";
-import ImageCoreComponent from "../coreComponents/ImageCoreComponent";
-import LabelCoreComponent from "../coreComponents/LabelCoreComponent";
-const CreateRecognitionSharedComponent = (props) => {
-  const {
-    src,
-    imgClassName,
-    buttonclassName,
-    type,
-    value,
-    variant,
-    size,
-    labelName,
-    labelClassName,
-  } = props;
-
-  return (
-    <Card>
-      <Card.Header>
-        <ImageCoreComponent src={src} className={imgClassName} />
-        <LabelCoreComponent labelName={labelName} className={labelClassName} />
-      </Card.Header>
-      <ButtonCoreComponent
-        className={buttonclassName}
-        type={type}
-        value={value}
-        variant={variant}
-        size={size}
+import CreateRecognitionSharedCardHeader from "sharedComponent/CreateRecognitionSharedCardHeader";
+import CreateRecognitionSharedCardBody from "sharedComponent/CreateRecognitionSharedCardBody";
+const CreateRecognitionSharedComponent = ({
+  createRecognitionSharedComponent,
+}) => (
+  <Card>
+    <Card.Header>
+      <CreateRecognitionSharedCardHeader
+        src={createRecognitionSharedComponent.src}
+        imgClassName={createRecognitionSharedComponent.imgClassName}
+        labelName={createRecognitionSharedComponent.labelName}
+        labelClassName={createRecognitionSharedComponent.labelClassName}
+        alt={createRecognitionSharedComponent.alt}
+        height={createRecognitionSharedComponent.height}
+        width={createRecognitionSharedComponent.width}
       />
-      <Card.Body></Card.Body>
-    </Card>
-  );
-};
+    </Card.Header>
+    <Card.Body>
+      <CreateRecognitionSharedCardBody
+        className={createRecognitionSharedComponent.buttonclassName}
+        type={createRecognitionSharedComponent.type}
+        value={createRecognitionSharedComponent.value}
+        variant={createRecognitionSharedComponent.variant}
+        size={createRecognitionSharedComponent.size}
+        buttonNameText={createRecognitionSharedComponent.buttonNameText}
+      />
+    </Card.Body>
+  </Card>
+);
 
 CreateRecognitionSharedComponent.propTypes = {
-  src: PropTypes.string.isRequired,
-  imgClassName: PropTypes.string,
-  type: PropTypes.string,
-  value: PropTypes.string,
-  variant: PropTypes.string,
-  size: PropTypes.oneOf(["sm", "lg"]),
-  buttonclassName: PropTypes.string,
-  onClick: PropTypes.func.isRequired,
-  labelName: PropTypes.string.isRequired,
-  labelClassName: PropTypes.string,
+  createRecognitionSharedComponent: PropTypes.shape({
+    src: PropTypes.string.isRequired,
+    imgClassName: PropTypes.string,
+    type: PropTypes.string,
+    value: PropTypes.string,
+    variant: PropTypes.string,
+    size: PropTypes.oneOf(["sm", "lg"]),
+    buttonclassName: PropTypes.string,
+    labelName: PropTypes.string.isRequired,
+    labelClassName: PropTypes.string,
+    alt: PropTypes.string,
+    height: PropTypes.string,
+    width: PropTypes.string,
+    buttonNameText: PropTypes.string.isRequired,
+  }),
 };
 
 export default CreateRecognitionSharedComponent;
