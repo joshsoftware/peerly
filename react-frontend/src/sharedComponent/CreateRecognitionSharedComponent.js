790e9b1af93608a5ce837ab78220efcd2b2d896a
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-07 13:40:44 +0530

    updated file with create recognition share comonent file

--- react-frontend/src/sharedComponent/CreateRecognitionSharedComponent.js
+++ react-frontend/src/sharedComponent/CreateRecognitionSharedComponent.js
@@ -1,11 +1,52 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import PropTypes from "prop-types";
 
-const CreateRecognitionSharedComponent = () => (
-  <Card>
-    <Card.Header></Card.Header>
-    <Card.Body></Card.Body>
-  </Card>
-);
+import ButtonCoreComponent from "../coreComponents/ButtonCoreComponent";
+import ImageCoreComponent from "../coreComponents/ImageCoreComponent";
+import LabelCoreComponent from "../coreComponents/LabelCoreComponent";
+const CreateRecognitionSharedComponent = (props) => {
+  const {
+    src,
+    imgClassName,
+    buttonclassName,
+    type,
+    value,
+    variant,
+    size,
+    labelName,
+    labelClassName,
+  } = props;
+
+  return (
+    <Card>
+      <Card.Header>
+        <ImageCoreComponent src={src} className={imgClassName} />
+        <LabelCoreComponent labelName={labelName} className={labelClassName} />
+      </Card.Header>
+      <ButtonCoreComponent
+        className={buttonclassName}
+        type={type}
+        value={value}
+        variant={variant}
+        size={size}
+      />
+      <Card.Body></Card.Body>
+    </Card>
+  );
+};
+
+CreateRecognitionSharedComponent.propTypes = {
+  src: PropTypes.string.isRequired,
+  imgClassName: PropTypes.string,
+  type: PropTypes.string,
+  value: PropTypes.string,
+  variant: PropTypes.string,
+  size: PropTypes.oneOf(["sm", "lg"]),
+  buttonclassName: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+  labelName: PropTypes.string.isRequired,
+  labelClassName: PropTypes.string,
+};
 
 export default CreateRecognitionSharedComponent;
