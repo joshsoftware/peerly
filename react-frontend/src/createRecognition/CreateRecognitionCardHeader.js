0b71946cc6e7fb588118a0bcbbb34651bccd3f90
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-07 13:38:24 +0530

    updated card header with changes

--- react-frontend/src/createRecognition/CreateRecognitionCardHeader.js
+++ react-frontend/src/createRecognition/CreateRecognitionCardHeader.js
@@ -1,8 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import ImageCoreComponent from "../coreComponents/ImageCoreComponent";
-import LabelCoreComponent from "../coreComponents/LabelCoreComponent";
+import ImageCoreComponent from "coreComponents/ImageCoreComponent";
+import LabelCoreComponent from "coreComponents/LabelCoreComponent";
 const CreateRecognitionCardHeader = ({
   labelName,
   src,
