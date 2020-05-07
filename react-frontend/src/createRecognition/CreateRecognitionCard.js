85a04d13418455cd6b79bf005a9d5bfccb5f8c19
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-07 23:32:02 +0530

    added props to the card for passing it to sub component

--- react-frontend/src/createRecognition/CreateRecognitionCard.js
+++ react-frontend/src/createRecognition/CreateRecognitionCard.js
@@ -5,31 +5,31 @@ import PropTypes from "prop-types";
 import CreateRecognitionCardHeader from "createRecognition/CreateRecognitionCardHeader";
 import CreateRecognitionCardBody from "createRecognition/CreateRecognitionCardBody";
 
-const CreateRecognitionCard = ({ cardHeader, cardBody }) => {
-  return (
-    <Card>
-      <Card.Header>
-        <CreateRecognitionCardHeader
-          labelName={cardHeader.labelName}
-          src={cardHeader.src}
-          imgClassName={cardHeader.imgClassName}
-          labelClassName={cardHeader.labelClassName}
-        />
-      </Card.Header>
-      <Card.Body>
-        <CreateRecognitionCardBody
-          coreValues={cardBody.coreValues}
-          labelName={cardBody.labelName}
-          labelClassName={cardBody.labelName}
-          buttonclassName={cardBody.buttonclassName}
-          type={cardBody.type}
-          variant={cardBody.variant}
-          size={cardBody.size}
-        />
-      </Card.Body>
-    </Card>
-  );
-};
+const CreateRecognitionCard = ({ cardHeader, cardBody }) => (
+  <Card>
+    <Card.Header>
+      <CreateRecognitionCardHeader
+        labelName={cardHeader.labelName}
+        src={cardHeader.src}
+        imgClassName={cardHeader.imgClassName}
+        labelClassName={cardHeader.labelClassName}
+      />
+    </Card.Header>
+    <Card.Body>
+      <CreateRecognitionCardBody
+        coreValues={cardBody.coreValues}
+        labelName={cardBody.labelName}
+        labelClassName={cardBody.labelName}
+        buttonclassName={cardBody.buttonclassName}
+        type={cardBody.type}
+        variant={cardBody.variant}
+        size={cardBody.size}
+        buttonNameText={cardBody.buttonNameText}
+        value={cardBody.value}
+      />
+    </Card.Body>
+  </Card>
+);
 
 CreateRecognitionCard.propTypes = {
   cardHeader: PropTypes.shape({
@@ -38,12 +38,10 @@ CreateRecognitionCard.propTypes = {
     imgClassName: PropTypes.string,
     labelClassName: PropTypes.string,
   }),
-
   cardBody: PropTypes.shape({
     coreValues: PropTypes.arrayOf(
       PropTypes.shape({
         labelName: PropTypes.string.isRequired,
-        value: PropTypes.string.isRequired,
       })
     ),
     labelName: PropTypes.string.isRequired,
@@ -52,6 +50,8 @@ CreateRecognitionCard.propTypes = {
     type: PropTypes.string,
     variant: PropTypes.string,
     size: PropTypes.oneOf(["sm", "lg"]),
+    buttonNameText: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
   }),
 };
 
