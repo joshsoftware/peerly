35f3092bbf959e2426580871b1e814879399b3fd
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-08 17:18:17 +0530

    updated core value component by adding icons

--- react-frontend/src/sharedComponent/CoreValueComponent.js
+++ react-frontend/src/sharedComponent/CoreValueComponent.js
@@ -2,12 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import LabelCoreComponent from "coreComponents/LabelCoreComponent";
-const CoreValueComponent = ({ labelName }) => (
-  <LabelCoreComponent labelName={labelName} />
+const CoreValueComponent = ({
+  labelName,
+  Icon,
+  labelClassName,
+  IconClassName,
+}) => (
+  <>
+    <Icon className={IconClassName} />
+    <LabelCoreComponent labelName={labelName} labelClassName={labelClassName} />
+  </>
 );
 
 CoreValueComponent.propTypes = {
   labelName: PropTypes.string.isRequired,
+  labelClassName: PropTypes.string,
+  IconClassName: PropTypes.string,
+  Icon: PropTypes.any.isRequired,
 };
 
 export default CoreValueComponent;
