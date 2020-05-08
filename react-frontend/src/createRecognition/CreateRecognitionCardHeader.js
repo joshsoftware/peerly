7fbfcfc419afbd26ed8260874daaf571270ee4aa
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-08 08:32:20 +0530

    updated proptype object

--- react-frontend/src/createRecognition/CreateRecognitionCardHeader.js
+++ react-frontend/src/createRecognition/CreateRecognitionCardHeader.js
@@ -2,36 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import EmployeeNameAndImageComponent from "sharedComponent/EmployeeNameAndImageComponent";
-const CreateRecognitionCardHeader = ({
-  src,
-  imgClassName,
-  labelName,
-  labelClassName,
-  alt,
-  height,
-  width,
-}) => {
-  return (
-    <EmployeeNameAndImageComponent
-      src={src}
-      imgClassName={imgClassName}
-      labelName={labelName}
-      labelClassName={labelClassName}
-      alt={alt}
-      height={height}
-      width={width}
-    />
-  );
-};
+const CreateRecognitionCardHeader = ({ cardHeader }) => (
+  <EmployeeNameAndImageComponent
+    src={cardHeader.src}
+    imgClassName={cardHeader.imgClassName}
+    labelName={cardHeader.labelName}
+    labelClassName={cardHeader.labelClassName}
+    alt={cardHeader.alt}
+    height={cardHeader.height}
+    width={cardHeader.width}
+  />
+);
 
 CreateRecognitionCardHeader.propTypes = {
-  labelName: PropTypes.string.isRequired,
-  src: PropTypes.string.isRequired,
-  imgClassName: PropTypes.string,
-  labelClassName: PropTypes.string,
-  alt: PropTypes.string,
-  height: PropTypes.string,
-  width: PropTypes.string,
+  cardHeader: PropTypes.shape({
+    labelName: PropTypes.string.isRequired,
+    src: PropTypes.string.isRequired,
+    imgClassName: PropTypes.string,
+    labelClassName: PropTypes.string,
+    alt: PropTypes.string,
+    height: PropTypes.string,
+    width: PropTypes.string,
+  }),
 };
 
 export default CreateRecognitionCardHeader;
