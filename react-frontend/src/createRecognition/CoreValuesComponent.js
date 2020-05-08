e356aff11cb87584e272171599c17e5f6cc91f4a
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-08 17:11:23 +0530

    updated core value with icon

--- react-frontend/src/createRecognition/CoreValuesComponent.js
+++ react-frontend/src/createRecognition/CoreValuesComponent.js
@@ -4,13 +4,22 @@ import PropTypes from "prop-types";
 import CoreValueComponent from "sharedComponent/CoreValueComponent";
 const CoreValuesComponent = ({ coreValues }) =>
   coreValues.map((coreValue, index) => (
-    <CoreValueComponent key={index} labelName={coreValue.labelName} />
+    <CoreValueComponent
+      key={index}
+      labelName={coreValue.labelName}
+      labelClassName={coreValue.labelClassName}
+      IconClassName={coreValue.IconClassName}
+      Icon={coreValue.Icon}
+    />
   ));
 
 CoreValuesComponent.propTypes = {
   coreValues: PropTypes.arrayOf(
     PropTypes.shape({
       labelName: PropTypes.string.isRequired,
+      labelClassName: PropTypes.string,
+      IconClassName: PropTypes.string,
+      Icon: PropTypes.any.isRequired,
     })
   ),
 };
