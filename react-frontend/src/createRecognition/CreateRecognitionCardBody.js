953bb46feb4d49723d9de090b82a9e0af23c3852
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-08 08:31:42 +0530

    changed prop objects

--- react-frontend/src/createRecognition/CreateRecognitionCardBody.js
+++ react-frontend/src/createRecognition/CreateRecognitionCardBody.js
@@ -4,59 +4,56 @@ import PropTypes from "prop-types";
 import LabelCoreComponent from "coreComponents/LabelCoreComponent";
 import ButtonCoreComponent from "coreComponents/ButtonCoreComponent";
 import CoreValuesComponent from "createRecognition/CoreValuesComponent";
-const CreateRecognitionCardBody = (props) => {
-  const {
-    coreValues,
-    buttonNameText,
-    labelName,
-    labelClassName,
-    buttonclassName,
-    type,
-    variant,
-    size,
-    value,
-  } = props;
+const CreateRecognitionCardBody = ({ cardBody }) => {
   const onClick = () => {}; //eslint-disable-line no-empty-function
+
   return (
     <>
-      <LabelCoreComponent labelName={labelName} ClassName={labelClassName} />
-      <CoreValuesComponent coreValues={coreValues} />
+      <LabelCoreComponent
+        labelName={cardBody.labelName}
+        ClassName={cardBody.labelClassName}
+      />
+      <CoreValuesComponent coreValues={cardBody.coreValues} />
       <ButtonCoreComponent
         onClick={onClick}
-        buttonNameText={buttonNameText}
-        value={value}
-        buttonclassName={buttonclassName}
-        type={type}
-        variant={variant}
-        size={size}
+        buttonNameText={cardBody.toggleButtonNameText}
+        value={cardBody.toggleButtonValue}
+        buttonclassName={cardBody.buttonclassName}
+        type={cardBody.type}
+        variant={cardBody.variant}
+        size={cardBody.size}
       />
       <ButtonCoreComponent
         onClick={onClick}
-        buttonNameText={buttonNameText}
-        value={value}
-        buttonclassName={buttonclassName}
-        type={type}
-        variant={variant}
-        size={size}
+        buttonNameText={cardBody.addCoreValueButtonNameText}
+        value={cardBody.addCoreValueButtonValue}
+        buttonclassName={cardBody.buttonclassName}
+        type={cardBody.type}
+        variant={cardBody.variant}
+        size={cardBody.size}
       />
     </>
   );
 };
 
 CreateRecognitionCardBody.propTypes = {
-  type: PropTypes.string,
-  coreValues: PropTypes.arrayOf(
-    PropTypes.shape({
-      labelName: PropTypes.string.isRequired,
-    })
-  ),
-  variant: PropTypes.string,
-  size: PropTypes.oneOf(["sm", "lg"]),
-  buttonclassName: PropTypes.string,
-  labelName: PropTypes.string.isRequired,
-  labelClassName: PropTypes.string,
-  buttonNameText: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
+  cardBody: PropTypes.shape({
+    type: PropTypes.string,
+    coreValues: PropTypes.arrayOf(
+      PropTypes.shape({
+        labelName: PropTypes.string.isRequired,
+      })
+    ),
+    variant: PropTypes.string,
+    size: PropTypes.oneOf(["sm", "lg"]),
+    buttonclassName: PropTypes.string,
+    labelName: PropTypes.string.isRequired,
+    labelClassName: PropTypes.string,
+    toggleButtonNameText: PropTypes.string.isRequired,
+    toggleButtonValue: PropTypes.string.isRequired,
+    addCoreValueButtonNameText: PropTypes.string.isRequired,
+    addCoreValueButtonValue: PropTypes.string.isRequired,
+  }),
 };
 
 export default CreateRecognitionCardBody;
