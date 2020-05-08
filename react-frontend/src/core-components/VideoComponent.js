959f6a3abd4593e1fe42c804dbed372ecb7816c1
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-05-08 15:42:45 +0530

    add some props in Video Core Component

--- react-frontend/src/core-components/VideoComponent.js
+++ react-frontend/src/core-components/VideoComponent.js
@@ -1,14 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const VideoComponent = ({ className, src, controls }) => (
-  <video className={className} src={src} controls={controls} />
+const VideoComponent = ({ className, src, controls, height, width }) => (
+  <video
+    className={className}
+    src={src}
+    controls={controls}
+    height={height}
+    width={width}
+  />
 );
 
 VideoComponent.propTypes = {
   className: PropTypes.string,
   src: PropTypes.string.isRequired,
   controls: PropTypes.oneOf(["controls"]),
+  height: PropTypes.number,
+  width: PropTypes.number,
+};
+
+VideoComponent.defaultProps = {
+  controls: "controls",
 };
 
 export default VideoComponent;
