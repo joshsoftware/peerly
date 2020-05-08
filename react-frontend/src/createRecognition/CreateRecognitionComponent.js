c6c1cf931cdfdf37602a783c53f700d1ac431c68
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-08 08:32:53 +0530

    added card as new prop object

--- react-frontend/src/createRecognition/CreateRecognitionComponent.js
+++ react-frontend/src/createRecognition/CreateRecognitionComponent.js
@@ -5,10 +5,7 @@ import CreateRecognitionCard from "createRecognition/CreateRecognitionCard";
 import CreateRecognitionSharedComponent from "sharedComponent/CreateRecognitionSharedComponent";
 const CreateRecognitionComponent = ({ createRecognitionComponent }) => (
   <>
-    <CreateRecognitionCard
-      cardBody={createRecognitionComponent.cardBody}
-      cardHeader={createRecognitionComponent.cardHeader}
-    />
+    <CreateRecognitionCard card={createRecognitionComponent.card} />
     <CreateRecognitionSharedComponent
       createRecognitionSharedComponent={
         createRecognitionComponent.createRecognitionSharedComponent
@@ -19,29 +16,33 @@ const CreateRecognitionComponent = ({ createRecognitionComponent }) => (
 
 CreateRecognitionComponent.propTypes = {
   createRecognitionComponent: PropTypes.shape({
-    cardBody: PropTypes.shape({
-      type: PropTypes.string,
-      coreValues: PropTypes.arrayOf(
-        PropTypes.shape({
-          labelName: PropTypes.string.isRequired,
-        })
-      ),
-      variant: PropTypes.string,
-      size: PropTypes.oneOf(["sm", "lg"]),
-      buttonclassName: PropTypes.string,
-      labelName: PropTypes.string.isRequired,
-      labelClassName: PropTypes.string,
-      buttonNameText: PropTypes.string.isRequired,
-      value: PropTypes.string.isRequired,
-    }),
-    cardHeader: PropTypes.shape({
-      labelName: PropTypes.string.isRequired,
-      src: PropTypes.string.isRequired,
-      imgClassName: PropTypes.string,
-      labelClassName: PropTypes.string,
-      alt: PropTypes.string,
-      height: PropTypes.string,
-      width: PropTypes.string,
+    card: PropTypes.shape({
+      cardBody: PropTypes.shape({
+        coreValues: PropTypes.arrayOf(
+          PropTypes.shape({
+            labelName: PropTypes.string.isRequired,
+          })
+        ),
+        type: PropTypes.string,
+        variant: PropTypes.string,
+        size: PropTypes.oneOf(["sm", "lg"]),
+        buttonclassName: PropTypes.string,
+        labelName: PropTypes.string.isRequired,
+        labelClassName: PropTypes.string,
+        toggleButtonNameText: PropTypes.string.isRequired,
+        toggleButtonValue: PropTypes.string.isRequired,
+        addCoreValueButtonNameText: PropTypes.string.isRequired,
+        addCoreValueButtonValue: PropTypes.string.isRequired,
+      }),
+      cardHeader: PropTypes.shape({
+        labelName: PropTypes.string.isRequired,
+        src: PropTypes.string.isRequired,
+        imgClassName: PropTypes.string,
+        labelClassName: PropTypes.string,
+        alt: PropTypes.string,
+        height: PropTypes.string,
+        width: PropTypes.string,
+      }),
     }),
     createRecognitionSharedComponent: PropTypes.shape({
       src: PropTypes.string.isRequired,
