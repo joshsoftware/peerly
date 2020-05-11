732cb0260518dec67003f8c87a32eff2e92dfc4e
Avinash Mane <avimanepatil@gmail.com>
2020-05-11 10:26:23 +0530

    change api response

--- node-backend/app/controllers/v1/profileController.js
+++ node-backend/app/controllers/v1/profileController.js
@@ -4,7 +4,19 @@ const Users = db.users;
 const jwtToken = require("../../jwtTokenValidation/jwtValidation");
 module.exports.getProfile = async (req, res) => {
   let userData = await jwtToken.getData(req.headers["authorization"]);
-  Users.findByPk(userData.userId)
+  Users.findByPk(userData.userId, {
+    attributes: [
+      "id",
+      "first_name",
+      "last_name",
+      "email",
+      "display_name",
+      "profile_image_url",
+      "role_id",
+      "hi5_quota_balance",
+      "org_id",
+    ],
+  })
     .then((profile) => {
       res.status(200).send({
         data: profile,
