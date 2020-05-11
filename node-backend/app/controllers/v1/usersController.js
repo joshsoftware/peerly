7e0e8c70dc1e0af62008576d8365098873d8e4f1
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-11 13:28:24 +0530

    updated with super admin id

--- node-backend/app/controllers/v1/usersController.js
+++ node-backend/app/controllers/v1/usersController.js
@@ -1,7 +1,7 @@
 const qs = require(/*eslint-disable  node/no-extraneous-require*/ "qs");
+
 const db = require("../../models/sequelize");
 const Users = db.users;
-const Roles = db.roles;
 const jwtToken = require("../../jwtTokenValidation/jwtValidation");
 module.exports.findUsersByOrg = async (req, res) => {
   const authHeader = req.headers["authorization"];
@@ -24,48 +24,42 @@ module.exports.findUsersByOrg = async (req, res) => {
       where: { org_id: obj.org_id },
       attributes: ["role_id"],
     });
-    if (adminRoleId.dataValues.role_id);
+    if (adminRoleId.dataValues.role_id == 1);
     {
-      let adminRole = await Roles.findOne({
-        where: { role_id: adminRoleId.dataValues.role_id },
-        attributes: ["name"],
-      });
-      if (adminRole == "Super Admin") {
-        Users.findAll({
-          where: { org_id: userData.orgId },
-          attributes: [
-            "id",
-            "first_name",
-            "last_name",
-            "email",
-            "display_name",
-            "profile_image_url",
-            "role_id",
-            "hi5_quota_balance",
-            "org_id",
-            "soft_delete_by",
-            "soft_delete_at",
-          ],
-          limit: limit,
-          offset: offset,
-        })
-          .then((users) => {
-            res.status(200).send({
-              data: users,
-            });
-          })
-          .catch(() => {
-            res.status(500).send({
-              error: {
-                message: "internal server error",
-              },
-            });
+      Users.findAll({
+        where: { org_id: userData.orgId },
+        attributes: [
+          "id",
+          "first_name",
+          "last_name",
+          "email",
+          "display_name",
+          "profile_image_url",
+          "role_id",
+          "hi5_quota_balance",
+          "org_id",
+          "soft_delete_by",
+          "soft_delete_at",
+        ],
+        limit: limit,
+        offset: offset,
+      })
+        .then((users) => {
+          res.status(200).send({
+            data: users,
           });
-      }
+        })
+        .catch(() => {
+          res.status(500).send({
+            error: {
+              message: "internal server error",
+            },
+          });
+        });
     }
   } else {
     Users.findAll({
-      where: { org_id: userData.orgId, role_id: 2 },
+      where: { org_id: userData.orgId, role_id: 3 },
       attributes: [
         "id",
         "first_name",
