d507f213bf04c22671731be21121c7dc695c5fc6
Avinash Mane <avimanepatil@gmail.com>
2020-05-11 13:30:13 +0530

    added get profile by id API

--- node-backend/app/routes/profileRoutes.js
+++ node-backend/app/routes/profileRoutes.js
@@ -5,12 +5,35 @@ const /*eslint-disable no-unused-vars*/ profileControllerV1 = require("../contro
 const utility = require("../utils/utility");
 const profileRouter = express.Router();
 profileRouter.use(bodyParser.urlencoded({ extended: true }));
+const jwtValidate = require("../jwtTokenValidation/jwtValidation");
 
-profileRouter.get("users/me/profile", async (req, res) => {
+async function authorizedRole(req, res, next) {
+  const tokenData = await jwtValidate.getData(req.headers["authorization"]);
+  if (tokenData.roleId == (1 || 20)) {
+    next();
+  } else {
+    res.status(401).send({
+      error: {
+        message: "unauthorised user",
+      },
+    });
+  }
+}
+
+profileRouter.get("/users/me/profile", async (req, res) => {
   let controller = await utility.getVersionedController(
     req.headers,
     "profileController"
   );
   /*eslint-disable no-eval*/ eval(controller).getProfile(req, res);
 });
+
+profileRouter.get("/users/:id/profile", authorizedRole, async (req, res) => {
+  let controller = await utility.getVersionedController(
+    req.headers,
+    "profileController"
+  );
+  /*eslint-disable no-eval*/ eval(controller).getProfileById(req, res);
+});
+
 module.exports = profileRouter;
