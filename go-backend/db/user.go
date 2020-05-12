8ae18b65143e4c2fede4e46a3fd1cae070004b10
J. Austin Hughey <jah@jah.io>
2020-05-12 15:45:43 -0600

    User.Organization()
    Given a user with a valid OrgID, calling user.Organization() will return the organization that corresponds to that ID in the database. Works only by re-calling db.Init() which returns a storer object that we run this against, and I’d like to find a better/cleaner way to do that but haven’t so far (suggestions welcome).
    
    + Changes multiple int64s to ints (int64 was a pain in the ass to work with and unnecessary)
    + Formatting fixes
    + CreateNewUser now specifies the default value (NOW() in UTC) for created_at. It also doesn’t halt if the error in its first pass at finding an existing user is sql.ErrNoRows.
    + Multiple db.Select statements changed to db.Get. Select wants to populate a slice/array, not just one variable. Get is what you need for a single variable.

--- go-backend/db/user.go
+++ go-backend/db/user.go
@@ -2,7 +2,7 @@ package db
 
 import (
 	"context"
-	"strconv"
+	"database/sql"
 	"time"
 
 	logger "github.com/sirupsen/logrus"
@@ -10,32 +10,47 @@ import (
 
 // User - basic struct representing a User
 type User struct {
-	ID              int64     `db:"id" json:"id"`
+	ID              int       `db:"id" json:"id"`
 	Name            string    `db:"name" json:"full_name"`
-	OrgID           int64     `db:"org_id" json:"org_id"`
+	OrgID           int       `db:"org_id" json:"org_id"`
 	Email           string    `db:"email" json:"email"`
 	DisplayName     string    `db:"display_name" json:"display_name"`
 	ProfileImageURL string    `db:"profile_image_url" json:"profile_image_url"`
 	SoftDelete      bool      `db:"soft_delete" json:"soft_delete"`
-	RoleID          int64     `db:"role_id" json:"role_id"`
+	RoleID          int       `db:"role_id" json:"role_id"`
 	Hi5QuotaBalance int       `db:"hi5_quota_balance" json:"hi5_quota_balance"`
-	SoftDeleteBy    int64     `db:"soft_delete_by" json:"soft_delete_by"`
+	SoftDeleteBy    int       `db:"soft_delete_by" json:"soft_delete_by"`
 	SoftDeleteOn    time.Time `db:"soft_delete_on" json:"soft_delete_on"`
 	CreatedAt       time.Time `db:"created_at" json:"created_at"`
+}
 
-	Organization
+// Organization - retrieve the user's organization based on the OrgID property
+func (u *User) Organization() (org Organization, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	store, err := Init() // TODO: Find a better way to do this
+	if err != nil {
+		// TODO: Log error
+	}
+	org, err = store.GetOrganization(ctx, u.OrgID)
+	if err != nil {
+		// TODO: Log error
+	}
+	return
 }
 
 // GetUserByEmail - Given an email address, return that user.
 func (s *pgStore) GetUserByEmail(ctx context.Context, email string) (user User, err error) {
-	err = s.db.Select(&user, `SELECT * FROM users WHERE email=$1 LIMIT 1`, email)
+	err = s.db.Get(&user, `SELECT * FROM users WHERE email=$1 LIMIT 1`, email)
 	if err != nil {
+		// Possible that there's no rows in the result set
 		logger.WithField("err", err.Error()).Error("Error selecting user from database by email " + email)
+		return
 	}
 
 	// Populate the user's organization data
 	org := Organization{}
-	err = s.db.Select(&org, `SELECT * FROM organizations WHERE id=$1`, user.OrgID)
+	err = s.db.Get(&org, `SELECT * FROM organizations WHERE id=$1 LIMIT 1`, user.OrgID)
 	if err != nil {
 		logger.WithField("err", err.Error()).Error("Error selecting user's organization from database by email and id " + email + " " + string(user.OrgID))
 	}
@@ -43,15 +58,15 @@ func (s *pgStore) GetUserByEmail(ctx context.Context, email string) (user User, 
 }
 
 // GetUserByID - Given the database ID for that user, look them up.
-func (s *pgStore) GetUserByID(ctx context.Context, id int64) (user User, err error) {
-	err = s.db.Select(&user, `SELECT * FROM users WHERE id=$1 LIMIT 1`, id)
+func (s *pgStore) GetUserByID(ctx context.Context, id int) (user User, err error) {
+	err = s.db.Get(&user, `SELECT * FROM users WHERE id=$1 LIMIT 1`, id)
 	if err != nil {
-		logger.WithField("err", err.Error()).Error("Error selecting user from database by id " + strconv.FormatInt(id, 10))
+		logger.WithField("err", err.Error()).Error("Error selecting user from database by id " + string(id))
 		return
 	}
 	// Populate the user's organization data
 	org := Organization{}
-	err = s.db.Select(&org, `SELECT * FROM organizations WHERE id=$1`, user.OrgID)
+	err = s.db.Get(&org, `SELECT * FROM organizations WHERE id=$1`, user.OrgID)
 	if err != nil {
 		logger.WithField("err", err.Error()).Error("Error selecting user's organization from database by user id and org id " + string(id) + " " + string(user.OrgID))
 	}
@@ -71,23 +86,18 @@ func (s *pgStore) ListUsers(ctx context.Context) (users []User, err error) {
 func (s *pgStore) CreateNewUser(ctx context.Context, u User) (newUser User, err error) {
 	// First, make sure they're not already in the database because if they are, we can just return that user
 	newUser, err = s.GetUserByEmail(ctx, u.Email)
-	if err != nil {
+	if err != nil && err != sql.ErrNoRows {
 		logger.WithField("err", err.Error()).Error("Error checking for duplicate user in db.CreateNewUser w/ email: " + u.Email)
 		return
 	}
 
-	// TODO: Check if newUser is a blank/nil object and if so, continue, otherwise return
-
-	// TODO: Check for the existence of an organization based on u.OrgID or the u.Organization.ID in the user struct
-	// Make sure it exists; if it doesn't, FAIL user creation
-
 	// If we made it this far, they don't appear to be in the database yet.
 	q := `INSERT INTO users (
     id, name, org_id, email, display_name, profile_image_url, soft_delete, role_id, hi5_quota_balance,
-    soft_delete_by, soft_delete_on
+    soft_delete_by, soft_delete_on, created_at
   ) VALUES (
     DEFAULT, :name, :org_id, :email, :display_name, :profile_image_url, :soft_delete, :role_id, :hi5_quota_balance,
-    :soft_delete_by, :soft_delete_on
+    :soft_delete_by, :soft_delete_on, DEFAULT
   )`
 	tx, err := s.db.Beginx() // Use Beginx instead of MustBegin so process doesn't die if there's an error
 	if err != nil {
