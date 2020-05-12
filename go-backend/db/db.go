abeac63f69c434ea15dd3793ed4f6838c4155aff
J. Austin Hughey <jah@jah.io>
2020-05-12 06:47:22 -0600

    resolve merge conflicts

--- go-backend/db/db.go
+++ go-backend/db/db.go
@@ -7,7 +7,6 @@ import (
 // Storer - an interface we use to expose methods that do stuff to the underlying database
 type Storer interface {
 	ListUsers(context.Context) ([]User, error)
-<<<<<<< HEAD
 	CleanBlacklistedTokens() error
 	CreateUserBlacklistedToken(context.Context, UserBlacklistedToken) error
 	CreateNewUser(context.Context, User) (User, error)
@@ -16,6 +15,6 @@ type Storer interface {
 	ListOrganizations(context.Context) ([]Organization, error)
 	GetOrganization(context.Context, int) (Organization, error)
 	CreateOrganization(context.Context, Organization) (Organization, error)
-	DeleteOrganization(context.Context, int) (error)
+	DeleteOrganization(context.Context, int) error
 	UpdateOrganization(context.Context, Organization, int) (Organization, error)
 }
