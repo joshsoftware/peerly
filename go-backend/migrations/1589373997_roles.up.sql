a174c4f3f33a8f0280b21693f437afcc239eb155
J. Austin Hughey <jah@jah.io>
2020-05-13 14:27:12 -0600

    Role.Name is so much better than Role.Role

--- go-backend/migrations/1589373997_roles.up.sql
+++ go-backend/migrations/1589373997_roles.up.sql
@@ -1,14 +1,14 @@
 -- Table definition
 CREATE TABLE IF NOT EXISTS roles (
     id BIGSERIAL NOT NULL PRIMARY KEY,
-    role VARCHAR(255)
+    name VARCHAR(255)
 );
 
 -- Create some indexes to help find roles easier
-CREATE UNIQUE INDEX IF NOT EXISTS idx_roles_role ON roles(role);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_roles_role ON roles(name);
 
 -- Create a foreign key over from users(role_id) to roles(id)
 ALTER TABLE IF EXISTS users ADD CONSTRAINT fk_users_role_id_roles_id FOREIGN KEY (role_id) REFERENCES roles(id);
 
 -- Create the default role, "Employee", at migration time
-INSERT INTO roles (id, role) VALUES (DEFAULT, 'Employee');
+INSERT INTO roles (id, name) VALUES (DEFAULT, 'Employee');
