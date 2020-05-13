35539e822d187c7d9da2062859aca7892bf66e10
J. Austin Hughey <jah@jah.io>
2020-05-13 14:28:27 -0600

    Clean up the interface definitions and group by object type

--- go-backend/db/db.go
+++ go-backend/db/db.go
@@ -6,16 +6,25 @@ import (
 
 // Storer - an interface we use to expose methods that do stuff to the underlying database
 type Storer interface {
+	// Users
 	ListUsers(context.Context) ([]User, error)
-	CleanBlacklistedTokens() error
-	CreateUserBlacklistedToken(context.Context, UserBlacklistedToken) error
 	CreateNewUser(context.Context, User) (User, error)
 	GetUserByEmail(context.Context, string) (User, error)
 	GetUserByID(context.Context, int) (User, error)
+
+	// Blacklisted Tokens
+	CleanBlacklistedTokens() error
+	CreateUserBlacklistedToken(context.Context, UserBlacklistedToken) error
+
+	// Organizations
 	ListOrganizations(context.Context) ([]Organization, error)
 	GetOrganization(context.Context, int) (Organization, error)
 	CreateOrganization(context.Context, Organization) (Organization, error)
 	DeleteOrganization(context.Context, int) error
 	UpdateOrganization(context.Context, Organization, int) (Organization, error)
 	GetOrganizationByDomainName(context.Context, string) (Organization, error)
+
+	// Roles
+	GetRoleByID(context.Context, int) (Role, error)
+	GetRoleByName(context.Context, string) (Role, error)
 }
