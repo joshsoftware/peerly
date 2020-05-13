9372a86e7e1f3be1015e305ffa7bfb7b67903727
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-13 13:14:43 +0530

    added a function for limit and offset value calculation

--- node-backend/app/utils/utility.js
+++ node-backend/app/utils/utility.js
@@ -24,3 +24,23 @@ module.exports.getVersionedController = (headers, route) => {
   }
 };
 /*eslint-enable no-useless-escape*/
+
+module.exports.getLimitAndOffset = (queryParamsObj) => {
+  let limit = 10;
+  let offset = 0;
+  if (queryParamsObj.limit) {
+    if (queryParamsObj.limit > 100) {
+      limit = 100;
+    } else {
+      limit = queryParamsObj.limit;
+    }
+  }
+  if (queryParamsObj.offset) {
+    offset = queryParamsObj.offset;
+  }
+  let limitOffsetObj = {
+    limit: limit,
+    offset: offset,
+  };
+  return limitOffsetObj;
+};
