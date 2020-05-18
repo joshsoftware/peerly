ff13689bb62ecc25153874710ab614d168813049
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-18 12:31:32 +0530

    updated users route for validation and delete route

--- node-backend/app/routes/usersRoutes.js
+++ node-backend/app/routes/usersRoutes.js
@@ -2,24 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const /*eslint-disable no-unused-vars*/ usersControllerV1 = require("../controllers/v1/usersController");
-const /*eslint-disable no-unused-vars*/ profileControllerV1 = require("../controllers/v1/profileController");
 const utility = require("../utils/utility");
-const jwtValidate = require("../jwtTokenValidation/jwtValidation");
 const usersRouter = express.Router();
 usersRouter.use(bodyParser.urlencoded({ extended: true }));
 
-async function authorizedRole(req, res, next) {
-  const tokenData = await jwtValidate.getData(req.headers["authorization"]);
-  if (tokenData.roleId == (1 || 2)) {
-    next();
-  } else {
-    res.status(401).send({
-      error: {
-        message: "unauthorised user",
-      },
-    });
-  }
-}
 usersRouter.get("/users", async (req, res) => {
   let controller = await utility.getVersionedController(
     req.headers,
@@ -36,7 +22,7 @@ usersRouter.get("/users/me", async (req, res) => {
   /*eslint-disable no-eval*/ eval(controller).getProfile(req, res);
 });
 
-usersRouter.get("/users/:id", authorizedRole, async (req, res) => {
+usersRouter.get("/users/:id", utility.authorizedRole, async (req, res) => {
   let controller = await utility.getVersionedController(
     req.headers,
     "usersController"
@@ -50,6 +36,22 @@ usersRouter.put("/users/me", async (req, res) => {
     "usersController"
   );
   /*eslint-disable no-eval*/ eval(controller).updateUser(req, res);
+});
+
+usersRouter.put("/users/:id", utility.authorizedRole, async (req, res) => {
+  let controller = await utility.getVersionedController(
+    req.headers,
+    "usersController"
+  );
+  /*eslint-disable no-eval*/ eval(controller).updateUserByAdmin(req, res);
+});
+
+usersRouter.delete("/users/:id", utility.authorizedRole, async (req, res) => {
+  let controller = await utility.getVersionedController(
+    req.headers,
+    "usersController"
+  );
+  /*eslint-disable no-eval*/ eval(controller).deleteUser(req, res);
 });
 
 module.exports = usersRouter;
