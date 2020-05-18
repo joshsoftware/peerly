3a2f0afb7b89a59f794212117d6f3d2128aac692
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-18 12:30:29 +0530

    updated users controller by implementing delete route

--- node-backend/app/controllers/v1/usersController.js
+++ node-backend/app/controllers/v1/usersController.js
@@ -1,20 +1,16 @@
 const qs = require(/*eslint-disable  node/no-extraneous-require*/ "qs");
-const yup = require("yup");
+const moment = require("moment");
 
 const utility = require("../../utils/utility");
 const db = require("../../models/sequelize");
 const Users = db.users;
 const jwtToken = require("../../jwtTokenValidation/jwtValidation");
+const validateSchema = require("./validationSchema/UsersValidationSchema");
+
 module.exports.findUsersByOrg = async (req, res) => {
   const authHeader = req.headers["authorization"];
   let userData = await jwtToken.getData(authHeader);
-
-  const schema = yup.object().shape({
-    org_id: yup.number().typeError({ org_id: "should be a number" }),
-    starts_with: yup.string().typeError({ starts_with: "should be a string" }),
-    limit: yup.number().typeError({ limit: "should be a number" }),
-    offset: yup.number().typeError({ offset: "should be a number" }),
-  });
+  const schema = validateSchema.findUsersByOrg();
   let obj = qs.parse(req.query);
   let limitOffsetObj = await utility.getLimitAndOffset(obj);
 
@@ -37,20 +33,9 @@ module.exports.findUsersByOrg = async (req, res) => {
           }
         } else {
           Users.findAll({
-            where: { org_id: userData.orgId },
-            attributes: [
-              "id",
-              "first_name",
-              "last_name",
-              "email",
-              "display_name",
-              "profile_image_url",
-              "role_id",
-              "hi5_quota_balance",
-              "org_id",
-              "soft_delete_by",
-              "soft_delete_at",
-            ],
+            where: { org_id: obj.org_id },
+            attributes: { exclude: ["soft_delete"] },
+            order: [["id", "ASC"]],
             limit: limitOffsetObj.limit,
             offset: limitOffsetObj.offset,
           })
@@ -70,19 +55,8 @@ module.exports.findUsersByOrg = async (req, res) => {
       } else {
         Users.findAll({
           where: { org_id: userData.orgId, role_id: 3 },
-          attributes: [
-            "id",
-            "first_name",
-            "last_name",
-            "email",
-            "display_name",
-            "profile_image_url",
-            "role_id",
-            "hi5_quota_balance",
-            "org_id",
-            "soft_delete_by",
-            "soft_delete_at",
-          ],
+          attributes: { exclude: ["soft_delete"] },
+          order: [["id", "ASC"]],
           limit: limitOffsetObj.limit,
           offset: limitOffsetObj.offset,
         })
@@ -114,17 +88,9 @@ module.exports.findUsersByOrg = async (req, res) => {
 module.exports.getProfile = async (req, res) => {
   let userData = await jwtToken.getData(req.headers["authorization"]);
   Users.findByPk(userData.userId, {
-    attributes: [
-      "id",
-      "first_name",
-      "last_name",
-      "email",
-      "display_name",
-      "profile_image_url",
-      "role_id",
-      "hi5_quota_balance",
-      "org_id",
-    ],
+    attributes: {
+      exclude: ["soft_delete", "soft_delete_by", "soft_delete_at"],
+    },
   })
     .then((profile) => {
       res.status(200).send({
@@ -142,28 +108,15 @@ module.exports.getProfile = async (req, res) => {
 
 module.exports.getProfileById = (req, res) => {
   const id = req.params.id;
-  const idSchema = yup.object().shape({
-    id: yup
-      .number()
-      .typeError({ id: "should be number" })
-      .required({ id: "required" }),
-  });
+  const idSchema = validateSchema.getProfileById();
   idSchema
     .validate({ id }, { abortEarly: false })
     .then(() => {
       Users.findAll({
         where: { id: id },
-        attributes: [
-          "id",
-          "first_name",
-          "last_name",
-          "email",
-          "display_name",
-          "profile_image_url",
-          "role_id",
-          "hi5_quota_balance",
-          "org_id",
-        ],
+        attributes: {
+          exclude: ["soft_delete", "soft_delete_by", "soft_delete_at"],
+        },
       })
         .then((data) => {
           if (data.length != 0) {
@@ -199,23 +152,7 @@ module.exports.getProfileById = (req, res) => {
 
 module.exports.updateUser = async (req, res) => {
   let userData = await jwtToken.getData(req.headers["authorization"]);
-  const schema = yup.object().shape({
-    first_name: yup
-      .string()
-      .typeError({ first_name: "should be string" })
-      .required({ first_name: "required" }),
-    last_name: yup
-      .string()
-      .typeError({ last_name: "should be string" })
-      .required({ last_name: "required" }),
-    display_name: yup
-      .string()
-      .typeError({ display_name: "should be string" })
-      .required({ display_name: "required" }),
-    profile_image_url: yup
-      .string()
-      .typeError({ profile_image_url: "should be string" }),
-  });
+  const schema = validateSchema.updateUser();
   const updateUser = {
     first_name: req.body.first_name,
     last_name: req.body.last_name,
@@ -242,6 +179,93 @@ module.exports.updateUser = async (req, res) => {
               },
             });
           }
+        })
+        .catch(() => {
+          res.status(500).send({
+            error: {
+              message: "internal server error",
+            },
+          });
+        });
+    })
+    .catch((err) => {
+      res.status(400).send({
+        error: utility.getFormattedErrorObj(
+          "invalid-user",
+          "Invalid user data",
+          err.errors
+        ),
+      });
+    });
+};
+
+module.exports.updateUserByAdmin = async (req, res) => {
+  const schema = validateSchema.updateUserByAdmin();
+  const updateUserByAdmin = {
+    role_id: req.body.role_id,
+  };
+  const updateUserValidation = {
+    role_id: req.body.role_id,
+    userId: req.params.id,
+  };
+
+  schema
+    .validate(updateUserValidation, { abortEarly: false })
+    .then(() => {
+      Users.update(updateUserByAdmin, {
+        returning: true,
+        where: { id: req.params.id },
+      })
+        .then(([rowsUpdate, [updateUserByAdmin]]) => {
+          if (rowsUpdate == 1) {
+            res.status(200).send({
+              data: updateUserByAdmin,
+            });
+          } else {
+            res.status(404).send({
+              error: {
+                message: "user not found for specified id",
+              },
+            });
+          }
+        })
+        .catch(() => {
+          res.status(500).send({
+            error: {
+              message: "internal server error",
+            },
+          });
+        });
+    })
+    .catch((err) => {
+      res.status(400).send({
+        error: utility.getFormattedErrorObj(
+          "invalid-user",
+          "Invalid user data",
+          err.errors
+        ),
+      });
+    });
+};
+
+module.exports.deleteUser = async (req, res) => {
+  const authHeader = req.headers["authorization"];
+  let userData = await jwtToken.getData(authHeader);
+  const schema = validateSchema.softDeleteUser();
+  const softDeleteUser = {
+    soft_delete: true,
+    soft_delete_by: userData.userId,
+    soft_delete_at: moment.utc().unix(),
+  };
+  schema
+    .validate({ id: req.params.id }, { abortEarly: false })
+    .then(() => {
+      Users.update(softDeleteUser, {
+        returning: true,
+        where: { id: req.params.id },
+      })
+        .then(() => {
+          res.status(200).send();
         })
         .catch(() => {
           res.status(500).send({
