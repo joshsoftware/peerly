abe393c9b8d91dc1bd7f64621085b136e8eeb746
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-05-19 15:59:26 +0530

    add button core component

---
+++ react-frontend/src/components/core/ButtonComponent.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import PropTypes from "prop-types";
+
+const ButtonComponent = ({
+  className,
+  type,
+  onClick,
+  size,
+  text,
+  style,
+  icon,
+}) => (
+  <Button
+    className={className}
+    type={type}
+    onClick={onClick}
+    size={size}
+    style={style}
+  >
+    {icon}
+    {text}
+  </Button>
+);
+
+ButtonComponent.propTypes = {
+  className: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  size: PropTypes.oneOf(["sm", "lg"]),
+  text: PropTypes.string.isRequired,
+  style: PropTypes.object,
+  icon: PropTypes.object,
+};
+
+ButtonComponent.defaultProps = {
+  type: "submit",
+  onClick: () => {
+    /* ToDo completing in future */
+  },
+};
+
+export default ButtonComponent;
