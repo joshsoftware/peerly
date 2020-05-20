07404b9e0d0fd6baf384d750c895880c6e2267dd
Avinash Mane <avimanepatil@gmail.com>
2020-05-20 16:03:45 +0530

    change directory structure

--- react-frontend/src/shared-components/loginTextComponent/LoginTextComponent.js
+++ react-frontend/src/shared-components/loginTextComponent/LoginTextComponent.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Col } from "react-bootstrap";
-import LabelComponent from "components/core/Label/LabelComponent";
+import LabelComponent from "core-components/Label/LabelComponent";
 
 const LoginTextComponent = ({
   className,
