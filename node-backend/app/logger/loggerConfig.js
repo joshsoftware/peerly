cb70490a57229abef6c8e10bdf87766473692323
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-20 13:07:59 +0530

    added logger file configuration

---
+++ node-backend/app/logger/loggerConfig.js
@@ -0,0 +1,16 @@
+const log4js = require("log4js");
+
+module.exports = log4js.configure({
+  appenders: {
+    fileAppender: {
+      type: "file",
+      filename: "./app/logger/logfile.log",
+    },
+  },
+  categories: {
+    default: {
+      appenders: ["fileAppender"],
+      level: "error",
+    },
+  },
+});
