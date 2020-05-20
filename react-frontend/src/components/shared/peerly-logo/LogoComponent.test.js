fa530aa1f32c042fe74c892b223e1a913074e50b
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-20 16:01:41 +0530

    added file updations

--- react-frontend/src/components/shared/peerly-logo/LogoComponent.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-
-import LogoComponent from "components/shared/peerly-logo/LogoComponent";
-
-it("should equal to + Peerly text", () => {
-  const { getByTestId } = render(<LogoComponent text="Peerly" />);
-  expect(getByTestId("peerlyLogoComponent")).toHaveTextContent("+ Peerly");
-});
