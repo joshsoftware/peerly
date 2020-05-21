3523c252a7387300ebc600689e84cbd49b6fc792
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-21 14:22:38 +0530

    followed directory structure

---
+++ react-frontend/src/shared-components/peerly-logo/LogoComponent.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Col, Row } from "react-bootstrap";
+
+import LabelComponent from "core-components/Label/LabelComponent";
+
+const LogoComponent = () => (
+  <Col data-testid="peerlyLogoComponent">
+    <Row>
+      <LabelComponent text="+" className="text-white text-center w-100" />
+    </Row>
+    <Row>
+      <LabelComponent text="Peerly" className="text-white text-center w-100" />
+    </Row>
+  </Col>
+);
+
+export default LogoComponent;
