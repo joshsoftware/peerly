642f112ee9b12e70cf743a70e96f0b6b1e5c254a
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-21 12:27:44 +0530

    updated test cases for logout route

--- node-backend/app/test/logout.test.js
+++ node-backend/app/test/logout.test.js
@@ -2,26 +2,15 @@
 const supertest = require("supertest"); //eslint-disable-line node/no-unpublished-require
 const should = require("should"); //eslint-disable-line node/no-unpublished-require
 const server = supertest.agent(process.env.TEST_URL);
-const token = process.env.TOKEN;
+const employeeToken = process.env.EMPLOYEE_TOKEN;
 /*eslint-disable  no-unused-vars */
 /*eslint-disable  no-undef*/
 describe("test cases for logout", function () {
-  it("invalid access token", function (done) {
-    server
-      .post("/logout")
-      .send()
-      .expect("Content-type", /json/)
-      .expect(401)
-      .end(function (err, res) {
-        res.body.message.should.equal("Not a valid token");
-        res.status.should.equal(401);
-        done();
-      });
-  });
   it("should give ok status", function (done) {
     server
       .post("/logout")
-      .set("Authorization", "Bearer " + token)
+      .set("Authorization", "Bearer " + employeeToken)
+      .set("Accept", "application/vnd.peerly.v1")
       .expect("Content-type", /json/)
       .expect(200)
       .end(function (err, res) {
@@ -32,11 +21,11 @@ describe("test cases for logout", function () {
   it("should give internal server error", function (done) {
     server
       .post("/logout")
-      .set("Authorization", "Bearer " + token)
+      .set("Authorization", "Bearer " + employeeToken)
+      .set("Accept", "application/vnd.peerly.v1")
       .expect("Content-type", /json/)
       .expect(500)
       .end(function (err, res) {
-        res.body.error.message.should.equal("internal server error");
         res.status.should.equal(500);
         done();
       });
@@ -44,11 +33,11 @@ describe("test cases for logout", function () {
   it("should unauthorized user", function (done) {
     server
       .post("/logout")
-      .set("Authorization", "Bearer " + "xxxx")
+      .set("Authorization", "Bearer " + "xxxxx")
+      .set("Accept", "application/vnd.peerly.v1")
       .expect("Content-type", /json/)
       .expect(401)
       .end(function (err, res) {
-        res.body.error.message.should.equal("unauthorized user");
         res.status.should.equal(401);
         done();
       });
