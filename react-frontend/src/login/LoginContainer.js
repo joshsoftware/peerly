500ba42da7761c6fe6855c8ac5ba96a37c70b9e0
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-22 20:15:58 +0530

    created login container that will call login saga

--- react-frontend/src/login/LoginContainer.js
+++ react-frontend/src/login/LoginContainer.js
@@ -1,22 +1,18 @@
 import React from "react";
 import { createStore, applyMiddleware } from "redux";
-import createSagaMiddleware from "redux-saga";
+import { createSagaMiddleware } from "redux-saga";
 
 import Login from "login/LoginComponent";
 import Reducers from "reducers/loginReducer";
-import LoginApi from "utils/postJson";
 import sagas from "sagas/loginSaga";
+
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(Reducers, applyMiddleware(sagaMiddleware));
 sagaMiddleware.run(sagas);
 
 const LoginContainer = () => {
-  const responseGoogle = async (response) => {
-    store.dispatch({ type: "GOOGLE_LOGIN_SUCCESS" });
-    const a = await (
-      await LoginApi({ access_token: response.tokenObj.access_token })
-    ).json();
-    store.dispatch({ type: "LOGIN_SUCCESS", value: a });
+  const responseGoogle = async ({ tokenObj }) => {
+    store.dispatch({ type: "LOGIN_API", payload: tokenObj.access_token });
   };
 
   return (
