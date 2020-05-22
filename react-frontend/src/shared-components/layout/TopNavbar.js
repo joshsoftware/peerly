e5513dbf2a37f40ea95fce7b792a6b08d1bd5fef
mayuriardad <mayuri.ardad@joshsoftware.com>
2020-05-22 14:55:11 +0530

    Add top navbar for peerly

--- react-frontend/src/shared-components/layout/TopNavbar.js
+++ react-frontend/src/shared-components/layout/TopNavbar.js
@@ -1,10 +1,47 @@
 import React from "react";
+import { Navbar, Container } from "react-bootstrap";
+import styled from "styled-components";
+
+import HighFiveComponent from "shared-components/high-five-components/HighFiveComponent";
+import NotificationBadgeComponent from "core-components/notification/NotificationBadgeComponent";
+
+const NavbarWrapper = styled(Navbar)`
+  height: 100px;
+  background: var(--white) 0% 0% no-repeat padding-box;
+  box-shadow: 0px 0px 10px #1b1c201a;
+  opacity: 1;
+`;
+
+const BrandText = styled(Navbar.Brand)`
+  position: absolute;
+  left: calc(50% - 27px);
+  right: 50%;
+`;
+
+const HighFiveIcon = styled(HighFiveComponent)`
+  height: 34px;
+`;
 
 function TopNavbar() {
   return (
-    <div>
-      <p>Navbar placeholder</p>
-    </div>
+    <NavbarWrapper>
+      <Container>
+        <div className="w-100">
+          <BrandText>Peerly</BrandText>
+          <div className="text-right">
+            <Navbar.Text className="">
+              <span className="d-sm-block d-none align-self-center">
+                {"This week's"}
+              </span>
+            </Navbar.Text>
+            <Navbar.Text>
+              <NotificationBadgeComponent count={2} />
+              <HighFiveIcon />
+            </Navbar.Text>
+          </div>
+        </div>
+      </Container>
+    </NavbarWrapper>
   );
 }
 
