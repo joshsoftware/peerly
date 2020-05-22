e0cf40cb39a8a92aa7f5ac283e360b9384a57f47
mayuriardad <mayuri.ardad@joshsoftware.com>
2020-05-22 14:55:11 +0530

    Add top navbar for peerly

--- react-frontend/src/core-components/notification/NotificationBadgeComponent.js
+++ react-frontend/src/core-components/notification/NotificationBadgeComponent.js
@@ -1,19 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-
 import PropTypes from "prop-types";
 
 const NotificationWrapper = styled.div`
   position: relative;
-  width: 100%;
-  height: 100%;
 `;
 
-const Notification = styled.span`
+const Badge = styled.span`
   border: 1px solid white;
-  display: inline-block;
   position: absolute;
-  min-width: 10px;
   padding: 1px 5px;
   font-size: 12px;
   font-weight: 700;
@@ -23,20 +18,29 @@ const Notification = styled.span`
   vertical-align: baseline;
   background-color: var(--sage);
   border-radius: 10px;
-  top: -5px;
-  right: 15px;
+  top: ${(props) => props.top && props.top};
+  right: ${(props) => props.right && props.right};
+  left: ${(props) => props.left && props.left};
+  bottom: ${(props) => props.bottom && props.bottom};
 `;
 
-const NotificationBadgeComponent = ({ count }) => {
+const NotificationBadgeComponent = ({ count, styles }) => {
   return (
     <NotificationWrapper>
-      <Notification data-testid="count-display">{count}</Notification>
+      <Badge data-testid="count-display" {...styles}>
+        {count}
+      </Badge>
     </NotificationWrapper>
   );
 };
 
 NotificationBadgeComponent.propTypes = {
   count: PropTypes.number.isRequired,
+  styles: PropTypes.object,
+};
+
+NotificationBadgeComponent.defaultProps = {
+  styles: { top: "-5px", right: "15px" },
 };
 
 export default NotificationBadgeComponent;
