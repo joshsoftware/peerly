1ac48baa4a218a21aa8ec2f5236702726e02dc8f
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-25 08:59:10 +0530

    added login reducer to root reducer

--- react-frontend/src/reducers/rootReducer.js
+++ react-frontend/src/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import appReducer from "reducers/appReducer.js";
+import loginReducer from "reducers/loginReducer";
 
 export default combineReducers({
   appReducer,
+  loginReducer,
 });
