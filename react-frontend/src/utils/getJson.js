06cae0a87b73b7c5b12c831b87d59351d57bc991
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-25 09:59:52 +0530

    updated getJson file

--- react-frontend/src/utils/getJson.js
+++ react-frontend/src/utils/getJson.js
@@ -1,21 +1,14 @@
-import objToQueryString from "utils/objToQueryString.js";
-import getRequestUrl from "utils/getReqUrl.js";
+//import objToQueryString from "utils/objToQueryString.js";
+//import getRequestUrl from "utils/getReqUrl.js";
 import getDefaultHeaders from "utils/commonHeaders.js";
 
-export default ({
-  path,
-  paramsObj = {},
-  apiToken,
-  signal,
-  additionalHeaders,
-}) => {
-  const queryString = objToQueryString(paramsObj);
-
-  return fetch(`${getRequestUrl(path)}${queryString}`, {
+export default ({ path, apiToken, additionalHeaders }) => {
+  //  const queryString = objToQueryString(paramsObj);
+  return fetch(path, {
     method: "GET",
     referrer: "no-referrer",
     redirect: "manual",
-    signal,
+    //signal,
     headers: new Headers({
       ...getDefaultHeaders(apiToken),
       ...additionalHeaders,
