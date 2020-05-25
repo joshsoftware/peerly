a99ae5618aabbd601d0cbb0e1fb15b483604c5b1
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-25 08:59:56 +0530

    added post json file with required parameters

--- react-frontend/src/utils/postJson.js
+++ react-frontend/src/utils/postJson.js
@@ -1,19 +1,23 @@
-//import getRequestUrl from "utils/getReqUrl.js";
-//import getDefaultHeaders from "utils/commonHeaders.js";
+import getRequestUrl from "utils/getReqUrl.js";
+import getDefaultHeaders from "utils/commonHeaders.js";
 
-export default (paramsObj = {}) => {
-  return fetch("http://localhost:3120/oauth/google", {
-    method: "POST",
+export default ({
+  path,
+  paramsObj = {},
+  apiToken,
+  signal,
+  additionalHeaders,
+  method = "POST",
+}) => {
+  return fetch(getRequestUrl(path), {
+    method,
     referrer: "no-referrer",
     redirect: "manual",
-    //  signal,
-    /* headers: new Headers({
-       ...getDefaultHeaders(apiToken),
-       ...additionalHeaders,
-     }),*/
-    body: paramsObj instanceof FormData ? paramsObj : JSON.stringify(paramsObj),
+    signal,
     headers: new Headers({
-      "Content-Type": "application/json",
+      ...getDefaultHeaders(apiToken),
+      ...additionalHeaders,
     }),
+    body: paramsObj instanceof FormData ? paramsObj : JSON.stringify(paramsObj),
   });
 };
