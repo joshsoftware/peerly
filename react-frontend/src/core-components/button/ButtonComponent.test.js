f31c3c4671dd4d3f1541ca9d22362e10f740a957
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-05-25 21:06:08 +0530

    change button core component and resolve comment on PR

--- react-frontend/src/core-components/button/ButtonComponent.test.js
+++ react-frontend/src/core-components/button/ButtonComponent.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
 
-import ButtonComponent from "core-components/button/ButtonComponent";
+import Button from "core-components/button/ButtonComponent";
 
 it("ButtonComponent should equal to submit text", () => {
-  const { getByText } = render(<ButtonComponent text="submit" type="submit" />);
+  const { getByText } = render(<Button type="submit">submit</Button>);
   expect(getByText("submit")).toHaveTextContent("submit");
 });
