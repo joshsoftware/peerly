fb356dd8aa971e4c8cad8a049fcce39fe20934fc
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-05-25 12:47:50 +0530

    resolved comment's on PR and add core-component for Row Col

--- react-frontend/src/core-components/label/LabelComponent.test.js
+++ react-frontend/src/core-components/label/LabelComponent.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import LabelComponent from "core-components/Label/LabelComponent";
+import LabelComponent from "core-components/label/LabelComponent";
 
 it("LabelComponent should equal to submit text", () => {
   const { getByText } = render(<LabelComponent text="submit" />);
