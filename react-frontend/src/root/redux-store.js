8bc9d70771415e7182ec13202df60e11c4f831cc
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-05-26 19:08:28 +0530

    add code in redux-store for run recognitionSaga at sagaMiddleware

--- react-frontend/src/root/redux-store.js
+++ react-frontend/src/root/redux-store.js
@@ -4,6 +4,7 @@ import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "reducers/rootReducer.js";
 import rootSaga from "sagas/rootSaga.js";
+import recognitionSaga from "sagas/recognitionSaga";
 
 // create the saga middleware
 export const sagaMiddleware = createSagaMiddleware();
@@ -19,3 +20,4 @@ export const store = configureStore();
 
 // run the root saga
 sagaMiddleware.run(rootSaga);
+sagaMiddleware.run(recognitionSaga);
