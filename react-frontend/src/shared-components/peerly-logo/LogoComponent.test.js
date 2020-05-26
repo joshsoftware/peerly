442cfee6b301f616e26f1a06215adced276e23d3
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-26 13:28:40 +0530

    removed text prop for logo-component from test file

--- react-frontend/src/shared-components/peerly-logo/LogoComponent.test.js
+++ react-frontend/src/shared-components/peerly-logo/LogoComponent.test.js
@@ -4,6 +4,6 @@ import { render } from "@testing-library/react";
 import LogoComponent from "shared-components/peerly-logo/LogoComponent";
 
 it("should equal to + Peerly text", () => {
-  const { getByTestId } = render(<LogoComponent text="Peerly" />);
+  const { getByTestId } = render(<LogoComponent />);
   expect(getByTestId("peerlyLogoComponent")).toHaveTextContent("+ Peerly");
 });
