fb79a18594b3d34f28ffbcc50c91d9fba6b08cda
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-26 20:23:21 +0530

    added logger configurations on weekly file rolling

--- node-backend/app/logger/loggerConfig.js
+++ node-backend/app/logger/loggerConfig.js
@@ -4,10 +4,10 @@ module.exports = log4js.configure({
   appenders: {
     fileAppender: {
       type: "dateFile",
-      pattern: ".yyyy-MM-dd-hh",
-      compress: true,
+      pattern: ".yyyy-ww",
+      compress: false,
       filename: "./app/logger/logfile.log",
-      daysToKeep: 2,
+      backups: 3,
     },
   },
   categories: {
