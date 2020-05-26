2e4a176f0e27d3b6b1ba2e5b243be7a1553ddbca
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-26 08:52:01 +0530

    added test case for login api

--- react-frontend/src/sagas/loginSaga.test.js
+++ react-frontend/src/sagas/loginSaga.test.js
@@ -1,5 +1,6 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, call } from "redux-saga/effects";
 import { loginApi, userLogin } from "sagas/loginSaga";
+import PostJson from "utils/postJson";
 
 describe("SAGAS", () => {
   it("should dispatch action 'LOGIN_API' ", () => {
@@ -7,4 +8,23 @@ describe("SAGAS", () => {
     expect(generator.next().value).toEqual(takeEvery("LOGIN_API", userLogin));
     expect(generator.next().done).toBeTruthy();
   });
+
+  it("should dispatch action 'LOGIN_API' with result from fetch users API", () => {
+    const mockResponse = { access_token: "Some content" };
+    const generator = userLogin(mockResponse);
+    expect(generator.next(mockResponse).value).toEqual(
+      call(PostJson, {
+        path: "http://localhost:3120/oauth/google",
+        apiToken: "",
+        signal: "",
+        additionalHeaders: "",
+        paramsObj: { access_token: undefined },
+      })
+    );
+  });
 });
+
+/* expect(generator.next().value)
+       .toEqual(put({type:"LOGIN_FAILURE", value: { error: 'user not found' }}))
+      expect(generator.next().done).toBeTruthy();
+    });*/
