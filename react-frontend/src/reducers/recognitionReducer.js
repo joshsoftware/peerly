1ee0089ea4f3668620e04002cdc71438fc2c812f
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-05-26 18:53:26 +0530

    update recognition Reducer

--- react-frontend/src/reducers/recognitionReducer.js
+++ react-frontend/src/reducers/recognitionReducer.js
@@ -3,9 +3,9 @@ const defaultState = [{}];
 export default (state = defaultState, action) => {
   switch (action.type) {
     case "GET_RECOGNITION_LIST_SUCCESS":
-      return action.value;
+      return action.payload;
     case "GET_RECOGNITION_LIST_FAILURE":
-      return action.value;
+      return action.payload;
     default:
       return state;
   }
