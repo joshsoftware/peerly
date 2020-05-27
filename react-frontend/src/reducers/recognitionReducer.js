3cf2b24ac11e68b5214abcdb2d6de931b549e179
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-05-27 16:37:30 +0530

    restructure default state of recognition reducer

--- react-frontend/src/reducers/recognitionReducer.js
+++ react-frontend/src/reducers/recognitionReducer.js
@@ -1,11 +1,14 @@
-const defaultState = [{}];
+const defaultState = {
+  list: [{}],
+  error: {},
+};
 
 export default (state = defaultState, action) => {
   switch (action.type) {
     case "GET_RECOGNITION_LIST_SUCCESS":
-      return action.payload;
+      return { ...state, list: action.payload };
     case "GET_RECOGNITION_LIST_FAILURE":
-      return action.payload;
+      return { ...state, error: action.payload };
     default:
       return state;
   }
