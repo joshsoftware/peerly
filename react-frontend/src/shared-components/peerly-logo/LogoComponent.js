8e6607062485fbf3ac3f223d2515f13aa63d1a64
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-27 17:53:18 +0530

    removed label and added css for plus sign

--- react-frontend/src/shared-components/peerly-logo/LogoComponent.js
+++ react-frontend/src/shared-components/peerly-logo/LogoComponent.js
@@ -52,32 +52,6 @@ const Logo = styled.div`
   }
 `;
 
-const Logo = Styled.div`
-& {
-  width: 50px;
-  height: 50px;   
-  position: absolute;
-  top: 10px;
-  transform: rotate(45deg);
-}
-&: before {
-  content: "";
-  width: 20px;
-  height: 20px;
-  border: 2px solid #FFFFFF;              
-  position: absolute;
-  transform: translate(-50%,-50%);
-  top: 50%;
-  left: 50%;
-}  
-`;
-const PlusSign = Styled.div`
-  position: absolute;
-  top: 55%;
-  left: 55%;
-  transform: translate(-50%, -50%) rotate(-45deg);
-  color: #FFFFFF;
-`;
 
 const LogoComponent = () => (
   <Row className="d-flex flex-column" data-testid="peerlyLogoComponent">
