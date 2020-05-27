7ae873c14fe9f77feb0989bad6efdc5224444354
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-05-27 16:55:52 +0530

    delete test case of ButtonComponent

--- react-frontend/src/core-components/button/ButtonComponent.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-
-import Button from "core-components/button/ButtonComponent";
-
-it("ButtonComponent should equal to submit text", () => {
-  const { getByText } = render(<Button type="submit">submit</Button>);
-  expect(getByText("submit")).toHaveTextContent("submit");
-});
