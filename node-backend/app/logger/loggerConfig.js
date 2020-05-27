61ae2af9f4619da219d59da307d45cb9e0e87c3a
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-27 12:18:07 +0530

    created function for accepting rolloing time from env

--- node-backend/app/logger/loggerConfig.js
+++ node-backend/app/logger/loggerConfig.js
@@ -1,10 +1,14 @@
 const log4js = require("log4js");
 
+const getDatePattern = () => {
+  return process.env.LOGGER_ROLLING_TIME;
+};
+
 module.exports = log4js.configure({
   appenders: {
     fileAppender: {
       type: "dateFile",
-      pattern: ".yyyy-ww",
+      pattern: getDatePattern(),
       compress: false,
       filename: "./app/logger/logfile.log",
       backups: 3,
