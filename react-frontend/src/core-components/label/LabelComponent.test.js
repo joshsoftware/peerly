7692adb299e314a9ec5ea3fe58b6586bdfc489e2
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-05-27 17:49:59 +0530

    delete LabelComponent and test case of LabelComponent

--- react-frontend/src/core-components/label/LabelComponent.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-
-import LabelComponent from "core-components/label/LabelComponent";
-
-it("LabelComponent should equal to submit text", () => {
-  const { getByText } = render(<LabelComponent text="submit" />);
-  expect(getByText("submit")).toHaveTextContent("submit");
-});
