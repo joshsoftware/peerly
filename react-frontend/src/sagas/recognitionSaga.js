ede91cda9c0b3a5fbb8790c438d072320eac69fb
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-05-27 16:39:01 +0530

    adjust code as per default state of recognition reducer

--- react-frontend/src/sagas/recognitionSaga.js
+++ react-frontend/src/sagas/recognitionSaga.js
@@ -1,9 +1,9 @@
 import { put, takeEvery, spawn, call } from "redux-saga/effects";
-import GetJson from "utils/getJson";
+import getJson from "utils/getJson";
 
 export function* getRecognitionList(action) {
   try {
-    const response = yield call(GetJson, {
+    const response = yield call(getJson, {
       path: "recognitions",
       apiToken:
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJub2RlLnBlZXJseS5jb20iLCJzdWIiOjE5LCJhdWQiOiJwZWVybHkuY29tIiwibmJmIjoxNTkwNTQ3MzA1LCJodHRwczovL3BlZXJseS5jb20iOnsicm9sZUlkIjozLCJvcmdJZCI6Mywib3JnTmFtZSI6Impvc2gifSwiaWF0IjoxNTkwNTQ3MzA1LCJleHAiOjE1OTA1ODMzMDV9.5zNi8j6IZHa0Y1lGBFjeyaNvUrh82Gf0kmOAZQHvuXg",
@@ -18,11 +18,11 @@ export function* getRecognitionList(action) {
     } else {
       yield put({
         type: "GET_RECOGNITION_LIST_FAILURE",
-        payload: [responseObj.error],
+        payload: responseObj.error,
       });
     }
   } catch (error) {
-    yield put({ type: "GET_RECOGNITION_LIST_FAILURE", payload: [error] });
+    yield put({ type: "GET_RECOGNITION_LIST_FAILURE", payload: error });
   }
 }
 
