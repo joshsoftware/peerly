6610c833867678f9cfe1544115b21374031835c2
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-05-28 16:34:49 +0530

    add some test case for recognitionSaga

--- react-frontend/src/sagas/recognitionSaga.test.js
+++ react-frontend/src/sagas/recognitionSaga.test.js
@@ -4,24 +4,24 @@ import { recognitionApi, getRecognitionList } from "sagas/recognitionSaga";
 import getJson from "utils/getJson";
 
 describe("RECOGNITION SAGAS", () => {
-  it("should dispatch action 'RECOGNITION_GET_API' for recognition saga", () => {
+  it("should dispatch action 'LIST_RECOGNITION_API' for recognition saga", () => {
     const generator = recognitionApi();
     expect(generator.next().value).toEqual(
-      takeEvery("RECOGNITION_GET_API", getRecognitionList)
+      takeEvery("LIST_RECOGNITION_API", getRecognitionList)
     );
     const status = generator.next().done;
     expect(status).toBeTruthy();
   });
 
-  it("should dispatch action 'RECOGNITION_GET_API' with result from fetch get list API", () => {
+  it("should dispatch action 'LIST_RECOGNITION_API' for fetch list", () => {
     const generator = getRecognitionList();
-
     expect(generator.next().value).toEqual(
       call(getJson, {
         path: "recognitions",
-        apiToken:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJub2RlLnBlZXJseS5jb20iLCJzdWIiOjE5LCJhdWQiOiJwZWVybHkuY29tIiwibmJmIjoxNTkwNjM2NDUyLCJodHRwczovL3BlZXJseS5jb20iOnsicm9sZUlkIjozLCJvcmdJZCI6Mywib3JnTmFtZSI6Impvc2gifSwiaWF0IjoxNTkwNjM2NDUyLCJleHAiOjE1OTA2NzI0NTJ9.MKkukGmgx9RHyFm_dfrGp8j_W09fBU8Qy9EzPXXcv3w",
+        apiToken: "",
       })
     );
+    expect(generator.next().done).toEqual(false);
+    expect(generator.next().done).toBeTruthy();
   });
 });
