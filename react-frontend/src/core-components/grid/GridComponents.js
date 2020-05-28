c1164abf2b9eda0168abd625196ed4a6fa6cbeb5
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-05-28 18:10:47 +0530

    add Wrapper for Row Col and Container in GridComponent

--- react-frontend/src/core-components/grid/GridComponents.js
+++ react-frontend/src/core-components/grid/GridComponents.js
@@ -1,3 +1,12 @@
+import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
 
-export { Row, Col, Container };
+const RowComponent = React.memo(Row);
+const ColComponent = React.memo(Col);
+const ContainerComponent = React.memo(Container);
+
+export {
+  RowComponent as Row,
+  ColComponent as Col,
+  ContainerComponent as Container,
+};
