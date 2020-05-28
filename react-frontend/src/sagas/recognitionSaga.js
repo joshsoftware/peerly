8138af0ac4570347f6509a53c7f30074c2fb88bf
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-05-28 15:53:18 +0530

    update recognitionSaga by use utils actionGenerator function

--- react-frontend/src/sagas/recognitionSaga.js
+++ react-frontend/src/sagas/recognitionSaga.js
@@ -1,33 +1,34 @@
 import { put, takeEvery, spawn, call } from "redux-saga/effects";
 import getJson from "utils/getJson";
 
-export function* getRecognitionList(action) {
+import actionGenerator from "utils/actionGenerator";
+
+export function* getRecognitionList() {
+  const status = actionGenerator("LIST_RECOGNITION");
   try {
     const response = yield call(getJson, {
       path: "recognitions",
-      apiToken:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJub2RlLnBlZXJseS5jb20iLCJzdWIiOjE5LCJhdWQiOiJwZWVybHkuY29tIiwibmJmIjoxNTkwNTQ3MzA1LCJodHRwczovL3BlZXJseS5jb20iOnsicm9sZUlkIjozLCJvcmdJZCI6Mywib3JnTmFtZSI6Impvc2gifSwiaWF0IjoxNTkwNTQ3MzA1LCJleHAiOjE1OTA1ODMzMDV9.5zNi8j6IZHa0Y1lGBFjeyaNvUrh82Gf0kmOAZQHvuXg",
-      paramsObj: action.payload,
+      apiToken: "",
     });
     const responseObj = yield response.json();
     if (responseObj.data) {
       yield put({
-        type: "GET_RECOGNITION_LIST_SUCCESS",
+        type: status.success,
         payload: responseObj.data,
       });
     } else {
       yield put({
-        type: "GET_RECOGNITION_LIST_FAILURE",
+        type: status.failure,
         payload: responseObj.error,
       });
     }
   } catch (error) {
-    yield put({ type: "GET_RECOGNITION_LIST_FAILURE", payload: error });
+    yield put({ type: status.failure, payload: error });
   }
 }
 
 export function* recognitionApi() {
-  yield takeEvery("RECOGNITION_GET_API", getRecognitionList);
+  yield takeEvery("LIST_RECOGNITION_API", getRecognitionList);
 }
 
 export default function* rootRecognitionSaga() {
