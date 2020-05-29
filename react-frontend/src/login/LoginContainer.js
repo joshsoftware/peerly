7ffe3f6b5992fd9f3ce0f34e7b3e09a802c14d3a
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-29 09:36:36 +0530

    added test cases in login container

--- react-frontend/src/login/LoginContainer.js
+++ react-frontend/src/login/LoginContainer.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import Login from "login/LoginComponent";
 import { store } from "root/redux-store";
+import UnauthorisedErrorComponent from "shared-components/UnauthorisedErrorComponent";
+import InternalServerErrorComponent from "shared-components/InternalServerErrorComponent";
 
 const LoginContainer = () => {
+  const loginAuthorization = useSelector((state) => state.loginReducer);
+
   const responseGoogleOnSuccess = ({ tokenObj }) => {
     store.dispatch({ type: "LOGIN_API", payload: tokenObj.access_token });
   };
@@ -12,6 +17,13 @@ const LoginContainer = () => {
     store.dispatch({ type: "LOGIN_FAILURE", value: error });
   };
 
+  if (loginAuthorization.error.message === "unauthorized user") {
+    return <UnauthorisedErrorComponent />;
+  } else if (loginAuthorization.error.error === "popup_closed_by_user") {
+    return <UnauthorisedErrorComponent />;
+  } else if (loginAuthorization.error.message === "internal server error") {
+    return <InternalServerErrorComponent />;
+  }
   return (
     <div data-testid="LoginContainer">
       <Login
