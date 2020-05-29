7d3237efbffeadbf35fff8391f45c36d29d8ab4a
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-29 09:25:51 +0530

    updated logger configuration for env

--- node-backend/app/logger/loggerConfig.js
+++ node-backend/app/logger/loggerConfig.js
@@ -1,17 +1,21 @@
 const log4js = require("log4js");
 
-const getDatePattern = () => {
-  return process.env.LOGGER_ROLLING_TIME;
+const getLoggerFileCompression = () => {
+  if (process.env.LOGGER_FILE_COMPRESSION == "false") {
+    return false;
+  } else {
+    return true;
+  }
 };
 
 module.exports = log4js.configure({
   appenders: {
     fileAppender: {
-      type: "dateFile",
-      pattern: getDatePattern(),
-      compress: false,
-      filename: "./app/logger/logfile.log",
-      backups: 3,
+      type: process.env.TYPE_OF_LOGGER_FILE,
+      pattern: process.env.LOGGER_ROLLING_TYPE,
+      compress: getLoggerFileCompression(),
+      filename: process.env.LOGGER_FILE_STORAGE_PATH,
+      daysToKeep: parseInt(process.env.BACKUP_FILES_FOR_LOGGER),
     },
   },
   categories: {
