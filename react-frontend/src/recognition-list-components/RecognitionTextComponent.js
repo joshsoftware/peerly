b9e5616943d3d9d2f4bd185e458506ae59772b2e
Avinash Mane <avimanepatil@gmail.com>
2020-05-29 19:47:53 +0530

    create card core-component

--- react-frontend/src/recognition-list-components/RecognitionTextComponent.js
+++ react-frontend/src/recognition-list-components/RecognitionTextComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+
 import { Row } from "core-components/grid/GridComponent";
 import { Form } from "core-components/form/FormComponent";
 import ImageComponent from "core-components/image/ImageComponent";
