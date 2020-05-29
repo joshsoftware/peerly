a075aa366ef26f3020816d698f24123973c52381
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-05-29 17:26:53 +0530

    added core value component

---
+++ react-frontend/src/shared-components/core-value/coreValue.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Styled from "styled-components";
+
+import { Form } from "core-components/form/FormComponent";
+
+const Wrapper = Styled.div`
+max-width:75px;
+min-height:75px;
+border-radius: 5px;
+border: 3px solid black;
+display: flex;
+text-align: center;
+font-size: 0.75em;
+`;
+
+const CoreValueComponent = ({
+  coreValueName,
+}) => (
+  <Wrapper>
+    <Form.Label>
+    {coreValueName}
+    </Form.Label>
+  </Wrapper>
+);
+
+CoreValueComponent.propTypes = {
+  coreValueName: PropTypes.string.isRequired,
+};
+
+export default CoreValueComponent;
+
+/**<Icon className={IconClassName} /> */
