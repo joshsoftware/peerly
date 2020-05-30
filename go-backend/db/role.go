4c80f054d7bd1df67768281673ccb54f9a095116
J. Austin Hughey <jah@jah.io>
2020-05-30 02:26:32 -0600

    remove unneeded code, add error condition returns

--- go-backend/db/role.go
+++ go-backend/db/role.go
@@ -17,6 +17,7 @@ func (s *pgStore) GetRoleByID(ctx context.Context, id int) (role Role, err error
 	err = s.db.Get(&role, `SELECT * FROM roles WHERE id=$1 LIMIT 1`, id)
 	if err != nil {
 		logger.WithField("err", err.Error()).Error("Error selecting role from database in GetRoleByID for id ", string(id))
+		return
 	}
 	return
 }
@@ -25,23 +26,7 @@ func (s *pgStore) GetRoleByName(ctx context.Context, name string) (role Role, er
 	err = s.db.Get(&role, `SELECT * FROM roles WHERE name=$1 LIMIT 1`, name)
 	if err != nil {
 		logger.WithField("err", err.Error()).Error("Error selecting role from database in GetRoleByName for name ", name)
-	}
-	return
-}
-
-/*// GetUserByID - Given the database ID for that user, look them up.
-func (s *pgStore) GetUserByID(ctx context.Context, id int) (user User, err error) {
-	err = s.db.Get(&user, `SELECT * FROM users WHERE id=$1 LIMIT 1`, id)
-	if err != nil {
-		logger.WithField("err", err.Error()).Error("Error selecting user from database by id " + string(id))
 		return
 	}
-	// Populate the user's organization data
-	org := Organization{}
-	err = s.db.Get(&org, `SELECT * FROM organizations WHERE id=$1`, user.OrgID)
-	if err != nil {
-		logger.WithField("err", err.Error()).Error("Error selecting user's organization from database by user id and org id " + string(id) + " " + string(user.OrgID))
-	}
 	return
 }
-*/
