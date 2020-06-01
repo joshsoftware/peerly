8414ca9de48fe75767e250ea9888aed3ea092da3
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-01 16:07:03 +0530

    updated styling of core value component

--- react-frontend/src/shared-components/core-value/coreValue.js
+++ react-frontend/src/shared-components/core-value/coreValue.js
@@ -2,27 +2,35 @@ import React from "react";
 import PropTypes from "prop-types";
 import Styled from "styled-components";
 
-import { Form } from "core-components/form/FormComponent";
+import CoreValueIcon from "shared-components/core-value-icon/coreValueIconComponent";
+import { Row, Container } from "core-components/grid/GridComponent";
 
 const Wrapper = Styled.div`
-max-width:75px;
-min-height:75px;
-border-radius: 5px;
-border: 3px solid black;
-display: flex;
-text-align: center;
-font-size: 0.75em;
+  max-width:75px;
+  min-height:75px;
+  border-radius: 10px;
+  border: 3px solid black;
+  display: flex;
+  text-align: center;
+  font-size: 0.75em;
 `;
 
-const CoreValueComponent = ({
-  coreValueName,
-}) => (
-  <Wrapper>
-    <Form.Label>
-    {coreValueName}
-    </Form.Label>
-  </Wrapper>
-);
+const CoreValueComponent = ({ coreValueName }) => {
+  const onClick = () => {
+    onClick(10);
+  };
+
+  return (
+    <Wrapper onClick={onClick}>
+      <Container>
+        <Row className="justify-content-center mt-1">
+          <CoreValueIcon size="30" color="red" />
+        </Row>
+        <Row className="justify-content-center mt-2">{coreValueName}</Row>
+      </Container>
+    </Wrapper>
+  );
+};
 
 CoreValueComponent.propTypes = {
   coreValueName: PropTypes.string.isRequired,
