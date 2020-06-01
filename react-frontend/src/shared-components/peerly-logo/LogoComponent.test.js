64e8326f7946b229e53c7109598bfa3ebbdd5d5f
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-01 14:23:29 +0530

    updated test case for peerly text

--- react-frontend/src/shared-components/peerly-logo/LogoComponent.test.js
+++ react-frontend/src/shared-components/peerly-logo/LogoComponent.test.js
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 
 import LogoComponent from "shared-components/peerly-logo/LogoComponent";
 
-it("should equal to + Peerly text", () => {
+it("should equal to Peerly text", () => {
   const { getByTestId } = render(<LogoComponent />);
   expect(getByTestId("peerlyLogoComponent")).toHaveTextContent("Peerly");
 });
