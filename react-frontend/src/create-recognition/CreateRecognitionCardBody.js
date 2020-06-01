2903096e7ebb1bc387bd97abc876005f0f59864a
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-01 16:05:29 +0530

    created card body component for create recognition

--- react-frontend/src/create-recognition/CreateRecognitionCardBody.js
+++ react-frontend/src/create-recognition/CreateRecognitionCardBody.js
@@ -1,9 +1,11 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { useState } from "react";
 
 import { Card } from "core-components/card/card";
-import CoreValue from "create-recognition/coreValues"
-import {Row, Col} from "core-components/grid/GridComponent"
+import { Form } from "core-components/form/FormComponent";
+import CoreValue from "create-recognition/coreValues";
+import { Row } from "core-components/grid/GridComponent";
+import { Button } from "core-components/button/ButtonComponent";
 
 const coreValues = [
   {
@@ -21,18 +23,32 @@ const coreValues = [
   {
     id: 4,
     coreValueName: "core Value Name 4",
-  }
-]
+  },
+];
 
-const CreateRecognitionCardBody = () =>
-  (
-    <Card.Body className="d-flex flex row justify-content-around">
-      <CoreValue coreValues={coreValues}/>
+const CreateRecognitionCardBody = () => {
+  const [comment, addComment] = useState(false);
+  const onClickAddComment = () => {
+    addComment(true);
+  };
+  return (
+    <Card.Body>
+      <Row className="space-around ml-5"> Select Value </Row>
+      <Row className="justify-content-around mt-4">
+        <CoreValue coreValues={coreValues} />
+      </Row>
+      <Row className="justify-content-center mt-5">
+        {comment ? (
+          <Form.Control as="textarea" rows="3"></Form.Control>
+        ) : (
+          <Button onClick={onClickAddComment}> Add Comments </Button>
+        )}
+      </Row>
+      <Row className="justify-content-center mt-5">
+        <Button> Done </Button>
+      </Row>
     </Card.Body>
-  )
-
-CreateRecognitionCardBody.propTypes = {
-
+  );
 };
 
 export default CreateRecognitionCardBody;
