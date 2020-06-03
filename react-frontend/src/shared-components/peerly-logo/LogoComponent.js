244e23a24fa7d9f5318f6543017eb721407eea4d
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-03 12:23:38 +0530

    added logo component with updated props

--- react-frontend/src/shared-components/peerly-logo/LogoComponent.js
+++ react-frontend/src/shared-components/peerly-logo/LogoComponent.js
@@ -14,7 +14,9 @@ const Plus = styled.div`
     left: 5%;
     transform: rotate(45deg);
     font-size: 45px;
-    color: ${(props) => props.theme.text};
+    color: ${({ theme }) =>
+      (theme.text === "dark" && "var(--black)") ||
+      (theme.text === "light" && "var(--white)")};
   }
 `;
 
@@ -30,7 +32,10 @@ const Logo = styled.div`
     content: " ";
     width: 50px;
     height: 50px;
-    border: 3px solid ${(props) => props.theme.text};
+    border: 3px solid
+      ${({ theme }) =>
+        (theme.text === "dark" && "var(--black)") ||
+        (theme.text === "light" && "var(--white)")};
     position: absolute;
     transform: translate(-50%, -50%);
     top: 50%;
@@ -42,7 +47,9 @@ const PeerlyText = styled.div`
   & {
     position: relative;
     margin-top: 80px;
-    color: ${(props) => props.theme.text};
+    color: ${({ theme }) =>
+      (theme.text === "dark" && "var(--black)") ||
+      (theme.text === "light" && "var(--white)")};
     font-size: 45px;
 `;
 
@@ -63,12 +70,14 @@ const LogoComponent = ({ theme }) => (
 
 LogoComponent.defaultProps = {
   theme: {
-    text: "var(--white)",
+    text: "light",
   },
 };
 
 LogoComponent.propTypes = {
-  theme: PropTypes.object,
+  theme: PropTypes.shape({
+    text: PropTypes.oneOf(["dark", "light"]),
+  }),
 };
 
 export default React.memo(LogoComponent);
