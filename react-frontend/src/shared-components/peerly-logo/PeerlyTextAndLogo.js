4f4cf29ca50f0363f3e94f6d27f92e94ab259656
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-03 19:34:39 +0530

    created file for wrapping text and logo

---
+++ react-frontend/src/shared-components/peerly-logo/PeerlyTextAndLogo.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import PeerlyLogo from "shared-components/peerly-logo/PeerlyLogo";
+import PeerlyTextComponent from "shared-components/peerly-logo/PeerlyTextComponent";
+
+const PeerlyTextAndLogo = ({ theme, fontSize }) => (
+  <>
+    <div data-testid="peerlyLogoComponent">
+      <PeerlyLogo theme={theme} fontSize={fontSize} />
+    </div>
+    <div className="text-center">
+      <PeerlyTextComponent theme={theme} fontSize={fontSize} />
+    </div>
+  </>
+);
+
+PeerlyTextAndLogo.defaultProps = {
+  theme: {
+    variant: "dark",
+  },
+  fontSize: "48px",
+};
+
+PeerlyTextAndLogo.propTypes = {
+  theme: PropTypes.shape({
+    variant: PropTypes.oneOf(["dark", "light"]),
+  }),
+  fontSize: PropTypes.string,
+};
+
+export default React.memo(PeerlyTextAndLogo);
