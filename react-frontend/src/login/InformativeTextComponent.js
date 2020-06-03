7a64351350ec7d59c883bd05a8ae5334f571860d
Avinash Mane <avimanepatil@gmail.com>
2020-06-03 19:16:31 +0530

    added snapshot test case

--- react-frontend/src/login/InformativeTextComponent.js
+++ react-frontend/src/login/InformativeTextComponent.js
@@ -2,39 +2,40 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-import { Row } from "core-components/grid/GridComponent";
-
 const HrLine = styled.hr`
   width: 10%;
-  background: white;
+  background: ${({ theme }) => (theme === "dark" ? "white" : "black")};
 `;
 
-const H3 = styled.h3`
-  margin: 2% 20% 0%;
+const Div = styled.div`
+  color: ${({ theme }) => (theme === "dark" ? "white" : "black")};
+  background-color: ${({ theme }) => (theme === "dark" ? "black" : "white")};
 `;
 
 const InformativeTextComponent = ({
+  theme,
   className,
   informativeText,
   encouragementThought,
 }) => (
-  <Row className={className}>
-    <H3>{informativeText}</H3>
-    <HrLine />
+  <Div theme={theme} className={className}>
+    <h3>{informativeText}</h3>
+    <HrLine theme={theme} />
     <h6>{encouragementThought}</h6>
-  </Row>
+  </Div>
 );
 
 InformativeTextComponent.defaultProps = {
   informativeText: "Lets Create the Office Positive",
   encouragementThought: "Encouragement Driven",
-  className: "text-white bg-dark text-center d-flex flex-column",
+  theme: "dark",
 };
 
 InformativeTextComponent.propTypes = {
   className: PropTypes.string,
   informativeText: PropTypes.string.isRequired,
   encouragementThought: PropTypes.string.isRequired,
+  theme: PropTypes.oneOf(["dark", "light"]),
 };
 
 export default React.memo(InformativeTextComponent);
--- react-frontend/src/login/InformativeTextComponent.test.js
+++ react-frontend/src/login/InformativeTextComponent.test.js
@@ -2,15 +2,12 @@ import React from "react";
 import { render } from "@testing-library/react";
 import InformativeTextComponent from "login/InformativeTextComponent";
 
-it("should equal informative text", () => {
-  const { getByText } = render(
+it("should render informative text component", () => {
+  const { asFragment } = render(
     <InformativeTextComponent
-      InformativeText="Lets Create the Office Positive"
+      informativeText="Lets Create the Office Positive"
       encouragementThought="thought"
     />
   );
-  expect(getByText("Lets Create the Office Positive")).toHaveTextContent(
-    "Lets Create the Office Positive"
-  );
-  expect(getByText("thought")).toHaveTextContent("thought");
+  expect(asFragment()).toMatchSnapshot();
 });
