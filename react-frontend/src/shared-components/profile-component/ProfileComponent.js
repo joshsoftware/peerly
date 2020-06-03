563cb727e86d77c8d5b12536112cd0fc497d5232
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-06-03 13:57:59 +0530

    update ProfileComponent for set responsive image

--- react-frontend/src/shared-components/profile-component/ProfileComponent.js
+++ react-frontend/src/shared-components/profile-component/ProfileComponent.js
@@ -8,7 +8,7 @@ import { Col, Row } from "core-components/grid/GridComponent";
 
 const ImageComponent = styled(Image)`
   height: inherit;
-  width: 100%;
+  width: inherit;
 }
 `;
 
@@ -22,7 +22,7 @@ const ProfileComponent = ({ className, text, imageSrc, key, alt }) => (
         roundedCircle={true}
       />
     </Col>
-    <Col className="w-50 text-center col">
+    <Col className="w-50 text-center">
       <Form.Label>{text}</Form.Label>
     </Col>
   </Row>
@@ -30,7 +30,6 @@ const ProfileComponent = ({ className, text, imageSrc, key, alt }) => (
 
 ProfileComponent.propTypes = {
   className: PropTypes.string,
-  onClick: PropTypes.func.isRequired,
   imageSrc: PropTypes.string.isRequired,
   text: PropTypes.string,
   name: PropTypes.string,
@@ -39,8 +38,7 @@ ProfileComponent.propTypes = {
 };
 
 ProfileComponent.defaultProps = {
-  imageSrc:
-    "https://public-v2links.adobecc.com/b8bcaf62-377d-428f-41ee-f038352e2a2e/component?params=component_id%3A5bfa76ea-8d11-4647-9d07-06db873f7685&params=version%3A0&token=1591158470_da39a3ee_052fa588be615b623a6dc87ade964e437182cbae&api_key=CometServer1",
+  imageSrc: require("./profile.png"),
   text: "Employee Name",
   className: "d-flex justify-content-center align-items-center h-100",
 };
