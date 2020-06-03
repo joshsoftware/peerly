532233e251332463bc36433c2549051d6fe91e4e
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-03 19:33:13 +0530

    updated plus sign component

--- react-frontend/src/shared-components/plus-sign/PlusSign.js
+++ react-frontend/src/shared-components/plus-sign/PlusSign.js
@@ -1,5 +1,41 @@
 import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
 
-const PlusSign = () => <div data-testid="peerlyplussign">+</div>;
+const PlusSignWrapper = styled.div`
+   {
+    transform: ${({ transform }) => transform};
+    font-size: ${({ fontSize }) => fontSize};
+    color: ${({ theme }) =>
+      (theme.variant === "dark" && "var(--white)") ||
+      (theme.variant === "light" && "var(--black)")};
+  }
+`;
+
+const PlusSign = ({ theme, transform, fontSize }) => (
+  <PlusSignWrapper
+    theme={theme}
+    transform={transform}
+    fontSize={fontSize}
+    data-testid="plusSignText"
+  >
+    +
+  </PlusSignWrapper>
+);
+
+PlusSign.defaultProps = {
+  theme: {
+    variant: "dark",
+  },
+  fontSize: "48px",
+};
+
+PlusSign.propTypes = {
+  theme: PropTypes.shape({
+    text: PropTypes.oneOf(["dark", "light"]),
+  }),
+  transform: PropTypes.string,
+  fontSize: PropTypes.string,
+};
 
 export default React.memo(PlusSign);
