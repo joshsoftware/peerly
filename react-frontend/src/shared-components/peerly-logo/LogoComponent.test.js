be51e94a7a3df016d470ce0c6750c97119cabaf5
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-03 19:32:39 +0530

    deleted old test file fo logo component

--- react-frontend/src/shared-components/peerly-logo/LogoComponent.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-
-import LogoComponent from "shared-components/peerly-logo/LogoComponent";
-
-it("should equal to Peerly text", () => {
-  const { getByTestId } = render(<LogoComponent />);
-  expect(getByTestId("peerlyLogoComponent")).toHaveTextContent("Peerly");
-});
