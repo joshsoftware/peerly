73123b02bea1b9a16e253fb5eb064e3eeb133b03
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-04 17:02:33 +0530

    deleted left panel

--- react-frontend/src/shared-components/left-panel-user-profile/LeftPanelCardHeader.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import { Card } from "core-components/card/card";
-
-const LeftPanelCardHeader = () => <Card.Header></Card.Header>;
-
-export default React.memo(LeftPanelCardHeader);
