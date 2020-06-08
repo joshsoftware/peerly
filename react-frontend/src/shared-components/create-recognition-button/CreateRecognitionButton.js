8c8952251b29fb2990127df7b68798c31f6dbb08
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-08 16:54:24 +0530

    added changes by importing peerly logo

--- react-frontend/src/shared-components/create-recognition-button/CreateRecognitionButton.js
+++ react-frontend/src/shared-components/create-recognition-button/CreateRecognitionButton.js
@@ -4,46 +4,52 @@ import PropTypes from "prop-types";
 
 import PlusSign from "shared-components/plus-sign/PlusSign"; //todo depend upon peerly logo
 
+const WrapperCircle = styled.div`
+  background: var(--white) 0% 0% no-repeat padding-box;
+  box-shadow: 0px 3px 6px var(--box-shadow-color);
+  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+  position: relative;
+`;
+
 const ButtonComponent = styled.div`
-  background: transparent url(${(props) => props.imageUrl}) 90% 60% no-repeat
-  opacity: 1;
+  background: transparent url(${(props) => props.imageUrl})
+  97% 50% no-repeat
+  padding-box;
   box-shadow: 0px 3px 6px #0000004e;
   width: 45px;
   height: 45px;
   background-color: var(--rust);
   border: none;
   border-radius: 50%;
-  margin-right:6px;
+  ”
 `;
 
-const PlusSignWrapper = styled(PlusSign)`
-  margin-left: 10px;
-  margin-top: 8px;
-`;
-
-const WrapperCircle = styled.div`
-  background: var(--white) 0% 0% no-repeat padding-box;
-  box-shadow: 0px 3px 6px var(--box-shadow-color);
-  opacity: 1;
-  border-radius: 50%;
-  width: 60px;
-  height: 60px;
-`;
-
-const CreateRecognitionButton = ({ onClick, imageUrl, theme }) => {
+const CreateRecognitionButton = ({ onClick, imageUrl, theme, fontSize }) => {
   return (
-    <WrapperCircle className="m-auto d-flex justify-content-center align-content-center">
-      <ButtonComponent className="m-auto" onClick={onClick} imageUrl={imageUrl}>
-        <PlusSignWrapper fontSize="23px" theme={theme} />
+    <WrapperCircle className="m-auto justify-content-center ">
+      <ButtonComponent
+        imageUrl={imageUrl}
+        className="m-auto text-center"
+        onClick={onClick}
+      >
+        <PlusSign fontSize={fontSize} theme={theme} />
       </ButtonComponent>
     </WrapperCircle>
   );
 };
 
+CreateRecognitionButton.defaultProps = {
+  theme: "light",
+  fontSize: "27px",
+};
+
 CreateRecognitionButton.propTypes = {
   onClick: PropTypes.func.isRequired,
   imageUrl: PropTypes.string.isRequired,
-  theme: PropTypes.oneOfoneOf(["dark", "light"]),
+  theme: PropTypes.oneOf(["dark", "light"]),
+  fontSize: PropTypes.string,
 };
 
 export default React.memo(CreateRecognitionButton);
