c371bde6fc2787f88bd54bc866b19ef59bd5b1fb
Avinash Mane <avimanepatil@gmail.com>
2020-06-10 12:56:26 +0530

    resolved comments

--- react-frontend/src/login/PreLoginDashboardComponent.js
+++ react-frontend/src/login/PreLoginDashboardComponent.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import PropTypes from "prop-types";
 
 import PeerlyTextAndLogo from "shared-components/peerly-logo/PeerlyTextAndLogo";
 import InformativeTextComponent from "login/InformativeTextComponent";
@@ -8,35 +7,24 @@ import LoginImageComponent from "login/LoginImageComponent";
 
 const Wrapper = styled.div`
   height: 100vh;
-  background-color: ${({ theme }) =>
-    theme === "dark" ? "var(--atomic)" : "var(--white)"};
+  background-color: var(--atomic);
 `;
 
-const PreLoginDashboardComponent = ({ theme, className }) => (
+const PreLoginDashboardComponent = () => (
   <Wrapper
-    theme={theme}
-    className={`d-sm-block d-xs-block d-md-none flex-column d-flex justify-content-around ${className}`}
+    className="d-md-none flex-column d-flex justify-content-around"
     data-testid="officeInfo"
   >
     <div>
-      <PeerlyTextAndLogo theme={theme} />
+      <PeerlyTextAndLogo theme="dark" />
     </div>
     <LoginImageComponent />
     <InformativeTextComponent
-      theme={theme}
+      theme="dark"
       informativeText="Lets Create the Office Positive"
       encouragementThought="Encouragement Driven"
     />
   </Wrapper>
 );
-
-PreLoginDashboardComponent.propTypes = {
-  theme: PropTypes.oneOf(["dark", "light"]),
-  className: PropTypes.string,
-};
-
-PreLoginDashboardComponent.defaultProps = {
-  theme: "dark",
-};
 
 export default React.memo(PreLoginDashboardComponent);
--- react-frontend/src/login/PreLoginDashboardComponent.test.js
+++ react-frontend/src/login/PreLoginDashboardComponent.test.js
@@ -2,34 +2,9 @@ import React from "react";
 import { render } from "@testing-library/react";
 import PreLoginDashboardComponent from "login/PreLoginDashboardComponent";
 
-describe("pre login panel component test", () => {
-  test("should render pre login panel component", () => {
-    const { asFragment, getByTestId } = render(
-      <PreLoginDashboardComponent
-        informativeText="Lets Create the Office Positive"
-        encouragementThought="thought"
-      />
-    );
+describe("pre login dashboard component test", () => {
+  test("should render pre login dashboard component", () => {
+    const { asFragment } = render(<PreLoginDashboardComponent />);
     expect(asFragment()).toMatchSnapshot();
-    expect(getByTestId("officeInfo")).toHaveStyleRule(
-      "background-color",
-      "var(--atomic)"
-    );
-  });
-
-  test("test case for style and classes", () => {
-    const { getByTestId } = render(
-      <PreLoginDashboardComponent
-        theme="light"
-        className="text-uppercase"
-        informativeText="Lets Create the Office Positive"
-        encouragementThought="thought"
-      />
-    );
-    expect(getByTestId("officeInfo")).toHaveStyleRule(
-      "background-color",
-      "var(--white)"
-    );
-    expect(getByTestId("officeInfo")).toHaveClass("text-uppercase");
   });
 });
