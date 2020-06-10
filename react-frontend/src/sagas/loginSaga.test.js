7f0f41b41e76c61ad5d8a3293282d3e00ad75122
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-10 07:40:29 +0530

    added test cases of login saga

--- react-frontend/src/sagas/loginSaga.test.js
+++ react-frontend/src/sagas/loginSaga.test.js
@@ -1,29 +1,89 @@
-import { takeEvery, call } from "redux-saga/effects";
+import { put, takeEvery, call } from "redux-saga/effects";
 import { loginApi, userLogin } from "sagas/loginSaga";
 import PostJson from "utils/postJson";
+import actionGenerator from "utils/actionGenerator";
+import SuccessResponse from "../../../mock-responses/login/success_response.json";
+import UnauthorizedResponse from "../../../mock-responses/login/unauthorized_response.json";
+import InternalServerError from "../../../mock-responses/login/internal_server_error.json";
 
 describe("SAGAS", () => {
+  const actionStatus = actionGenerator("LOGIN");
+
+  const mockRequest = {
+    payload: {
+      access_token: "xxxxx",
+    },
+  };
+
   it("should dispatch action 'LOGIN_API' ", () => {
     const generator = loginApi();
     expect(generator.next().value).toEqual(takeEvery("LOGIN_API", userLogin));
     expect(generator.next().done).toBeTruthy();
   });
 
-  it("should dispatch action 'LOGIN_API' with result from fetch users API", () => {
-    const mockResponse = {
-      type: "LOGIN_API",
-      payload: {
-        access_token: "Some content",
-      },
-    };
-    const generator = userLogin(mockResponse);
+  it("should return with mock success result", () => {
+    const response = { json: () => "success", status: 200 };
+    const generator = userLogin(mockRequest);
     expect(generator.next().value).toEqual(
       call(PostJson, {
         path: "/oauth/google",
-        paramsObj: { access_token: mockResponse.payload },
+        paramsObj: { access_token: mockRequest.payload },
       })
     );
-    generator.next();
-    expect(generator.next().done).toEqual(true);
+    generator.next(response).value;
+    expect(generator.next(SuccessResponse).value).toEqual(
+      put({
+        type: actionStatus.success,
+        payload: {
+          status: response.status,
+          value: SuccessResponse.data,
+        },
+      })
+    );
+    expect(generator.next().done).toBeTruthy();
+  });
+
+  it("should return with mock unauthorized error", () => {
+    const response = { json: () => "unauthorized", status: 401 };
+    const generator = userLogin(mockRequest);
+    expect(generator.next().value).toEqual(
+      call(PostJson, {
+        path: "/oauth/google",
+        paramsObj: { access_token: mockRequest.payload },
+      })
+    );
+    generator.next(response).value;
+    expect(generator.next(UnauthorizedResponse).value).toEqual(
+      put({
+        type: actionStatus.failure,
+        payload: {
+          status: response.status,
+          value: UnauthorizedResponse.message,
+        },
+      })
+    );
+    expect(generator.next().done).toBeTruthy();
+  });
+
+  it("should return with mock internal server error", () => {
+    const response = { json: () => "internal server error", status: 500 };
+    const generator = userLogin(mockRequest);
+    expect(generator.next().value).toEqual(
+      call(PostJson, {
+        path: "/oauth/google",
+        paramsObj: { access_token: mockRequest.payload },
+      })
+    );
+    generator.next(response).value;
+    expect(generator.next(InternalServerError).value).toEqual(
+      put({
+        type: actionStatus.failure,
+        payload: {
+          status: response.status,
+          value: InternalServerError.message,
+        },
+      })
+    );
+    expect(generator.next().done).toBeTruthy();
   });
 });
