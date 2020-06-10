22b66ed0604cf21e7a741db525dd9eb45a1c0d25
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-10 07:38:00 +0530

    resolved errors in login container

--- react-frontend/src/login/LoginContainer.js
+++ react-frontend/src/login/LoginContainer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-import Login from "login/LoginComponent"; /*to do this component is depent on login presentational component */
+import LoginComponent from "login/LoginComponent"; /*to do this component is depent on login presentational component */
 import { store } from "root/redux-store";
 import UnauthorisedErrorComponent from "shared-components/UnauthorisedErrorComponent";
 import InternalServerErrorComponent from "shared-components/InternalServerErrorComponent";
+import actionGenerator from "utils/actionGenerator";
+import RecognitionList from "recognition-list/RecognitionListContainer"; //todo apply path after recognition list container
 
 const LoginContainer = () => {
   const loginAuthorization = useSelector((state) => state.loginReducer);
@@ -13,19 +15,26 @@ const LoginContainer = () => {
     store.dispatch({ type: "LOGIN_API", payload: tokenObj.access_token });
   };
 
-  const responseGoogleOnFailure = (error) => {
-    store.dispatch({ type: "LOGIN_FAILURE", value: error });
+  const responseGoogleOnFailure = (message) => {
+    const actionStatus = actionGenerator("LOGIN");
+    store.dispatch({
+      type: actionStatus.failure,
+      payload: {
+        value: { message: message.error },
+      },
+    });
   };
   if (loginAuthorization.status === 401) {
     return <UnauthorisedErrorComponent />;
-  } else if (loginAuthorization.error.error === "popup_closed_by_user") {
-    return <UnauthorisedErrorComponent />;
-  } else if (loginAuthorization.error.message === 500) {
+  } else if (loginAuthorization.status === 500) {
     return <InternalServerErrorComponent />;
+  } else if (loginAuthorization.error.message === "popup_closed_by_user") {
+    return <UnauthorisedErrorComponent />;
+  } else if (loginAuthorization.status === 200) {
+    return <RecognitionList />;
   }
   return (
-    <Login
-      data-testid="LoginContainer"
+    <LoginComponent
       responseGoogleOnSuccess={responseGoogleOnSuccess}
       responseGoogleOnFailure={responseGoogleOnFailure}
     />
