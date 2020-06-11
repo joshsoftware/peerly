22ca1160eceda378a68aa86551354d3032f430e6
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-11 12:02:03 +0530

    removed dependency of recognition list container and removed status code 201

--- react-frontend/src/login/LoginContainer.js
+++ react-frontend/src/login/LoginContainer.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-import LoginComponent from "login/LoginComponent"; /*to do this component is depent on login presentational component */
+import LoginComponent from "login/LoginComponent"; /*TODO: this component is depent on login presentational component */
 import { store } from "root/redux-store";
 import UnauthorisedErrorComponent from "shared-components/UnauthorisedErrorComponent";
 import InternalServerErrorComponent from "shared-components/InternalServerErrorComponent";
 import actionGenerator from "utils/actionGenerator";
-import RecognitionList from "recognition-list/RecognitionListContainer"; //todo apply path after recognition list container
 
 const LoginContainer = () => {
   const loginAuthorization = useSelector((state) => state.loginReducer);
@@ -30,18 +29,15 @@ const LoginContainer = () => {
     return <InternalServerErrorComponent />;
   } else if (loginAuthorization.error.message === "popup_closed_by_user") {
     return <UnauthorisedErrorComponent />;
-  } else if (
-    loginAuthorization.status === 200 ||
-    loginAuthorization.status === 201
-  ) {
-    return <RecognitionList />; //TODO depends upon recognition list container
+  } else if (loginAuthorization.status === 200) {
+    return "Successful"; //TODO: redirect to dashboard
   }
   return (
     <LoginComponent
       responseGoogleOnSuccess={responseGoogleOnSuccess}
       responseGoogleOnFailure={responseGoogleOnFailure}
     />
-  ); //TODO depends upon login presentational component
+  ); //TODO: depends upon login presentational component
 };
 
 export default React.memo(LoginContainer);
