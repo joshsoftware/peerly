50ecee816ffcf0a4428a3a187147a5cc43bad804
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-11 18:08:53 +0530

    updated test cases for login saga

--- react-frontend/src/sagas/loginSaga.test.js
+++ react-frontend/src/sagas/loginSaga.test.js
@@ -1,13 +1,15 @@
-import { put, takeEvery, call } from "redux-saga/effects";
+import { put, takeLatest, call } from "redux-saga/effects";
+import { LOGIN_API, LOGIN } from "constants/actionConstants";
 import { loginApi, userLogin } from "sagas/loginSaga";
 import PostJson from "utils/postJson";
 import actionGenerator from "utils/actionGenerator";
 import SuccessResponse from "../../../mock-responses/login/success_response.json";
 import UnauthorizedResponse from "../../../mock-responses/login/unauthorized_response.json";
 import InternalServerError from "../../../mock-responses/login/internal_server_error.json";
+import actionObjectGenerator from "actions/actionObjectGenerator";
 
-describe("SAGAS", () => {
-  const actionStatus = actionGenerator("LOGIN");
+describe("SAGAS test cases", () => {
+  const actionStatus = actionGenerator(LOGIN);
 
   const mockRequest = {
     payload: {
@@ -17,13 +19,19 @@ describe("SAGAS", () => {
 
   it("should dispatch action 'LOGIN_API' ", () => {
     const generator = loginApi();
-    expect(generator.next().value).toEqual(takeEvery("LOGIN_API", userLogin));
+    expect(generator.next().value).toEqual(takeLatest(LOGIN_API, userLogin));
     expect(generator.next().done).toBeTruthy();
   });
 
   it("should return with mock success result", () => {
     const response = { json: () => "success", status: 200 };
     const generator = userLogin(mockRequest);
+
+    const dispatchObject = actionObjectGenerator(actionStatus.success, {
+      status: response.status,
+      value: SuccessResponse.data,
+    });
+
     expect(generator.next().value).toEqual(
       call(PostJson, {
         path: "/oauth/google",
@@ -31,21 +39,19 @@ describe("SAGAS", () => {
       })
     );
     generator.next(response).value;
-    expect(generator.next(SuccessResponse).value).toEqual(
-      put({
-        type: actionStatus.success,
-        payload: {
-          status: response.status,
-          value: SuccessResponse.data,
-        },
-      })
-    );
+    expect(generator.next(SuccessResponse).value).toEqual(put(dispatchObject));
     expect(generator.next().done).toBeTruthy();
   });
 
   it("should return with mock unauthorized error", () => {
     const response = { json: () => "unauthorized", status: 401 };
     const generator = userLogin(mockRequest);
+
+    const dispatchObject = actionObjectGenerator(actionStatus.failure, {
+      status: response.status,
+      value: UnauthorizedResponse.message,
+    });
+
     expect(generator.next().value).toEqual(
       call(PostJson, {
         path: "/oauth/google",
@@ -54,13 +60,7 @@ describe("SAGAS", () => {
     );
     generator.next(response).value;
     expect(generator.next(UnauthorizedResponse).value).toEqual(
-      put({
-        type: actionStatus.failure,
-        payload: {
-          status: response.status,
-          value: UnauthorizedResponse.message,
-        },
-      })
+      put(dispatchObject)
     );
     expect(generator.next().done).toBeTruthy();
   });
@@ -68,6 +68,11 @@ describe("SAGAS", () => {
   it("should return with mock internal server error", () => {
     const response = { json: () => "internal server error", status: 500 };
     const generator = userLogin(mockRequest);
+
+    const dispatchObject = actionObjectGenerator(actionStatus.failure, {
+      status: response.status,
+      value: InternalServerError.message,
+    });
     expect(generator.next().value).toEqual(
       call(PostJson, {
         path: "/oauth/google",
@@ -76,13 +81,7 @@ describe("SAGAS", () => {
     );
     generator.next(response).value;
     expect(generator.next(InternalServerError).value).toEqual(
-      put({
-        type: actionStatus.failure,
-        payload: {
-          status: response.status,
-          value: InternalServerError.message,
-        },
-      })
+      put(dispatchObject)
     );
     expect(generator.next().done).toBeTruthy();
   });
