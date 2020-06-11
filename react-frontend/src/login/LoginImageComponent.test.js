dbdf52d49675f5c303f68f176ae1b3b50d820fed
Avinash Mane <avimanepatil@gmail.com>
2020-06-11 08:02:15 +0530

    change in  image test case

--- react-frontend/src/login/LoginImageComponent.test.js
+++ react-frontend/src/login/LoginImageComponent.test.js
@@ -2,8 +2,6 @@ import React from "react";
 import { render } from "@testing-library/react";
 
 import LoginImageComponent from "login/LoginImageComponent";
-import { IMG_BASE_PATH } from "constants/appConstants";
-import Image from "../../public/assets/images/cat-img.png";
 
 describe("Image component test", () => {
   test("renders image component with image", () => {
@@ -15,6 +13,6 @@ describe("Image component test", () => {
   test("render image with correct size", () => {
     const { getByAltText } = render(<LoginImageComponent size="lg" />);
     const testImage = getByAltText("login image");
-    expect(testImage.src).toBe(`http://localhost${IMG_BASE_PATH}/${Image}`);
+    expect(testImage.src).toBe("http://localhost/assets/images//cat-img.png");
   });
 });
