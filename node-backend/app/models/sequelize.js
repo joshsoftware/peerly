4fdf05799b9f1a3d97489e530723a91a26ec4954
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-06-12 16:46:32 +0530

    update  complete code of sequelize.js file as per optimize code

--- node-backend/app/models/sequelize.js
+++ node-backend/app/models/sequelize.js
@@ -1,39 +1,45 @@
-const Sequelize = require("sequelize");
+"use strict";
 
-const dbConfig = require("../config/db.config.js");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const basename = path.basename(__filename);
+const config = require(__dirname + "/../config/db.config");
+const db = {};
+let sequelize;
+
+sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+  host: config.HOST,
+  dialect: config.dialect,
   operatorsAliases: false,
   pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
+    max: config.pool.max,
+    min: config.pool.min,
+    acquire: config.pool.acquire,
+    idle: config.pool.idle,
   },
 });
-const db = {};
-db.Sequelize = Sequelize;
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    if (file !== "demo_sequelize.js") {
+      const model = sequelize["import"](path.join(__dirname, file));
+      db[model.name] = model;
+    }
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
 db.sequelize = sequelize;
-db.roles = require("./roles.model.js")(sequelize, Sequelize);
-db.organizations = require("./organizations.model.js")(sequelize, Sequelize);
-db.users = require("./users.model.js")(sequelize, Sequelize);
-db.core_value = require("./core_values.model")(sequelize, Sequelize);
-db.badges = require("./badges.model")(sequelize, Sequelize);
-db.user_blacklisted_tokens = require("./user_blacklisted_tokens.model")(
-  sequelize,
-  Sequelize
-);
-db.recognitions = require("./recognitions.model")(sequelize, Sequelize);
-db.reported_recognitions = require("./reported_recognitions.model")(
-  sequelize,
-  Sequelize
-);
-db.recognition_moderation = require("./recognition_moderation.model")(
-  sequelize,
-  Sequelize
-);
-db.coreValues = require("./core_values.model")(sequelize, Sequelize);
-db.recognition_hi5 = require("./recognition_hi5.model")(sequelize, Sequelize);
+db.Sequelize = Sequelize;
 
 module.exports = db;
