77ae6e08b789b1fa8b7d8b1b21bb5af07100eadf
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-15 12:16:14 +0530

    added model files

---
+++ node-backend/models/recognition_hi5.model.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, Sequelize) => {
+  const Recognition_hi5 = sequelize.define(
+    "recognition_hi5",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        notNull: true,
+      },
+      recognition_id: {
+        type: Sequelize.INTEGER,
+        notNull: true,
+      },
+      comment: {
+        type: Sequelize.STRING,
+      },
+      given_by: {
+        type: Sequelize.INTEGER,
+        notNull: true,
+      },
+      given_at: {
+        type: Sequelize.BIGINT,
+        notNull: true,
+      },
+    },
+    {
+      timestamp: false,
+      createdAt: false,
+      updatedAt: false,
+      freezeTableName: true,
+    }
+  );
+  return Recognition_hi5;
+};
