d0205f62389266c06e671f3fbb3200d3d1c9d524
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-06-15 12:41:45 +0530

    add all constant are use for coreValueController

--- node-backend/app/constant/responseConstants.js
+++ node-backend/app/constant/responseConstants.js
@@ -5,3 +5,8 @@ module.exports.INTRENAL_SERVER_ERROR_MESSAGE = "internal server error";
 module.exports.INVALID_CORE_VALUE_CODE = "invalid-core-value";
 
 module.exports.INVALID_CORE_VALUE_MESSAGE = "Invalid core value data";
+
+module.exports.CORE_VALUE_NOT_FOUND_CODE = "core-value-not-found";
+
+module.exports.CORE_VALUE_NOT_FOUND_MESSAGE =
+  "core value not found for specified id";
