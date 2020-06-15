722f4cad2adc66fc8fc347ab95f18e0c16873f44
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-15 11:38:34 +0530

    updated imports in reset hi5 quota file

--- node-backend/app/cronJobs/ResetHi5QuotaBalance.js
+++ node-backend/app/cronJobs/ResetHi5QuotaBalance.js
@@ -1,6 +1,6 @@
 var cron = require("node-cron");
 
-const db = require("../models/sequelize");
+const db = require("../../models/sequelize");
 cron.schedule(process.env.TIME_RESET_HI5_QUOTA, async () => {
   db.sequelize.query("select resetHi5Quota();");
 });
