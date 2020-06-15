b329ac5ac83e484bcf168eea37bebde7cddce3e7
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-15 12:34:59 +0530

    changed imported path to original folder

--- node-backend/app/jwtTokenValidation/deleteExpiredToken.js
+++ node-backend/app/jwtTokenValidation/deleteExpiredToken.js
@@ -1,7 +1,7 @@
 const moment = require("moment");
 var cron = require("node-cron");
 
-const db = require("../../models/sequelize");
+const db = require("../models/sequelize");
 cron.schedule(process.env.TOKEN_DELETION_INTERVAL, () => {
   db.sequelize.query(
     "delete from user_blacklisted_tokens where expires_at < " +
