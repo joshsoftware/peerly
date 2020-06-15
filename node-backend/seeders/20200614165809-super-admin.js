ec9bbcbf29ac6a51d71ab719a0ecabb0808b5806
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-15 15:17:10 +0530

    added seeder file for super admin

---
+++ node-backend/seeders/20200614165809-super-admin.js
@@ -0,0 +1,72 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface) => {
+    let organizationsCount = await queryInterface.sequelize.query(
+      "SELECT count(*) from organizations;"
+    );
+    if (organizationsCount[0][0].count > 0) {
+      return organizationsCount;
+    } else {
+      await queryInterface.bulkInsert(
+        "roles",
+        [
+          {
+            role: "SuperAdmin",
+          },
+          {
+            role: "OrganisationAdmin",
+          },
+          {
+            role: "Employee",
+          },
+          {
+            role: "Moderator",
+          },
+        ],
+        {}
+      );
+
+      await queryInterface.bulkInsert(
+        "organizations",
+        [
+          {
+            name: "josh software",
+            contact_email: "info@joshsoftware.com",
+            domain_name: "joshsoftware.com",
+            hi5_limit: 5,
+            subscription_status: 1,
+            subscription_valid_upto: 12239,
+            hi5_quota_renewal_frequency: "WEEKLY",
+            timezone: "india",
+          },
+        ],
+        {}
+      );
+
+      let organizationId = await queryInterface.sequelize.query(
+        "SELECT id, hi5_limit from organizations;"
+      );
+      return queryInterface.bulkInsert(
+        "users",
+        [
+          {
+            org_id: organizationId[0][0].id,
+            first_name: "admin",
+            email: "admin123@gmail.com",
+            soft_delete: false,
+            role_id: 1,
+            hi5_quota_balance: organizationId[0][0].hi5_limit,
+          },
+        ],
+        {}
+      );
+    }
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.bulkDelete("users", null, {});
+    await queryInterface.bulkDelete("organizations", null, {});
+    await queryInterface.bulkDelete("roles", null, {});
+  },
+};
