f730544eff728e060664ca6c6d808bf640be1a23
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-15 15:18:08 +0530

    added super admin seeding

--- node-backend/seeders/20200614165809-super-admin.js
+++ node-backend/seeders/20200614165809-super-admin.js
@@ -44,19 +44,19 @@ module.exports = {
         {}
       );
 
-      let organizationId = await queryInterface.sequelize.query(
+      let organizationObject = await queryInterface.sequelize.query(
         "SELECT id, hi5_limit from organizations;"
       );
       return queryInterface.bulkInsert(
         "users",
         [
           {
-            org_id: organizationId[0][0].id,
+            org_id: organizationObject[0][0].id,
             first_name: "admin",
             email: "admin123@gmail.com",
             soft_delete: false,
             role_id: 1,
-            hi5_quota_balance: organizationId[0][0].hi5_limit,
+            hi5_quota_balance: organizationObject[0][0].hi5_limit,
           },
         ],
         {}
