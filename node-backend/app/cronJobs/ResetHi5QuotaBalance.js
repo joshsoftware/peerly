c12b832c7b246977da3a348875c627865cf17c4d
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-15 12:52:18 +0530

    updated logic for reset hi5 quota balance

--- node-backend/app/cronJobs/ResetHi5QuotaBalance.js
+++ node-backend/app/cronJobs/ResetHi5QuotaBalance.js
@@ -10,49 +10,40 @@ const logger = log4js.getLogger();
 
 cron.schedule(process.env.TIME_RESET_HI5_QUOTA, async () => {
   try {
-    const noOfOrganisations = await Organizations.count();
-
-    try {
-      const organisationObject = await Organizations.findAll({
-        attributes: ["id", "hi5_limit", "hi5_quota_renewal_frequency"],
-      });
-      let iterator = 0;
-      while (iterator < noOfOrganisations) {
-        let updateHi5Quota = {
-          hi5_quota_balance: organisationObject[iterator].dataValues.hi5_limit,
-        };
-        if (
-          organisationObject[iterator].dataValues.hi5_quota_renewal_frequency ==
-          "WEEKLY"
-        ) {
-          if (moment().format("dddd") == "Monday") {
-            Users.update(updateHi5Quota, {
-              where: { org_id: organisationObject[iterator].dataValues.id },
-            });
-          }
-        } else if (
-          organisationObject[iterator].dataValues.hi5_quota_renewal_frequency ==
-          "MONTHLY"
-        ) {
-          if (moment().format("DD") == 1) {
-            Users.update(updateHi5Quota, {
-              where: { org_id: organisationObject[iterator].dataValues.id },
-            });
-          }
+    const organisationObject = await Organizations.findAll({
+      attributes: ["id", "hi5_limit", "hi5_quota_renewal_frequency"],
+    });
+    let iterator = 0;
+    while (iterator < organisationObject.length) {
+      let updateHi5Quota = {
+        hi5_quota_balance: organisationObject[iterator].dataValues.hi5_limit,
+      };
+      if (
+        organisationObject[iterator].dataValues.hi5_quota_renewal_frequency ==
+        "WEEKLY"
+      ) {
+        if (moment().format("dddd") == "Monday") {
+          Users.update(updateHi5Quota, {
+            where: { org_id: organisationObject[iterator].dataValues.id },
+          });
         }
-        iterator++;
+      } else if (
+        organisationObject[iterator].dataValues.hi5_quota_renewal_frequency ==
+        "MONTHLY"
+      ) {
+        if (moment().format("DD") == 1) {
+          Users.update(updateHi5Quota, {
+            where: { org_id: organisationObject[iterator].dataValues.id },
+          });
+        }
       }
-    } catch (error) {
-      logger.error("executing reset hi5 quota cron job");
-      logger.error(
-        "error accured at find hi5_limit and quota_renewal_frequency and id of organisations"
-      );
-      logger.error(error);
-      logger.info("=========================================");
+      iterator++;
     }
   } catch (error) {
     logger.error("executing reset hi5 quota cron job");
-    logger.error("error accured at counting organisations");
+    logger.error(
+      "error accured at find hi5_limit and quota_renewal_frequency and id of organisations"
+    );
     logger.error(error);
     logger.info("=========================================");
   }
