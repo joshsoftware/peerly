90290082b421c7d58c4c9780d25c47c39bfd1d9d
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-06-15 14:32:57 +0530

    add constant in responseConstants.js file use for OrganisationController

--- node-backend/app/constant/responseConstants.js
+++ node-backend/app/constant/responseConstants.js
@@ -1,12 +1,21 @@
 module.exports.INTRENAL_SERVER_ERROR_CODE = "server-error";
 
-module.exports.INTRENAL_SERVER_ERROR_MESSAGE = "internal server error";
+module.exports.INTRENAL_SERVER_ERROR_MESSAGE = "Internal server error";
 
 module.exports.INVALID_CORE_VALUE_CODE = "invalid-core-value";
 
-module.exports.INVALID_CORE_VALUE_MESSAGE = "invalid core value data";
+module.exports.INVALID_CORE_VALUE_MESSAGE = "Invalid core value data";
 
 module.exports.CORE_VALUE_NOT_FOUND_CODE = "core-value-not-found";
 
 module.exports.CORE_VALUE_NOT_FOUND_MESSAGE =
-  "core value not found for specified id";
+  "Core value not found for specified id";
+
+module.exports.INVALID_ORGANISATION_CODE = "invalid-orgnisation";
+
+module.exports.INVALID_ORGANISATION_MESSAGE = "Invalid organisation data";
+
+module.exports.ORGANISATION_NOT_FOUND_CODE = "organisation-not-found";
+
+module.exports.ORGANISATION_NOT_FOUND_MESSAGE =
+  "Organisation with specified id is not found";
