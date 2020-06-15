2bf405ca457896c0a257bec9263715e7f2647b0a
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-06-15 12:33:08 +0530

    add responseConstant.js file for get code and message from this file

---
+++ node-backend/app/constant/responseConstants.js
@@ -0,0 +1,7 @@
+module.exports.INTRENAL_SERVER_ERROR_CODE = "server-error";
+
+module.exports.INTRENAL_SERVER_ERROR_MESSAGE = "internal server error";
+
+module.exports.INVALID_CORE_VALUE_CODE = "invalid-core-value";
+
+module.exports.INVALID_CORE_VALUE_MESSAGE = "Invalid core value data";
