e555888719f146acfda69664eaa63b34c012d5c4
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-16 12:01:55 +0530

    added admin credentials to users

--- node-backend/seeders/20200614165825-users.js
+++ node-backend/seeders/20200614165825-users.js
@@ -2,20 +2,21 @@
 
 module.exports = {
   up: async (queryInterface) => {
-    let usersCount = await queryInterface.sequelize.query(
+    const usersCount = await queryInterface.sequelize.query(
       "SELECT count(*) from users;"
     );
     if (usersCount[0][0].count > 0) {
       return usersCount;
     } else {
-      let organizationObject = await queryInterface.sequelize.query(
+      const organizationObject = await queryInterface.sequelize.query(
         "SELECT id, hi5_limit from organizations;"
       );
       return queryInterface.bulkInsert("users", [
         {
           org_id: organizationObject[0][0].id,
-          first_name: "admin",
-          email: "admin@123",
+          first_name: "sunil",
+          last_name: "sonawane",
+          email: "sunil.sonawane@joshsoftware.com",
           soft_delete: false,
           role_id: 1,
           hi5_quota_balance: organizationObject[0][0].hi5_limit,
