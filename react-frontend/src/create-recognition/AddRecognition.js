dda0d388f2f62e51318ea236fefdeb0a6c755e5c
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-19 12:50:00 +0530

    updated toggle button

--- react-frontend/src/create-recognition/AddRecognition.js
+++ react-frontend/src/create-recognition/AddRecognition.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-import Image from "shared-components/image-name/UserImageName";
+import ProfileComponent from "shared-components/profile-component/ProfileComponent";
 import { Modal } from "core-components/modal/ModalComponent";
 import { Button } from "core-components/button/ButtonComponent";
 
@@ -12,6 +12,16 @@ const Border = styled.fieldset`
   opacity: 1;
   margin-left: 5%;
   margin-right: 5%;
+`;
+
+const Profile = styled(ProfileComponent)`
+  align-items: center;
+  margin-left: auto;
+`;
+const CommentBox = styled.div`
+  margin-left: 5%;
+  margin-right: 5%;
+  margin-bottom: 5%;
 `;
 
 const Legend = styled.legend`
@@ -25,6 +35,8 @@ const AddRecognition = ({
   handleClose,
   recognitionToImage,
   commentText,
+  recognitionToName,
+  sendData,
 }) => (
   <Modal
     show={show}
@@ -33,20 +45,25 @@ const AddRecognition = ({
     aria-labelledby="contained-modal-title-vcenter"
   >
     <Modal.Header closeButton>
-      <Image imageSrc={recognitionToImage} />
+      <Profile
+        className="d-flex flex-column"
+        size="14"
+        src={recognitionToImage}
+        name={recognitionToName}
+      />
     </Modal.Header>
     <Modal.Body>
       <Border>
         <Legend>Your Note</Legend>
-        {commentText}
+        <CommentBox> {commentText} </CommentBox>
       </Border>
     </Modal.Body>
     <Modal.Footer>
       <Button variant="secondary" onClick={handleClose}>
         Close
       </Button>
-      <Button variant="primary" onClick={handleClose}>
-        Save Changes
+      <Button variant="primary" onClick={sendData}>
+        Send
       </Button>
     </Modal.Footer>
   </Modal>
@@ -57,6 +74,8 @@ AddRecognition.propTypes = {
   handleClose: PropTypes.func,
   commentText: PropTypes.string,
   recognitionToImage: PropTypes.string,
+  recognitionToName: PropTypes.string,
+  sendData: PropTypes.func,
 };
 
 export default AddRecognition;
