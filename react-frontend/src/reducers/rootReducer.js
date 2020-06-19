525ad97c3d0ae2b6a4749da4c21b3ecd4d211bdf
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-19 10:41:59 +0530

    updated root reducers

--- react-frontend/src/reducers/rootReducer.js
+++ react-frontend/src/reducers/rootReducer.js
@@ -3,9 +3,13 @@ import { combineReducers } from "redux";
 import appReducer from "reducers/appReducer.js";
 import listRecognitionReducer from "reducers/listRecognitionReducer";
 import loginReducer from "reducers/loginReducer";
+import coreValueListReducer from "reducers/coreValueListReducer";
+import RecognizeToReducer from "reducers/recognizeToReducer";
 
 export default combineReducers({
   appReducer,
   listRecognitionReducer,
   loginReducer,
+  coreValueListReducer,
+  RecognizeToReducer,
 });
