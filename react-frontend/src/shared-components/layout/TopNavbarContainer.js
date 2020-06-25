4c363742bfeefe55c4605d99765163ffd58a1bcc
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-25 08:06:08 +0530

    added logout API

--- react-frontend/src/shared-components/layout/TopNavbarContainer.js
+++ react-frontend/src/shared-components/layout/TopNavbarContainer.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-//import actionObjectGenerator from "actions/listRecognitionAction";
-//import actionGenerator from "utils/actionGenerator";
-//import { LOGOUT_API } from "constants/actionConstants";
-//import { store } from "root/redux-store";*/
+import actionObjectGenerator from "actions/listRecognitionAction";
+import actionGenerator from "utils/actionGenerator";
+import { LOGOUT_API } from "constants/actionConstants";
+import { store } from "root/redux-store";
 import TopNavbar from "shared-components/layout/TopNavbar";
 
 import actionObjectGenrator from "actions/listRecognitionAction";
@@ -11,6 +11,7 @@ import actionGenrator from "utils/actionGenerator";
 import { USER_PROFILE_API } from "constants/actionConstants";
 
 const TopNavbarContainer = () => {
+  let hi5_count = 0;
   const userProfile = useSelector((state) => state.userProfileReducer);
   const dispatch = useDispatch();
   const status = actionGenrator(USER_PROFILE_API);
@@ -18,14 +19,14 @@ const TopNavbarContainer = () => {
     dispatch(actionObjectGenrator(status.success));
   }, [dispatch, status.success]);
   const onClickLogout = () => {
-    localStorage.clear();
+    const actionStatus = actionGenerator(LOGOUT_API);
+    const dispatchObject = actionObjectGenerator(actionStatus.success);
+    store.dispatch(dispatchObject);
   };
-  return (
-    <TopNavbar
-      onClickLogout={onClickLogout}
-      count={userProfile.data.hi5_quota_balance}
-    />
-  );
+  if (userProfile.data.hi5_quota_balance) {
+    hi5_count = userProfile.data.hi5_quota_balance;
+  }
+  return <TopNavbar onClickLogout={onClickLogout} count={hi5_count} />;
 };
 
 export default TopNavbarContainer;
