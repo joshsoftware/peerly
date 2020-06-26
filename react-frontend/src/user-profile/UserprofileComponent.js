dd5905285409392b0a99204cf57a19096b8a385c
Avinash Mane <avimanepatil@gmail.com>
2020-06-27 00:11:18 +0530

    added form control

--- react-frontend/src/user-profile/UserprofileComponent.js
+++ react-frontend/src/user-profile/UserprofileComponent.js
@@ -1,51 +1,86 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 import { Form } from "core-components/form/FormComponent";
+import { Card } from "core-components/card/CardComponent";
+import { Button } from "core-components/button/ButtonComponent";
+import { Col } from "core-components/grid/GridComponent";
 import ProfileComponent from "shared-components/profile-component/ProfileComponent";
 
+const CardComponent = styled(Card)`
+  border-radius: 20px;
+  margin: 20px 20px;
+  width: 550px;
+  box-shadow: 0px 5px 20px var(--box-shadow-color);
+  opacity: 1;
+  background: var(--white) 0% 0% no-repeat padding-box;
+`;
+
 const UserProfileComponent = ({
-  className,
   firstName,
   lastName,
   email,
   profileImage,
   displayName,
-  id,
 }) => {
   const onClick = () => {
     // console.log(id)
   };
   return (
-    <div className={className}>
+    <CardComponent className="text-center">
       <Form>
-        <div className="form-group">
-          <ProfileComponent
-            src={profileImage}
-            name={displayName}
-            onClick={onClick}
-          />
-          <span>
-            {email}
-            {firstName}
-            {lastName}
-            {id}
-          </span>
-          <input type="file" className="form-control-file"></input>
-        </div>
+        <ProfileComponent
+          src={profileImage}
+          name={displayName}
+          onClick={onClick}
+          className="d-flex flex-column "
+        />
+        <Form.Label>{email}</Form.Label>
+        <Form.Group controlId="formFirstName" className=" d-flex">
+          <Col>
+            <Form.Label>First Name</Form.Label>
+          </Col>
+          <Col>
+            <Form.Control type="text" placeholder={firstName} />
+          </Col>
+        </Form.Group>
+        <Form.Group controlId="formLastName" className=" d-flex">
+          <Col>
+            <Form.Label>Last Name</Form.Label>
+          </Col>
+          <Col>
+            <Form.Control type="text" placeholder={lastName} />
+          </Col>
+        </Form.Group>
+        <Form.Group controlId="formDisplayName" className=" d-flex">
+          <Col>
+            <Form.Label>Display Name</Form.Label>
+          </Col>
+          <Col>
+            <Form.Control type="text" placeholder={displayName} />
+          </Col>
+        </Form.Group>
+        <Form.Group controlId="formImageUrl" className=" d-flex">
+          <Col>
+            <Form.Label>Profile Image</Form.Label>
+          </Col>
+          <Col>
+            <Form.Control type="file" className="text-center" />
+          </Col>
+        </Form.Group>
+        <Button onClick={onClick}>Upload</Button>
       </Form>
-    </div>
+    </CardComponent>
   );
 };
 
 UserProfileComponent.propTypes = {
-  className: PropTypes.string,
   firstName: PropTypes.string,
   lastName: PropTypes.string,
   profileImage: PropTypes.string,
   displayName: PropTypes.string,
   email: PropTypes.string,
-  id: PropTypes.number,
 };
 
 export default React.memo(UserProfileComponent);
