bb5078d692eef112153e97dabcc5061e57868b55
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-30 19:08:59 +0530

    updated filter recognition

--- react-frontend/src/filterRecognition/FilterRecognitionComponent.js
+++ react-frontend/src/filterRecognition/FilterRecognitionComponent.js
@@ -10,6 +10,7 @@ import CoreValues from "../shared-components/core-value/CoreValues";
 
 const CardComponent = styled(Card)`
   margin-right: 100px;
+  border: none;
 `;
 
 const FilterRecognitionComponent = ({
@@ -23,11 +24,11 @@ const FilterRecognitionComponent = ({
 }) => {
   return (
     <CardComponent className="text-center">
-      <div className="d-flex justify-content-around flex-row text-dark">
+      <div className="d-flex justify-content-around flex-row text-dark mt-3">
         <CoreValues coreValues={coreValues} setCoreValueId={setCoreValueId} />
       </div>
       <Form className="p-3" onSubmit={onSubmit} data-testid="filterform">
-        <Form.Group controlId="filterGivenFor">
+        <Form.Group className="mt-5" controlId="filterGivenFor">
           <Form.Label>Given For</Form.Label>
           <AsyncSelect
             defaultOptions={givenForList}
@@ -36,7 +37,7 @@ const FilterRecognitionComponent = ({
             placeholder="select given for"
           />
         </Form.Group>
-        <Form.Group controlId="filterGivenBy">
+        <Form.Group className="mt-2" controlId="filterGivenBy">
           <Form.Label>Given By</Form.Label>
           <AsyncSelect
             defaultOptions={givenByList}
@@ -45,7 +46,7 @@ const FilterRecognitionComponent = ({
             placeholder="select given by"
           />
         </Form.Group>
-        <div className="text-center">
+        <div className="text-center mt-5">
           <Button type="submit" variant="success">
             Apply Filter
           </Button>
