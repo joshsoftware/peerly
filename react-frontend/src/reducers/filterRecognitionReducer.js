f702f24e02f098c7b5d259c7b8a607960b90558f
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-06-30 19:11:32 +0530

    updated filter recognition reducer

--- react-frontend/src/reducers/filterRecognitionReducer.js
+++ react-frontend/src/reducers/filterRecognitionReducer.js
@@ -6,6 +6,10 @@ export const defaultState = {
     given_for: null,
     given_by: null,
   },
+  error: {
+    code: null,
+    message: null,
+  },
 };
 
 const status = actionGenerator("FILTER_RECOGNITION");
@@ -20,16 +24,25 @@ export default (state = defaultState, action) => {
           given_for: null,
           given_by: null,
         },
+        error: {
+          code: null,
+          message: null,
+        },
       };
     case status.success:
       return {
         ...state,
         filterData: action.payload,
+        error: {
+          code: null,
+          message: null,
+        },
       };
     case status.failure:
       return {
         ...state,
         filterData: null,
+        error: action.payload,
       };
     default:
       return state;
