f9a523a89d130da7302d053d1047e018735dc295
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-02 17:51:08 +0530

    updated user list container by adding pagination

--- react-frontend/src/shared-components/user-list/UserListContainer.js
+++ react-frontend/src/shared-components/user-list/UserListContainer.js
@@ -1,38 +1,101 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import SessionTimeoutComponent from "shared-components/SessionTimeoutComponent";
-import UnauthorisedErrorComponent from "shared-components/UnauthorisedErrorComponent";
+//import SessionTimeoutComponent from "shared-components/SessionTimeoutComponent";
+//import UnauthorisedErrorComponent from "shared-components/UnauthorisedErrorComponent";
 import actionObjectGenrator from "actions/listRecognitionAction";
 import actionGenrator from "utils/actionGenerator";
-import { LIST_USERS_API } from "constants/actionConstants";
-import ListOfUsers from "shared-components/user-list/UserListComponent";
+import {
+  //GIVE_HI5_API,
+  LIST_USERS_API,
+  //LIST_RECOGNITION_API,
+  SHOW_MODAL,
+} from "constants/actionConstants";
+//import ListOfUsers from "shared-components/user-list/UserListComponent";
+import PopupWindow from "shared-components/user-list/PopupUserList";
 import { useHistory } from "react-router-dom";
 
 const UserListContainer = () => {
   let history = useHistory();
-  let [userId, setUserId] = useState(null);
-  const recognitionList = useSelector((state) => state.userListReducer);
+  // const [showError, setShowError] = useState(0);
+  // const [showPopup, setShowPopup] = useState(false);
+  // const [refresh, changeRefresh] = useState(0);
+  const [searchTerm, setSearchTerm] = useState(null);
+  const [searchResults, setSearchResults] = useState([null]);
+  const showModal = actionGenrator(SHOW_MODAL);
+  //const [show, setShow] = useState(false);
+  const handleClose = () => {
+    dispatch(actionObjectGenrator(showModal.init));
+  };
+  //const handleShow = () => setShow(true);
+  const sendData = () => {
+    //TODO
+  };
   const dispatch = useDispatch();
-  const status = actionGenrator(LIST_USERS_API);
+  //const hi5Status = actionGenrator(GIVE_HI5_API);
+  /* const handleCloseError = () => {
+     setShowPopup(false);
+     dispatch(actionObjectGenrator(hi5Status.init));
+   };*/
+  // const showErrorPopup = () => setShowPopup(true);
+  let [user1Id, setUser1Id] = useState(null);
+  //const [show, setShow] = useState(false);
+  const userList = useSelector((state) => state.userListReducer);
+  const modalShow = useSelector((state) => state.modalShowReducer);
+  const userListStatus = actionGenrator(LIST_USERS_API);
+  //  const status = actionGenrator(LIST_RECOGNITION_API);
+  const searchBox = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  const handleObserver = (entries) => {
+    if (entries[0].isIntersecting) {
+      dispatch(actionObjectGenrator(userListStatus.success));
+    }
+  };
   useEffect(() => {
-    dispatch(actionObjectGenrator(status.success));
-  }, [dispatch, status.success]);
+    if (userList.list[0].first_name) {
+      const results = userList.list.filter((person) =>
+        person.first_name.toLowerCase().includes(searchTerm)
+      );
+      setSearchResults(results);
+    }
+    if (document.getElementById("#12345")) {
+      dispatch(actionObjectGenrator(userListStatus.success));
+      const options = {
+        root: document.getElementById("#12345"), // Page as root
+        rootMargin: "0px",
+        threshold: 0,
+      };
+      const observer = new IntersectionObserver(
+        handleObserver, //callback
+        options
+      );
+      observer.observe(document.getElementById("#12345"));
+    }
+  }, [dispatch, userListStatus.success, searchTerm]);
 
-  if (recognitionList.error === "invalid_token") {
-    return <SessionTimeoutComponent />;
-  } else if (recognitionList.error === "access_denied") {
-    return <UnauthorisedErrorComponent />;
-  } else if (userId) {
-    localStorage.setItem("userId", userId);
-    setUserId(null);
+  //console.log(document.getElementById("#12345"));
+  //console.log(searchResults);
+  if (user1Id) {
+    localStorage.setItem("userId", user1Id);
+    setUser1Id(null);
     history.push("/createREcognition");
   }
-
+  //console.log(userList.list[0].first_name)
+  //console.log(searchResults[0])
   return (
-    <div>
-      <ListOfUsers userList={recognitionList.list} setUserId={setUserId} />
-    </div>
+    <PopupWindow
+      show={modalShow.show}
+      handleClose={handleClose}
+      // recognitionToImage={profileImage}
+      // recognitionToName={profileName}
+      sendData={sendData}
+      listOfEmployee={userList.list}
+      userList={searchResults[0] === null ? userList.list : searchResults}
+      //userList={userList.list}
+      setUserId={setUser1Id}
+      searchBox={searchBox}
+    ></PopupWindow>
   );
 };
 
