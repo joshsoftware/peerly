8862831ab442f3a39eb5e4d99951b2c3b3b19314
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-02 17:45:49 +0530

    updated left panel component

--- react-frontend/src/shared-components/left-panel/LeftPanelComponent.js
+++ react-frontend/src/shared-components/left-panel/LeftPanelComponent.js
@@ -1,44 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
 import { Card } from "core-components/card/CardComponent";
 import ProfileComponent from "shared-components/profile-component/ProfileComponent";
-import CreateRecognitionButton from "shared-components/create-recognition-button/CreateRecognitionButton";
 import HighFiveComponent from "shared-components/high-five-components/HighFiveComponent";
-import PopupWindow from "shared-components/user-list/PopupUserList";
-import { Modal } from "core-components/modal/ModalComponent";
+import CreateRecognitionContainer from "shared-components/create-recognition-button/CreateRecognitionContainer";
 
 const Wrapper = styled(Card)`
-  position: absolute;
-  margin-top: 100px;
-  margin-left: 260px;
-  width: 175px;
   border-radius: 36px 36px 0px 0px;
-  height: 537px;
+  height: 100%;
+  width: 20%;
   background: linear-gradient(var(--sage) 15%, var(--white) 0%);
 `;
 
 const LeftPanelComponent = ({
-  hi5ImageForButton,
   profileImage,
   profileName,
   collectedHi5,
-  listOfEmployee,
-  errorMessage,
-  handleCloseError,
-  handleShowError,
-  showPopup,
-  hi5_quota_balance,
   setUserId,
   id,
 }) => {
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  const sendData = () => {
-    //TODO
-  };
   return (
     <div className="d-none d-md-block">
       <Wrapper className="justify-content-around align-items-center d-flex flex-column  position-fixed">
@@ -59,31 +41,7 @@ const LeftPanelComponent = ({
             <span>{collectedHi5}collected</span>
           </div>
         </div>
-        <CreateRecognitionButton
-          imageUrl={hi5ImageForButton}
-          onClick={hi5_quota_balance !== 0 ? handleShow : handleShowError}
-        />
-        {showPopup ? (
-          <Modal
-            show={showPopup}
-            onHide={handleCloseError}
-            centered={true}
-            aria-labelledby="contained-modal-title-vcenter"
-          >
-            <Modal.Body closeButton className="text-center text-danger">
-              {errorMessage}
-            </Modal.Body>
-          </Modal>
-        ) : (
-          <PopupWindow
-            show={show}
-            handleClose={handleClose}
-            recognitionToImage={profileImage}
-            recognitionToName={profileName}
-            sendData={sendData}
-            listOfEmployee={listOfEmployee}
-          ></PopupWindow>
-        )}
+        <CreateRecognitionContainer />
       </Wrapper>
     </div>
   );
