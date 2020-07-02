82baaa097ce5a29fbee4d5fee82122702eeabd7f
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-02 17:48:38 +0530

    updated create recognition button component

--- react-frontend/src/shared-components/create-recognition-button/CreateRecognitionButton.js
+++ react-frontend/src/shared-components/create-recognition-button/CreateRecognitionButton.js
@@ -2,7 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
-import PlusSign from "shared-components/plus-sign/PlusSign"; //todo depend upon peerly logo
+import PlusSign from "shared-components/plus-sign/PlusSign";
+import PopupWindow from "shared-components/user-list/UserListContainer";
+import { Modal } from "core-components/modal/ModalComponent";
 
 const WrapperCircle = styled.div`
   background: var(--white) 0% 0% no-repeat padding-box;
@@ -24,18 +26,44 @@ const ButtonComponent = styled.div`
   border-radius: 50%;
 `;
 
-const CreateRecognitionButton = ({ onClick, imageUrl, theme, fontSize }) => {
+const CreateRecognitionButton = ({
+  theme,
+  fontSize,
+  hi5ImageForButton,
+  errorMessage,
+  handleCloseError,
+  handleShowError,
+  showPopup,
+  hi5_quota_balance,
+  handleShow,
+}) => {
   return (
-    <WrapperCircle className="m-auto">
-      <ButtonComponent
-        id="create-recognition-button"
-        imageUrl={imageUrl}
-        className="m-auto text-center"
-        onClick={onClick}
-      >
-        <PlusSign fontSize={fontSize} theme={theme} />
-      </ButtonComponent>
-    </WrapperCircle>
+    <>
+      <WrapperCircle className="m-auto">
+        <ButtonComponent
+          id="create-recognition-button"
+          imageUrl={hi5ImageForButton}
+          className="m-auto text-center"
+          onClick={hi5_quota_balance !== 0 ? handleShow : handleShowError}
+        >
+          <PlusSign fontSize={fontSize} theme={theme} />
+        </ButtonComponent>
+      </WrapperCircle>
+      {showPopup ? (
+        <Modal
+          show={showPopup}
+          onHide={handleCloseError}
+          centered={true}
+          aria-labelledby="contained-modal-title-vcenter"
+        >
+          <Modal.Body closeButton className="text-center text-danger">
+            {errorMessage}
+          </Modal.Body>
+        </Modal>
+      ) : (
+        <PopupWindow></PopupWindow>
+      )}
+    </>
   );
 };
 
@@ -49,6 +77,22 @@ CreateRecognitionButton.propTypes = {
   imageUrl: PropTypes.string.isRequired,
   theme: PropTypes.oneOf(["dark", "light"]),
   fontSize: PropTypes.string,
+  listOfEmployee: PropTypes.array,
+  hi5ImageForButton: PropTypes.string.isRequired,
+  profileImage: PropTypes.string.isRequired,
+  profileName: PropTypes.string.isRequired,
+  collectedHi5: PropTypes.number.isRequired,
+  errorMessage: PropTypes.string,
+  showError: PropTypes.string,
+  handleCloseError: PropTypes.func,
+  handleShowError: PropTypes.func,
+  showPopup: PropTypes.func,
+  hi5_quota_balance: PropTypes.number,
+  setUserId: PropTypes.func,
+  id: PropTypes.number,
+  userList: PropTypes.array,
+  setUser1Id: PropTypes.func,
+  handleShow: PropTypes.bool,
 };
 
 export default React.memo(CreateRecognitionButton);
