7eab56624d38e5f2602ac0405609a7676689099f
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-02 17:53:18 +0530

    updated recognition list container as change in API

--- react-frontend/src/recognition-list/RecognitionListContainer.js
+++ react-frontend/src/recognition-list/RecognitionListContainer.js
@@ -12,6 +12,7 @@ import {
   USER_PROFILE_API,
   GIVE_HI5_POST_RESPONSE,
   FILTER_STATUS,
+  //FILTER_ERROR_STATUS,
 } from "constants/actionConstants";
 import RecognitionListComponent from "recognition-list-components/RecognitionListComponent";
 
@@ -25,18 +26,23 @@ const RecognnitionListContainer = () => {
   };
   const userProfileStatus = actionGenrator(USER_PROFILE_API);
   const recognitionList = useSelector((state) => state.listRecognitionReducer);
+  /*const filterRecognition = useSelector(
+    (state) => state.filterRecognitionErrorResponse
+  );*/
   const hi5StatusResponseReducer = useSelector(
     (state) => state.hi5StatusResponseReducer
   );
   const filterStatus = useSelector((state) => state.filterStatus);
 
   const filterReducerStatus = actionGenrator(FILTER_STATUS);
+  //const filterErrorStatus = actionGenrator(FILTER_ERROR_STATUS);
   const dispatch = useDispatch();
   const status = actionGenrator(LIST_RECOGNITION_API);
   const hi5Status = actionGenrator(GIVE_HI5_API);
   const hi5StatusResponse = actionGenrator(GIVE_HI5_POST_RESPONSE);
   const [refresh, changeRefresh] = useState(0);
   const [show, setShow] = useState(false);
+  const [showHi5ListPopup, setShowHi5ListPopup] = useState(false);
   if (filterStatus.status == "applied") {
     sliderOff();
     store.dispatch(actionObjectGenrator(filterReducerStatus.init));
@@ -50,6 +56,7 @@ const RecognnitionListContainer = () => {
       dispatch(actionObjectGenrator(status.success));
     }
   };
+  //console.log(filterRecognition);
 
   useEffect(() => {
     if (refresh === 0) {
@@ -72,6 +79,10 @@ const RecognnitionListContainer = () => {
     dispatch(actionObjectGenrator(hi5StatusResponse.init));
   }
 
+  const showHi5List = () => {
+    setShowHi5ListPopup(true);
+  };
+
   const giveHi5func = async (id) => {
     if (id !== 0) {
       dispatch(
@@ -82,14 +93,18 @@ const RecognnitionListContainer = () => {
       changeRefresh(refresh + 1);
     }
   };
-
   if (recognitionList.error === "invalid_token") {
     return <SessionTimeoutComponent />;
   } else if (recognitionList.error === "access_denied") {
     return <UnauthorisedErrorComponent />;
   } else if (recognitionList.hi5.error.code !== undefined && show == false) {
     setShow(true);
-  }
+  } /*else if () {
+    console.log("in true")
+  } if (Object.keys(recognitionList.list[0].coreValue).length == 0 && filterRecognition.error.code == "recognition-not-found") {
+    dispatch(actionObjectGenrator(filterErrorStatus.init));
+    setShow(true);
+  }*/
   return (
     <div>
       <RecognitionListComponent
@@ -101,6 +116,8 @@ const RecognnitionListContainer = () => {
         sliderOn={sliderOn}
         filter={filter}
         sliderOff={sliderOff}
+        showHi5List={showHi5List}
+        showHi5ListPopup={showHi5ListPopup}
       />
     </div>
   );
