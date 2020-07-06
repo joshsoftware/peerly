51a049acbcaa50814c6324ffe0efeb375d98217b
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-06 17:02:24 +0530

    added validations for core value id

--- node-backend/controllers/v1/validationSchema/s3SignedUrlValidation.js
+++ node-backend/controllers/v1/validationSchema/s3SignedUrlValidation.js
@@ -5,3 +5,19 @@ module.exports.s3SignedUrl = () => {
     type: yup.string().required({ type: "required" }),
   });
 };
+
+module.exports.profileFileType = () => {
+  return yup.object().shape({
+    file_type: yup.string().required({ file_type: "required" }),
+  });
+};
+
+module.exports.coreValueFileType = () => {
+  return yup.object().shape({
+    file_type: yup.string().required({ file_type: "required" }),
+    core_value_id: yup
+      .number()
+      .typeError({ core_value_id: "should be number" })
+      .required({ core_value_id: "required" }),
+  });
+};
