97e730c7368f94a30e66a5b5be0a4b430f7e7d05
pooja thanekar <pujathanekar@gmail.com>
2020-07-06 15:01:44 +0530

    aws s3 signed url handler mock testing fixes aded

--- go-backend/service/aws-s3_service_http_test.go
+++ go-backend/service/aws-s3_service_http_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"encoding/json"
+	"joshsoftware/peerly/aws"
 	"joshsoftware/peerly/db"
 	"net/http"
 
@@ -12,21 +13,20 @@ import (
 
 type AWSs3TestSuite struct {
 	suite.Suite
-
-	dbMock *db.DBMockStore
+	awsMock *db.DBMockStore
 }
 
 func (suite *AWSs3TestSuite) SetupTest() {
-	suite.dbMock = &db.DBMockStore{}
+	suite.awsMock = &db.DBMockStore{}
 }
 
-var testSignedURL = db.S3SignedURL{
+var testSignedURL = aws.S3SignedURL{
 	S3SignedURL: "https://profile.s3.ap-south-1.amazonaws.com/test.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3FX253RYITCJN4AB%2F20200622%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20200622T083416Z&X-Amz-Expires=900&X-Amz-SignedHeaders=content-type%3Bhost&X-Amz-Signature=23831994bad402573f47119cd6826b8f6bd15d0c8aa9e8dc48f67c6668abac69",
 }
 
 func (suite *AWSs3TestSuite) TestGetAWSS3SignedURLSuccess() {
 
-	suite.dbMock.On("GetAWSS3SignedURL", mock.Anything, mock.Anything, mock.Anything).Return(
+	suite.awsMock.On("GetAWSS3SignedURL", mock.Anything, mock.Anything, mock.Anything).Return(
 		testSignedURL, nil,
 	)
 
@@ -34,19 +34,19 @@ func (suite *AWSs3TestSuite) TestGetAWSS3SignedURLSuccess() {
 		"/aws/s3_signed_url}",
 		"/aws/s3_signed_url?type=profile&filename=test.jpg",
 		"",
-		getS3SignedURLHandler(Dependencies{Store: suite.dbMock}),
+		getS3SignedURLHandler(Dependencies{Store: suite.awsMock}),
 	)
 
-	testSignedURLData := db.S3SignedURL{}
-	_ = json.Unmarshal(recorder.Body.Bytes(), &testSignedURLData)
-
+	responseSignedURLData := aws.S3SignedURL{}
+	_ = json.Unmarshal(recorder.Body.Bytes(), &responseSignedURLData)
 	assert.Equal(suite.T(), http.StatusOK, recorder.Code)
-
-	suite.dbMock.AssertExpectations(suite.T())
+	assert.Equal(suite.T(), responseSignedURLData, testSignedURL)
+	suite.awsMock.AssertExpectations(suite.T())
 }
 
 func (suite *AWSs3TestSuite) TestGetAWSS3SignedURLFailure() {
-	suite.dbMock.On("GetAWSS3SignedURL", mock.Anything, mock.Anything, mock.Anything).Return(
+
+	suite.awsMock.On("GetAWSS3SignedURL", mock.Anything, mock.Anything, mock.Anything).Return(
 		testSignedURL, nil,
 	)
 
@@ -54,10 +54,14 @@ func (suite *AWSs3TestSuite) TestGetAWSS3SignedURLFailure() {
 		"/aws/s3_signed_url}",
 		"/aws/s3_signed_url?type=profile&filename=test.jpg",
 		"",
-		getS3SignedURLHandler(Dependencies{Store: suite.dbMock}),
+		getS3SignedURLHandler(Dependencies{Store: suite.awsMock}),
 	)
 
-	assert.Equal(suite.T(), http.StatusInternalServerError, recorder.Code)
+	responseSignedURLData := aws.S3SignedURL{}
+	_ = json.Unmarshal(recorder.Body.Bytes(), &responseSignedURLData)
 
-	suite.dbMock.AssertExpectations(suite.T())
+	assert.Equal(suite.T(), http.StatusInternalServerError, recorder.Code)
+	assert.NotEqual(suite.T(), responseSignedURLData, testSignedURL)
+
+	suite.awsMock.AssertExpectations(suite.T())
 }
