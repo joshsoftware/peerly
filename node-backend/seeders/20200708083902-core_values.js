6233634a04c28c3c1254bcf0dd99e6ef7a95920b
Anil Maurya <anil@joshsoftware.com>
2020-07-08 22:21:35 +0530

    Replace external image with peerly-images

--- node-backend/seeders/20200708083902-core_values.js
+++ node-backend/seeders/20200708083902-core_values.js
@@ -13,32 +13,32 @@ module.exports = {
           org_id: 1,
           text: "collaboration",
           description: "Helping others in their work.",
-          thumbnail_url: "https://picsum.photos/200/300/?blur",
+          thumbnail_url: "https://peerly-images.s3.ap-south-1.amazonaws.com/mentoring%402x.png",
           parent_core_value_id: 2,
         },
         {
           org_id: 1,
-          text: "Good Citizen",
-          description: "Doing the right thing for another person or a group.",
+          text: "Training",
+          description: "Given training",
           thumbnail_url:
-            "https://i.picsum.photos/id/1012/3973/2639.jpg?hmac=s2eybz51lnKy2ZHkE2wsgc6S81fVD1W2NKYOSh8bzDc",
+            "https://peerly-images.s3.ap-south-1.amazonaws.com/training%402x.png",
           parent_core_value_id: 2,
         },
         {
           org_id: 1,
           text: "Blogger",
-          description: "Publish a blog post.",
+          description: "Published a blog post.",
           thumbnail_url:
-            "https://i.picsum.photos/id/1/5616/3744.jpg?hmac=kKHwwU8s46oNettHKwJ24qOlIAsWN9d2TtsXDoCWWsQ",
+            "https://peerly-images.s3.ap-south-1.amazonaws.com/quality%402x.png",
           parent_core_value_id: 2,
         },
         {
           org_id: 1,
           text: "Planner",
           description:
-            "Planning a party, activity that involves a lot of peolpe at Josh.",
+            "Planned a activity that involves a lots of peolpe at Josh.",
           thumbnail_url:
-            "https://i.picsum.photos/id/113/4168/2464.jpg?hmac=p1FqJDS9KHL70UWqUjlYPhJKBdiNOI_CIH0Qo-74_fU",
+            "https://peerly-images.s3.ap-south-1.amazonaws.com/planner%402x.png",
           parent_core_value_id: 2,
         },
       ]);
