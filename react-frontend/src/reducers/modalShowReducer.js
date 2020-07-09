0074190fe4a2ed0be5283025c538068f2f01cf32
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-09 13:50:12 +0530

    updated modal show reducer for console errors

--- react-frontend/src/reducers/modalShowReducer.js
+++ react-frontend/src/reducers/modalShowReducer.js
@@ -1,14 +1,14 @@
 import actionGenerator from "utils/actionGenerator";
 
 export const defaultState = {
-  show: false,
+  show: { show: false },
 };
 const status = actionGenerator("SHOW_MODAL");
 
 export default (state = defaultState, action) => {
   switch (action.type) {
     case status.init:
-      return { ...state, show: false };
+      return { ...state, show: { show: false } };
     case status.success:
       return { ...state, show: action.payload };
     case status.failure:
