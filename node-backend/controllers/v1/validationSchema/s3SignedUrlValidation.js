4cedf117afea9b8cbf5b43b84575e8ddfed97fbb
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-14 09:33:21 +0530

    removed validation for type

--- node-backend/controllers/v1/validationSchema/s3SignedUrlValidation.js
+++ node-backend/controllers/v1/validationSchema/s3SignedUrlValidation.js
@@ -6,15 +6,8 @@ module.exports.s3SignedUrl = () => {
   });
 };
 
-module.exports.profileFileType = () => {
-  return yup.object().shape({
-    file_type: yup.string().required({ file_type: "required" }),
-  });
-};
-
 module.exports.coreValueFileType = () => {
   return yup.object().shape({
-    file_type: yup.string().required({ file_type: "required" }),
     core_value_id: yup
       .number()
       .typeError({ core_value_id: "should be number" })
